85552591085b16b9c19315dfcabde22b9c5e7a81
Max Klenk <max@klenk.biz>
2021-05-28 15:13:35 +0200

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
 import './App.css';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div >
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+
+      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+          Content
+        </div>
+      </Content>
+      
+      <Footer style={{ textAlign: 'center' }}>©2021</Footer>
+    </Layout>
   );
 }
 
