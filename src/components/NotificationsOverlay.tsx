3f5d47093ba5d835a1801bbe762697e33445bd38
Addminus <adrianweniger@gmail.com>
2021-09-23 10:18:35 +0200

    added animation to overly using motion one

--- src/components/NotificationsOverlay.tsx
+++ src/components/NotificationsOverlay.tsx
@@ -1,8 +1,13 @@
 import './NotificationsOverlay.css';
 import { Button, Typography } from 'antd';
+import { animate } from "motion"
+import { useEffect, useState } from 'react';
 
-import notifications  from '../services/localNotifications'
-import { useState } from 'react';
+import notifications  from '../services/notifications'
+
+const NOTIFICATION_OVERLAY_CSS_ID = "notification-overlay"
+const NOTIFICATION_OVERLAY_VERTICAL_START_POSITION = "-400px"
+const NOTIFICATION_OVERLAY_VERTICAL_DISPLAY_POSITION = "50px"
 
 // reset permissions on chrome://settings/content/siteDetails
 function NotificationOverlay() {
@@ -10,14 +15,35 @@ function NotificationOverlay() {
 
   const activateNotifications = async () => {
     const status: NotificationPermission = await notifications.activateNotifications()
-    console.log(status)
     setPermissionStatus(status)
   }
+
+  const dismissOverlay = async () =>{
+    await animate(`#${NOTIFICATION_OVERLAY_CSS_ID}`, {
+      right: [NOTIFICATION_OVERLAY_VERTICAL_DISPLAY_POSITION, NOTIFICATION_OVERLAY_VERTICAL_START_POSITION],
+      opacity:[1,0]
+    },{
+      duration:0.2,
+      easing: "ease-in"
+    }).finished
+    setPermissionStatus("denied")
+  }
+
+  useEffect(() => {
+    animate(`#${NOTIFICATION_OVERLAY_CSS_ID}`,{
+      right: [null, NOTIFICATION_OVERLAY_VERTICAL_DISPLAY_POSITION],
+    },{
+      delay: 4,
+      duration:0.2,
+      easing: "ease-out"
+    })
+  }, [])
+
 
   //only show when user has not decided yet
   if (permissionStatus !== "default") return null
   return (
-    <div id="notification-overlay">
+    <div id={NOTIFICATION_OVERLAY_CSS_ID} style={{right: NOTIFICATION_OVERLAY_VERTICAL_START_POSITION}}>
       <div>
       <Typography.Title level={5}>Enable Notifications</Typography.Title>
       <Typography.Text>
@@ -30,7 +56,7 @@ function NotificationOverlay() {
       <Button type="link" style={{padding: 0}} onClick={activateNotifications}>
         Enable Notifications
       </Button>
-      <Button type="link" danger onClick={() => setPermissionStatus("denied")}> Dismiss</Button>
+      <Button type="link" danger onClick={dismissOverlay}> Dismiss</Button>
       </div>
     </div>
   )
