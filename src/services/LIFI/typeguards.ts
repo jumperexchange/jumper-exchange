df50b4aa7b5570aba825cb5e3c7322861e929399
Addminus <adrianweniger@gmail.com>
2021-11-10 12:00:43 +0100

    (wip) improve typeguards

--- src/services/LIFI/typeguards.ts
+++ src/services/LIFI/typeguards.ts
@@ -1,9 +1,42 @@
-import { DepositAction, WithdrawAction } from "../../types"
+import { DepositAction, WithdrawAction, Token } from "../../types"
+
+/*
+  Alternative: https://github.com/rhys-vdw/ts-auto-guard
+*/
+
 
 export const isDeposit = (deposit: DepositAction): deposit is DepositAction =>{
-  return deposit.type === 'deposit' && typeof deposit.chainId === 'number'
+  return (
+    deposit.type === 'deposit' &&
+    typeof deposit.chainId === 'number' &&
+    typeof deposit.amount === 'string' // &&
+    // isToken (deposit.token)
+    )
 }
 
 export const isWithdraw = (withdraw: WithdrawAction): withdraw is WithdrawAction =>{
-  return withdraw.type === 'withdraw' && typeof withdraw.chainId === 'number'
+  return (
+    withdraw.type === 'withdraw' &&
+    typeof withdraw.chainId === 'number' &&
+    typeof withdraw.toAddress === 'string' &&
+    typeof withdraw.slippage === 'number' // &&
+    // isToken (deposit.token)
+  )
 }
+
+export const isToken = (token: Token): token is Token => {
+  return true
+}
+
+
+  //   // token: Token;
+  //   export interface Token {
+  //     id: string;
+  //     symbol: string;
+  //     decimals: number;
+  //     chainId: number;
+  //     name: string;
+  //     chainKey: ChainKey;
+  //     key: CoinKey;
+  //     logoURI: string;
+  // }
