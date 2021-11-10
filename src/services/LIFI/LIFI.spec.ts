3e8a7d67d46812b7865216efd7efa3edf4fedae6
Addminus <adrianweniger@gmail.com>
2021-11-10 16:14:30 +0100

    fix type import

--- src/services/LIFI/LIFI.spec.ts
+++ src/services/LIFI/LIFI.spec.ts
@@ -1,84 +1,97 @@
-import axios, {AxiosStatic} from 'axios'
-jest.mock('axios')
-const mockedAxios = axios as jest.Mocked< typeof axios >
+import axios, { AxiosStatic } from "axios";
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+import {
+  ChainId,
+  ChainKey,
+  CoinKey,
+  findDefaultCoinOnChain,
+  DepositAction,
+  WithdrawAction,
+} from "@lifinance/types";
 
-import { ChainId, ChainKey, CoinKey } from '../../types/shared/base.types'
-import {findDefaultCoinOnChain} from '../../types/shared/coins.types'
-import { DepositAction, WithdrawAction } from '../../types/shared/step.types'
-import Lifi from './Lifi'
+import Lifi from "./Lifi";
 
-describe('LIFI SDK', () => {
-  describe('findRoutes', () => {
-    describe('user input is invalid', () => {
-      it('should throw invalid deposit type', async () => {
+describe("LIFI SDK", () => {
+  describe("findRoutes", () => {
+    describe("user input is invalid", () => {
+      it("should throw invalid deposit type", async () => {
         const deposit = {
-          type: 'deposit xxx',
+          type: "deposit xxx",
           chainId: ChainId.BSC,
-          amount: '10000000000000',
-          token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.BSC)
-        }
+          amount: "10000000000000",
+          token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.BSC),
+        };
 
         const withdraw: WithdrawAction = {
-          type: 'withdraw',
+          type: "withdraw",
           chainId: ChainId.DAI,
           token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.DAI),
-          amount: '',
-          toAddress: '',
+          amount: "",
+          toAddress: "",
           slippage: 0.03,
-        }
+        };
 
-        await expect(Lifi.findRoutes(deposit as unknown as DepositAction, withdraw as WithdrawAction)).rejects.toThrow('Invalid Deposit Type');
-        expect(mockedAxios.post).toHaveBeenCalledTimes(0)
-
-
-      })
-      it.skip('should throw invalid withdraw type', async () => {
+        await expect(
+          Lifi.findRoutes(
+            deposit as unknown as DepositAction,
+            withdraw as WithdrawAction
+          )
+        ).rejects.toThrow("Invalid Deposit Type");
+        expect(mockedAxios.post).toHaveBeenCalledTimes(0);
+      });
+      it.skip("should throw invalid withdraw type", async () => {
         const deposit: DepositAction = {
-          type: 'deposit',
+          type: "deposit",
           chainId: ChainId.BSC,
-          amount: '10000000000000',
+          amount: "10000000000000",
           token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.BSC),
-        }
+        };
 
         const withdraw = {
-          type: 'withdraw xxx',
+          type: "withdraw xxx",
           chainId: ChainId.DAI,
           token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.DAI),
-          amount: '',
-          toAddress: '',
+          amount: "",
+          toAddress: "",
           slippage: 0.03,
-        }
+        };
 
-        await expect(Lifi.findRoutes(deposit as DepositAction, withdraw as unknown as WithdrawAction)).rejects.toThrow('Invalid Withdraw Type');
-        expect(mockedAxios.post).toHaveBeenCalledTimes(0)
-      })
-    })
+        await expect(
+          Lifi.findRoutes(
+            deposit as DepositAction,
+            withdraw as unknown as WithdrawAction
+          )
+        ).rejects.toThrow("Invalid Withdraw Type");
+        expect(mockedAxios.post).toHaveBeenCalledTimes(0);
+      });
+    });
 
-    describe.skip('user input is valid',  () => {
-      it('should call server once', async () => {
+    describe.skip("user input is valid", () => {
+      it("should call server once", async () => {
         const deposit: DepositAction = {
-          type: 'deposit',
+          type: "deposit",
           chainId: ChainId.BSC,
-          amount: '10000000000000',
-          token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.BSC)
-        }
+          amount: "10000000000000",
+          token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.BSC),
+        };
 
         const withdraw: WithdrawAction = {
-          type: 'withdraw',
+          type: "withdraw",
           chainId: ChainId.DAI,
           token: findDefaultCoinOnChain(CoinKey.USDC, ChainKey.DAI),
-          amount: '',
-          toAddress: '',
+          amount: "",
+          toAddress: "",
           slippage: 0.03,
-        }
+        };
 
-        Lifi.findRoutes(deposit as DepositAction, withdraw as unknown as WithdrawAction)
-        expect(mockedAxios.post).toHaveBeenCalledTimes(1)
-      })
-    })
-
-  })
-
-
-})
+        Lifi.findRoutes(
+          deposit as DepositAction,
+          withdraw as unknown as WithdrawAction
+        );
+        expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+});
