c1f4261e81401f74261fe89c374995e7935f3183
Addminus <adrianweniger@gmail.com>
2021-12-03 14:29:42 +0100

    WIP add lifi functions

--- src/services/LIFI/Lifi.ts
+++ src/services/LIFI/Lifi.ts
@@ -1,9 +1,20 @@
-import { RoutesRequest, RoutesResponse, Step, StepTransactionResponse } from '@lifinance/types'
+import {
+  Route,
+  RoutesRequest,
+  RoutesResponse,
+  Step,
+  StepTransactionResponse,
+} from '@lifinance/types'
 import axios from 'axios'
+import { Signer } from 'ethers'
 
+import { StepExecutor } from './executionFiles/StepExecutor'
 import { isRoutesRequest, isStep } from './typeguards'
 
 class LIFI {
+  private activeRoutes: Route[] = []
+  private registeredCallbackFunctions: Record<string, (updatedRoute: Route) => void> = {}
+
   getRoutes = async (routesRequest: RoutesRequest): Promise<RoutesResponse> => {
     if (!isRoutesRequest(routesRequest)) {
       throw new Error('SDK Validation: Invalid Routs Request')
@@ -30,24 +41,50 @@ class LIFI {
     return result.data
   }
 
-  // executeRoute = (signer: Signer, route: Route): Promise<Route> => {
+  executeRoute = async (signer: Signer, route: Route): Promise<Route> => {
+    /*
+      loop though steps
+        for each step start execution
+        if step execution done or fails
+      // TODO: add generalized execution Manger class
 
-  // }
+    */
+    // check if route is already running
+    const activeRoute = this.activeRoutes.find((activeRoute) => activeRoute.id === route.id)
+    if (activeRoute) return activeRoute
+    this.activeRoutes.push(route)
 
+    // execute all steps
+    // for (const step of route.steps) {
+    //   const stepExecutor = new StepExecutor()
+    //   stepExecutor.executeStep(signer, step, updateFunction)
+    //   this.registeredCallbackFunctions[route.id](route)
+    // }
+
+    //clean up after execution
+    this.deregisterCallback(this.registeredCallbackFunctions[route.id], route)
+    this.activeRoutes = this.activeRoutes.filter((activeRoute) => activeRoute.id !== route.id)
+    return route
+  }
+
+  stopExecution = (route: Route): Route => {
+    return route
+  }
+
+  registerCallback = (callback: (updatedRoute: Route) => void, route: Route): void => {
+    this.registeredCallbackFunctions[route.id] = callback
+  }
+
+  deregisterCallback = (callback: (updateRoute: Route) => void, route: Route): void => {
+    delete this.registeredCallbackFunctions[route.id]
+  }
+
+  getActiveRoutes = (): Route[] => {
+    return this.activeRoutes
+  }
+
+  // TODO: implement this function
   // getCurrentStatus = (route: Route): Route => {
-
-  // }
-
-  // registerCallback = (callback: (updatedRoute: Route) => void, route?: Route): void => {
-
-  // }
-
-  // deregisterCallback = (callback: (updateRoute: Route) => void, route?: Route): void => {
-
-  // }
-
-  // getActiveExecutions = (): Route[] => {
-
   // }
 }
 
