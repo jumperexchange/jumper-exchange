8200abf3184624e427750d5c0edcdf0621d03ef6
Adrian Weniger <adrianweniger@gmail.com>
2022-02-09 16:11:12 +0100

    refactor: disable testnet page (#209)

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,10 +1,20 @@
+import { Typography } from 'antd'
 import { Content } from 'antd/lib/layout/layout'
 import React from 'react'
 
 function NotFoundPage() {
   return (
-    <Content className="site-layout">
-      <div>Not Found</div>
+    <Content style={{ width: '100vw', height: 'calc(100vh - 64px)' }} className="site-layout">
+      <Typography.Title
+        disabled
+        style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}>
+        Not Found
+      </Typography.Title>
     </Content>
   )
 }
