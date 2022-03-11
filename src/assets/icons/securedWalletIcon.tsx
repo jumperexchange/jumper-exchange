a990c81226f7a46882612710a10e96c29b79fe9b
Addminus <adrianweniger@gmail.com>
2022-03-11 10:57:18 +0100

    style: implement design fixes

--- src/assets/icons/securedWalletIcon.tsx
+++ src/assets/icons/securedWalletIcon.tsx
@@ -2,24 +2,28 @@ import React from 'react'
 
 export function SecuredWalletIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="41" height="41" fill="none" viewBox="0 0 41 41">
-      <path
-        stroke="#222"
-        strokeLinecap="square"
+    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none" viewBox="0 0 80 80">
+      <rect width="56" height="56" x="12" y="12" stroke="#000" strokeWidth="2" rx="10"></rect>
+      <path stroke="#000" strokeWidth="2" d="M7 26h10M7 54h10"></path>
+      <rect
+        width="22"
+        height="22"
+        x="32"
+        y="29"
+        fill="#000"
+        stroke="#000"
         strokeWidth="2"
-        d="M35.667 9.667V1H4a3 3 0 00-3 3v5.667M1 37V9.667h39V40H4a3 3 0 01-3-3z"></path>
-      <path
-        stroke="#222"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        rx="11"></rect>
+      <rect
+        width="32"
+        height="32"
+        x="27"
+        y="24"
+        stroke="#000"
+        strokeDasharray="4 4"
         strokeWidth="2"
-        d="M16.604 21.059v-1.716c0-1.982 1.863-3.586 4.158-3.586 2.295 0 4.15 1.604 4.15 3.586v1.656M26.937 33.568H14.58c-.29 0-.568-.117-.773-.324a1.114 1.114 0 01-.32-.783v-9.248c.002-.263.095-.516.262-.717.167-.2.398-.336.653-.382l3.726-.78a15.549 15.549 0 015.31 0l3.685.772c.254.046.484.181.65.382.166.2.256.454.256.716v9.257a1.12 1.12 0 01-.32.783 1.094 1.094 0 01-.772.324v0z"></path>
-      <path
-        stroke="#222"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M20.76 28.394c1.038 0 1.88-.852 1.88-1.904s-.841-1.905-1.88-1.905c-1.038 0-1.88.853-1.88 1.905s.842 1.904 1.88 1.904zM20.767 28.506v1.552"></path>
+        rx="16"></rect>
+      <rect width="8" height="8" x="44" y="36" fill="#fff" rx="4"></rect>
     </svg>
   )
 }
