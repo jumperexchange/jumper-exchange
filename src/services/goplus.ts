ed6ce776bb57f48c987688850fd4312b0b586efc
Max Klenk <max@klenk.biz>
2022-04-08 11:03:20 +0200

    feat(goplus): improve token data parsing

--- src/services/goplus.ts
+++ src/services/goplus.ts
@@ -1,7 +1,6 @@
 import { ChainId, getChainById } from '@lifinance/sdk'
 import axios from 'axios'
 import { ethers } from 'ethers'
-import { isSharedArrayBuffer } from 'util/types'
 
 const API = 'https://api.gopluslabs.io/api/v1'
 
@@ -32,52 +31,84 @@ interface Dex {
   pair: string
 }
 interface TokenSecurityDataRaw {
-  is_airdrop_scam: string
-  buy_tax: string
-  cannot_sell_all: string
-  creator_address: string
-  creator_balance: string // float
-  creator_percent: string // float
-  dex: Dex[]
-  holder_count: string
-  holders: Holder[]
-  is_in_dex: string
+  // Contract Security (https://docs.gopluslabs.io/reference/token-info-response-detail/contract-security)
   is_open_source: string
-  is_proxy: string
-  lp_holder_count: string
-  lp_holders: Holder[]
-  lp_total_supply: string // float
+  is_proxy?: string
+  is_mintable?: string
   owner_address: string
-  owner_balance: string
-  owner_percent: string
-  sell_tax: string
-  total_supply: string
+  can_take_back_ownership?: string
+  owner_change_balance?: string
+
+  // Trading Security (https://docs.gopluslabs.io/reference/token-info-response-detail/trading-security)
+  buy_tax?: string
+  sell_tax?: string
+  cannot_sell_all?: string
+  slippage_modifiable?: string
+  is_honeypot?: string
+  transfer_pausable?: string
+  is_blacklisted?: string // holder list in contract
+  is_whitelisted?: string // holder list in contract
+  is_in_dex: string
+  dex: Dex[]
+  anti_whale?: string
+
+  // Info Security (https://docs.gopluslabs.io/reference/token-info-response-detail/info-security)
   token_name: string
   token_symbol: string
+  holder_count: string // number
+  total_supply: string
+  holders: Holder[] // top 10
+  owner_balance?: string // number
+  owner_percent?: string // float
+  creator_address: string
+  creator_balance?: string // number
+  creator_percent?: string // float
+  lp_holder_count?: string // number
+  lp_total_supply?: string // float
+  lp_holders?: Holder[]
+  is_true_token?: string // boolean
+  is_airdrop_scam?: string // boolean
+  trust_list?: string // boolean
 }
 interface TokenSecurityData {
-  is_airdrop_scam: boolean
-  buy_tax: boolean
-  cannot_sell_all: boolean
-  creator_address: string
-  creator_balance: string // float
-  creator_percent: string // float
-  dex: Dex[]
-  holder_count: string
-  holders: Holder[]
-  is_in_dex: boolean
+  // Contract Security
   is_open_source: boolean
-  is_proxy: boolean
-  lp_holder_count: string
-  lp_holders: Holder[]
-  lp_total_supply: string // float
-  owner_address: string
-  owner_balance: string
-  owner_percent: string
-  sell_tax: boolean
-  total_supply: string
+  is_proxy?: boolean
+  is_mintable?: boolean
+  owner_address?: string
+  can_take_back_ownership?: boolean
+  owner_change_balance?: boolean
+
+  // Trading Security
+  buy_tax?: boolean
+  sell_tax?: boolean
+  cannot_sell_all?: boolean
+  slippage_modifiable?: boolean
+  is_honeypot?: boolean
+  transfer_pausable?: boolean
+  is_blacklisted?: boolean // holder list in contract
+  is_whitelisted?: boolean // holder list in contract
+  is_in_dex: boolean
+  dex: Dex[]
+  anti_whale?: boolean
+
+  // Info Security
   token_name: string
   token_symbol: string
+  holder_count: string // number
+  total_supply: string
+  holders: Holder[] // top 10
+  owner_balance?: string // number
+  owner_percent?: string // float
+  creator_address: string
+  creator_balance?: string // number
+  creator_percent?: string // float
+  lp_holder_count?: string // number
+  lp_total_supply?: string // float
+  lp_holders?: Holder[]
+  is_true_token?: boolean
+  is_airdrop_scam?: boolean
+  trust_list?: boolean
 }
 export enum TokenSecurityState {
   SAFE = 'safe',
@@ -96,17 +127,41 @@ const map: Record<number, Record<string, Promise<TokenSecurity>>> = {}
 const stringToBoolean = (input: string): boolean => {
   return input === '1'
 }
+const stringToBooleanOptional = (input?: string): boolean | undefined => {
+  if (input === undefined) {
+    return undefined
+  }
+  return stringToBoolean(input)
+}
 
 const parseTokenSecurityDataRaw = (data: TokenSecurityDataRaw): TokenSecurityData => {
   return {
     ...data,
-    is_airdrop_scam: stringToBoolean(data.is_airdrop_scam),
-    buy_tax: stringToBoolean(data.buy_tax),
-    cannot_sell_all: stringToBoolean(data.cannot_sell_all),
-    is_in_dex: stringToBoolean(data.is_in_dex),
+
+    // Contract Security
     is_open_source: stringToBoolean(data.is_open_source),
-    is_proxy: stringToBoolean(data.is_proxy),
-    sell_tax: stringToBoolean(data.sell_tax),
+    is_proxy: stringToBooleanOptional(data.is_proxy),
+    is_mintable: stringToBooleanOptional(data.is_mintable),
+    owner_address: data.owner_address !== '' ? data.owner_address : undefined,
+    can_take_back_ownership: stringToBooleanOptional(data.can_take_back_ownership),
+    owner_change_balance: stringToBooleanOptional(data.owner_change_balance),
+
+    // Trading Security
+    buy_tax: stringToBooleanOptional(data.buy_tax),
+    sell_tax: stringToBooleanOptional(data.sell_tax),
+    cannot_sell_all: stringToBooleanOptional(data.cannot_sell_all),
+    slippage_modifiable: stringToBooleanOptional(data.slippage_modifiable),
+    is_honeypot: stringToBooleanOptional(data.is_honeypot),
+    transfer_pausable: stringToBooleanOptional(data.transfer_pausable),
+    is_blacklisted: stringToBooleanOptional(data.is_blacklisted),
+    is_whitelisted: stringToBooleanOptional(data.is_whitelisted),
+    is_in_dex: stringToBoolean(data.is_open_source),
+    anti_whale: stringToBooleanOptional(data.anti_whale),
+
+    // Info Security
+    is_true_token: stringToBooleanOptional(data.is_true_token),
+    is_airdrop_scam: stringToBooleanOptional(data.is_airdrop_scam),
+    trust_list: stringToBooleanOptional(data.trust_list),
   }
 }
 
@@ -139,12 +194,21 @@ const isSafeToken = (data: TokenSecurityData) => {
     return false // unable to verify - dangerous
   }
 
-  if (data.is_airdrop_scam) {
+  if (data.is_airdrop_scam === true || data.is_true_token === false) {
     return false // clearly scam
   }
+  if (data.trust_list === true || data.is_true_token === true) {
+    return true // trusted
+  }
 
-  if (data.sell_tax || data.buy_tax) {
+  if (data.sell_tax === true || data.buy_tax === true) {
     return false // can't be used and potential scam
+  }
+  if (data.owner_change_balance === true) {
+    return false // owner can steal funds
+  }
+  if (data.is_honeypot === true) {
+    return false
   }
 
   return true
