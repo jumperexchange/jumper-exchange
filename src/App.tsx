beaa5bc4595cd18167857b38aebace27b44b1b18
Max Klenk <max@klenk.biz>
2022-04-20 16:40:31 +0200

    hide from menu until released

--- src/App.tsx
+++ src/App.tsx
@@ -172,9 +172,10 @@ function App() {
                         Explore Docs
                       </a>
                     </Menu.Item>
-                    <Menu.Item key="/showcase/etherspot-klima">
+                    {/* Hidden until officially announced */}
+                    {/* <Menu.Item key="/showcase/etherspot-klima">
                       <Link to="/showcase/etherspot-klima">Etherspot+KLIMA</Link>
-                    </Menu.Item>
+                    </Menu.Item> */}
                     {/* <Menu.Item>
                       <a href="https://docs.li.finance/for-users/user-faq" target="_blank" rel="noreferrer">FAQ</a>
                     </Menu.Item> */}
