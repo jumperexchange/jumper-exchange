7fd65b4340bbc863dc0e1a04982e0ce9bd5599ae
Addminus <adrianweniger@gmail.com>
2022-06-13 15:52:03 +0200

    feat: setup storybook for shared components

---
+++ packages/shared/src/atoms/link/link.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "./";
+
+export default {
+  title: "Atoms/Link",
+  component: Link,
+};
+
+export const Default = () => (
+  <Link url="https://google.com">Click Here For Fun!</Link>
+);
