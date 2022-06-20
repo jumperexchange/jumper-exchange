9720c24a8378cf7f74d9dc5184402b46aef3f852
dennyscode <43956540+dennyscode@users.noreply.github.com>
2022-06-20 04:47:09 -0500

    feat: add spacing presets (#2)

---
+++ packages/shared/style/spacing.ts
@@ -0,0 +1,24 @@
+import { SpacingPalette } from '@lifi/types'
+
+export const spacing: SpacingPalette = {
+  space: '4px',
+  space_2: '8px',
+  space_3: '12px',
+  space_4: '16px',
+  space_5: '20px',
+  space_6: '24px',
+  space_7: '28px',
+  space_8: '32px',
+  space_9: '36px',
+  space_10: '40px',
+  space_11: '44px',
+  space_12: '48px',
+  space_13: '52px',
+  space_14: '56px',
+  space_15: '60px',
+  space_16: '64px',
+  space_17: '68px',
+  space_18: '72px',
+  space_19: '76px',
+  space_20: '80px',
+}
