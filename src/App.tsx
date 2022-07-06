470e9238233cea0297579ea144709bdd800d8bfc
Addminus <adrianweniger@gmail.com>
2022-07-06 14:25:57 +0200

    refactor: extract navConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
 import './AntOverrides.css'
 
-import { CaretDownOutlined, GithubOutlined, TwitterOutlined } from '@ant-design/icons'
+import { GithubOutlined, TwitterOutlined } from '@ant-design/icons'
 import { Button, Col, Layout, Menu, Row } from 'antd'
 import { Content, Header } from 'antd/lib/layout/layout'
-import { ItemType } from 'antd/lib/menu/hooks/useItems'
 import { useEffect, useState } from 'react'
 import { Link, Redirect, Route, Switch, useLocation } from 'react-router-dom'
 
@@ -20,18 +19,18 @@ import SwapUkraine from './components/SwapUkraine'
 import WalletButtons from './components/web3/WalletButtons'
 import Web3ConnectionManager from './components/web3/Web3ConnectionManager'
 import WrappedWeb3ReactProvider from './components/web3/WrappedWeb3ReactProvider'
-import { useIsMobile } from './hooks/useIsMobile'
+import {
+  ENABLE_ETHERSPOT_KLIMA_SHOWCASE,
+  REACT_APP_ENABLE_OFFSET_CARBON_SHOWCASE,
+} from './constants/featureFlags'
+import { useNavConfig } from './hooks/useNavConfig'
 import { usePageViews } from './hooks/usePageViews'
 import { ChainsTokensToolsProvider } from './providers/chainsTokensToolsProvider'
 import setMetatags from './services/metatags'
 import { initStomt } from './services/stomt'
 
-const ENABLE_ETHERSPOT_KLIMA_SHOWCASE = process.env.REACT_APP_ENABLE_ETHERSPOT_KLIMA === 'true'
-const REACT_APP_ENABLE_OFFSET_CARBON_SHOWCASE =
-  process.env.REACT_APP_ENABLE_OFFSET_CARBON === 'true'
-
 function App() {
-  const isMobile = useIsMobile()
+  const navConfig = useNavConfig()
   const location = useLocation()
   const path = usePageViews()
   const [adjustNavBarToBgGradient, setAdjustNavBarToBgGradient] = useState(
@@ -100,130 +99,6 @@ function App() {
     )
   }
 
-  const menuItems: ItemType[] = [
-    { label: <Link to="/swap">Swap & Bridge</Link>, key: '/swap' },
-    { label: <Link to="/dashboard">Dashboard</Link>, key: '/dashboard' },
-    {
-      label: (
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSe9fDY1zCV3vnaubD0740GHzUYcfZoiz2KK_5TIME-rnIA3sg/viewform"
-          target="_blank"
-          rel="nofollow noreferrer">
-          Developers
-        </a>
-      ),
-      key: 'dev-list',
-    },
-    {
-      label: (
-        <span className="lifi-more-submenu-title-wrapper">
-          More <CaretDownOutlined className="lifi-more-submenu-title-icon" />
-        </span>
-      ),
-      key: 'lifi-more-submenu',
-      disabled: false,
-      children: [
-        {
-          label: (
-            <a href="https://blog.li.finance/" target="_blank" rel="nofollow noreferrer">
-              Blog
-            </a>
-          ),
-          key: 'blog',
-        },
-        {
-          label: (
-            <a href="https://docs.li.finance/" target="_blank" rel="nofollow noreferrer">
-              Explore Docs
-            </a>
-          ),
-          key: 'docs',
-        },
-        {
-          label: (
-            <a href="https://li.fi/" target="_blank" rel="nofollow noreferrer">
-              About
-            </a>
-          ),
-          key: 'about',
-        },
-        {
-          label: 'Showcases',
-          key: 'lifi-showcase-submenu',
-          children: [
-            {
-              key: '/showcase/ukraine',
-              label: (
-                <>
-                  <span className="ukraine-flag">&#127482;&#127462;</span>
-                  <Link to="/showcase/ukraine">Help Ukraine!</Link>
-                </>
-              ),
-            },
-            {
-              type: 'group',
-              label: 'Showcases',
-              key: '/showcase',
-              children: [
-                {
-                  key: '/showcase/etherspot-klima',
-                  label: <Link to="/showcase/etherspot-klima">Cross-Chain Klima Staking</Link>,
-                },
-                REACT_APP_ENABLE_OFFSET_CARBON_SHOWCASE
-                  ? {
-                      key: '/showcase/carbon-offset',
-                      label: (
-                        <Link to="/showcase/carbon-offset">Cross-Chain Carbon Offsetting</Link>
-                      ),
-                    }
-                  : null,
-              ],
-            },
-          ],
-        },
-        {
-          label: 'Legals',
-          key: 'legals-submenu',
-          children: [
-            {
-              label: (
-                <a href="https://li.fi/legal/privacy-policy/" target={'_blank'} rel="noreferrer">
-                  Privacy Policy
-                </a>
-              ),
-              key: 'privacy',
-            },
-            {
-              label: (
-                <a href="https://li.fi/legal/imprint/" target={'_blank'} rel="noreferrer">
-                  Imprint
-                </a>
-              ),
-              key: 'imprint',
-            },
-            {
-              label: (
-                <a
-                  href="https://li.fi/legal/terms-and-conditions/"
-                  target={'_blank'}
-                  rel="noreferrer">
-                  Terms & Conditions
-                </a>
-              ),
-              key: 'termsAndConditions',
-            },
-          ],
-        },
-      ],
-    },
-    isMobile
-      ? {
-          key: 'wallet-button',
-          label: <WalletButtons></WalletButtons>,
-        }
-      : null,
-  ]
-
   return (
     <WrappedWeb3ReactProvider>
       <Web3ConnectionManager>
@@ -261,7 +136,7 @@ function App() {
                       </a>
                     </div>
                     <Menu
-                      items={menuItems}
+                      items={navConfig}
                       theme="light"
                       mode="horizontal"
                       triggerSubMenuAction="hover"
