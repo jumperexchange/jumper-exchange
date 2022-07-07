4c2163145f701edf0c6d18c2c05e9399b8a9271c
Addminus <adrianweniger@gmail.com>
2022-07-07 11:20:26 +0200

    feat: add discord icon to support link

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Content, Header } from 'antd/lib/layout/layout'
 import { useEffect, useState } from 'react'
 import { Link, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 
+import DiscordIcon from './assets/icons/discordIcon'
 import { PoweredByLiFi } from './assets/Li.Fi/poweredByLiFi'
 import Dashboard from './components/Dashboard'
 import SwapCarbonOffsetEmbed from './components/EmbedViews/SwapCarbonOffsetEmbed'
@@ -174,12 +175,14 @@ function App() {
                       </Col>
                       <Col>
                         <a
-                          style={{ padding: '13.5px 24px 13.5px 24px' }}
+                          style={{
+                            padding: '13.5px 24px 13.5px 24px',
+                          }}
                           className="lifi-support-link headerIconLink lifi-header-social-links"
                           href="https://discord.gg/lifi"
                           target="_blank"
                           rel="nofollow noreferrer">
-                          Support
+                          <DiscordIcon style={{ marginRight: 4 }} /> Support
                         </a>
                       </Col>
                       <Col>
@@ -282,7 +285,7 @@ function App() {
                   href="https://discord.gg/lifi"
                   target="_blank"
                   rel="nofollow noreferrer">
-                  Support
+                  <DiscordIcon style={{ marginRight: 4 }} /> Support
                 </Button>
               </div>
 
