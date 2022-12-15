98acc672a0a65fcc0e2f1268ec263e8ecf010bfc
Addminus <adrianweniger@gmail.com>
2022-12-15 17:05:45 +0100

    chore: remove dashboard code

--- packages/dashboard-vite/src/services/balances/utils.ts
+++
@@ -1,9 +0,0 @@
-import { TokenAmount } from '@lifi/sdk'
-
-import { blockedTokens } from './config'
-
-export const filterBlockedTokenAmounts = (tokenAmounts: TokenAmount[]) => {
-  return tokenAmounts.filter(
-    ({ chainId, address }) => !(blockedTokens[chainId] && blockedTokens[chainId].includes(address)),
-  )
-}
