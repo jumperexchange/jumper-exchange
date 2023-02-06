ba69dfad26201624a765b949ae15b06317e1e3c4
Eugene Chybisov <imchybisov@gmail.com>
2023-02-06 15:02:51 +0700

    fix: add new languages exports

--- packages/dapp/src/i18n/index.ts
+++ packages/dapp/src/i18n/index.ts
@@ -1,5 +1,14 @@
 import de from './de.json';
 import en from './en.json';
+import it from './it.json';
 import uk from './uk.json';
+import zh from './zh.json';
 
-export { de, en, uk };
+export {
+  de,
+  en,
+  it,
+  uk,
+  zh
+};
+
