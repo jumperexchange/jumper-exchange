53a5b227933811f9c333266dce639cc9b4aa6a19
Eugene Chybisov <imchybisov@gmail.com>
2023-02-13 15:03:40 +0700

    feat: add es translations

--- packages/dapp/src/i18n/index.ts
+++ packages/dapp/src/i18n/index.ts
@@ -1,5 +1,6 @@
 import de from './de.json';
 import en from './en.json';
+import es from './es.json';
 import it from './it.json';
 import uk from './uk.json';
 import zh from './zh.json';
@@ -7,6 +8,7 @@ import zh from './zh.json';
 export {
   de,
   en,
+  es,
   it,
   uk,
   zh
