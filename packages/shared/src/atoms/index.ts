12ff61d10e53826d15b3dd7639df92d0a7d15e93
Max Klenk <max@klenk.biz>
2023-02-19 12:41:42 +0100

    fix: rename capitalized folder

--- packages/shared/src/atoms/index.ts
+++ packages/shared/src/atoms/index.ts
@@ -2,4 +2,4 @@ export { Button } from './Button';
 export { ButtonPrimary } from './ButtonPrimary';
 export { ButtonSecondary } from './ButtonSecondary';
 export * from './icons';
-export * from './Link';
+export * from './link';
