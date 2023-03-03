77bd03cac1674b5269023605378520c12c2c2255
Eugene Chybisov <imchybisov@gmail.com>
2023-03-03 15:39:39 +0800

    i18n: add Thai language

--- packages/dapp/src/i18n/index.ts
+++ packages/dapp/src/i18n/index.ts
@@ -5,8 +5,9 @@ import fr from './fr.json';
 import id from './id.json';
 import it from './it.json';
 import ko from './ko.json';
+import th from './th.json';
 import tr from './tr.json';
 import uk from './uk.json';
 import zh from './zh.json';
 
-export { de, en, es, fr, id, it, ko, tr, uk, zh };
+export { de, en, es, fr, id, it, ko, th, tr, uk, zh };
