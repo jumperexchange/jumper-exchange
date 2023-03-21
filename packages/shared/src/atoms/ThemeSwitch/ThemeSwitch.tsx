d06e18d26e57e6c28fec6b719f2df87d82852376
dennyscode <43956540+dennyscode@users.noreply.github.com>
2023-03-20 18:58:10 -0600

    feat: add theme-switch tracking event (#113)

--- packages/shared/src/atoms/ThemeSwitch/ThemeSwitch.tsx
+++ packages/shared/src/atoms/ThemeSwitch/ThemeSwitch.tsx
@@ -3,6 +3,7 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import NightlightIcon from '@mui/icons-material/Nightlight';
 import Tooltip from '@mui/material/Tooltip';
 import { useTranslation } from 'react-i18next';
+import { useUserTracking } from '../../../../dapp/src/hooks';
 import { useDetectDarkModePreference } from '../../../../dapp/src/providers/ThemeProvider';
 import { useSettings } from '../../hooks';
 import { ButtonThemeSwitch } from './ThemeSwitch.style';
@@ -11,9 +12,16 @@ export const ThemeSwitch = () => {
   const settings = useSettings();
   const { t: translate } = useTranslation();
   const i18Path = 'navbar.';
+  const { trackEvent } = useUserTracking();
 
   const handleThemeSwitch = () => {
     settings.onChangeMode(isDarkMode ? 'light' : 'dark');
+    trackEvent({
+      category: 'theme-switch',
+      action: `click-theme-switch`,
+      label: isDarkMode ? 'light' : 'dark',
+      data: { themeSwitch: `theme-${isDarkMode ? 'light' : 'dark'}` },
+    });
   };
 
   return (
