f7049c486032564843cdb5432d1e379105870a77
dennyscode <sander.bewerbung@gmx.de>
2023-04-13 17:40:30 +0200

    fix: use integrator string, add to envs

--- packages/dapp/src/components/Widget/Widget.tsx
+++ packages/dapp/src/components/Widget/Widget.tsx
@@ -3,5 +3,10 @@ import { useWidgetConfig } from '../../hooks/';
 
 export function Widget({ starterVariant }) {
   const widgetConfig = useWidgetConfig((starterVariant = { starterVariant }));
-  return <LiFiWidget config={widgetConfig} />;
+  return (
+    <LiFiWidget
+      config={widgetConfig}
+      integrator={import.meta.env.VITE_WIDGET_INTEGRATOR_STRING}
+    />
+  );
 }
