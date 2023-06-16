f1f550be6357bd6f7e89ff94a962f1b1542682bf
dennyscode <sander.bewerbung@gmx.de>
2023-06-16 11:29:08 +0200

    fix: markup

--- packages/dapp/src/components/StatsModal/StatsModal.style.ts
+++ packages/dapp/src/components/StatsModal/StatsModal.style.ts
@@ -2,6 +2,7 @@ import {
   AppBar,
   AppBarProps,
   Box,
+  BoxProps,
   Breakpoint,
   Grid,
   GridProps,
@@ -9,38 +10,36 @@ import {
 
 import { alpha, styled } from '@mui/material/styles';
 
-export interface ModalHeaderAppBarProps extends Omit<AppBarProps, 'component'> {
+export interface ModalContainerProps extends Omit<BoxProps, 'component'> {
   component?: string;
 }
 
-export const ModalContainer = styled(AppBar)<ModalHeaderAppBarProps>(
-  ({ theme }) => ({
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    margin: 'auto',
-    paddingBottom: theme.spacing(6),
-    borderRadius: '12px',
-    boxShadow:
-      theme.palette.mode === 'dark'
-        ? '0px 2px 4px rgba(0, 0, 0, 0.08), 0px 8px 16px rgba(0, 0, 0, 0.16)'
-        : '0px 2px 4px rgba(0, 0, 0, 0.08), 0px 8px 16px rgba(0, 0, 0, 0.08)',
-    width: `calc( 100% - ${theme.spacing(6)})`,
-    maxWidth: '640px',
-    maxHeight: '85%',
-    overflowY: 'auto',
-    background:
-      theme.palette.mode === 'dark'
-        ? theme.palette.surface2.main
-        : theme.palette.surface1.main,
+export const ModalContainer = styled(Box)<ModalContainerProps>(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  margin: 'auto',
+  paddingBottom: theme.spacing(6),
+  borderRadius: '12px',
+  boxShadow:
+    theme.palette.mode === 'dark'
+      ? '0px 2px 4px rgba(0, 0, 0, 0.08), 0px 8px 16px rgba(0, 0, 0, 0.16)'
+      : '0px 2px 4px rgba(0, 0, 0, 0.08), 0px 8px 16px rgba(0, 0, 0, 0.08)',
+  width: `calc( 100% - ${theme.spacing(6)})`,
+  maxWidth: '640px',
+  maxHeight: '85%',
+  overflowY: 'auto',
+  background:
+    theme.palette.mode === 'dark'
+      ? theme.palette.surface2.main
+      : theme.palette.surface1.main,
 
-    [theme.breakpoints.up('md' as Breakpoint)]: {
-      margin: 0,
-      width: '640px',
-    },
-  }),
-);
+  [theme.breakpoints.up('md' as Breakpoint)]: {
+    margin: 0,
+    width: '640px',
+  },
+}));
 
 export const ModalContent = styled(Grid)<GridProps>(({ theme }) => ({
   alignItems: 'center',
@@ -51,6 +50,10 @@ export const ModalContent = styled(Grid)<GridProps>(({ theme }) => ({
     justifyContent: 'inherit',
   },
 }));
+
+export interface ModalHeaderAppBarProps extends Omit<AppBarProps, 'component'> {
+  component?: string;
+}
 
 export const ModalHeaderAppBar = styled(AppBar)<ModalHeaderAppBarProps>(
   ({ theme }) => ({
