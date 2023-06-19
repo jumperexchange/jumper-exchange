1a2105c33f618ac2eac1655dc7a446a09b5fb074
dennyscode <sander.bewerbung@gmx.de>
2023-06-19 15:21:27 +0200

    feat: add active flag on FeatureCards

--- packages/dapp/src/components/FeatureCards/FeatureCard.tsx
+++ packages/dapp/src/components/FeatureCards/FeatureCard.tsx
@@ -23,100 +23,102 @@ export const FeatureCard = ({ data, loading, error }) => {
   }, [data]);
 
   return (
-    <Slide
-      direction="up"
-      in={
-        open &&
-        (!displayConditions.showOnce ||
-          !disabledFeatureCards.includes(displayConditions.id))
-      }
-      unmountOnExit
-      appear={false}
-      timeout={400}
-    >
-      <Card gradient={data?.gradientColor}>
-        <CardContent sx={{ padding: theme.spacing(6), position: 'relative' }}>
-          <IconButton
-            disableRipple={true}
-            sx={{
-              position: 'absolute',
-              right: theme.spacing(2),
-              top: theme.spacing(2),
-            }}
-            onClick={() => {
-              setOpen(false);
-              onDisableFeatureCard(
-                data?.displayConditions && data?.displayConditions[0]?.id,
-              );
-            }}
-          >
-            <CloseIcon
+    displayConditions.active && (
+      <Slide
+        direction="up"
+        in={
+          open &&
+          (!displayConditions.showOnce ||
+            !disabledFeatureCards.includes(displayConditions.id))
+        }
+        unmountOnExit
+        appear={false}
+        timeout={400}
+      >
+        <Card gradient={data?.gradientColor}>
+          <CardContent sx={{ padding: theme.spacing(6), position: 'relative' }}>
+            <IconButton
+              disableRipple={true}
               sx={{
-                width: '24px',
-                height: '24px',
-                color:
-                  theme.palette.mode === 'dark'
-                    ? theme.palette.white.main
-                    : theme.palette.black.main,
+                position: 'absolute',
+                right: theme.spacing(2),
+                top: theme.spacing(2),
               }}
-            />
-          </IconButton>
+              onClick={() => {
+                setOpen(false);
+                onDisableFeatureCard(
+                  data?.displayConditions && data?.displayConditions[0]?.id,
+                );
+              }}
+            >
+              <CloseIcon
+                sx={{
+                  width: '24px',
+                  height: '24px',
+                  color:
+                    theme.palette.mode === 'dark'
+                      ? theme.palette.white.main
+                      : theme.palette.black.main,
+                }}
+              />
+            </IconButton>
 
-          {loading && error ? (
-            'loading'
-          ) : (
-            <>
-              <Typography
-                variant={'lifiHeaderSmall'}
-                sx={{
-                  fontSize: '24px',
-                  lineHeight: '32px',
-                }}
-                gutterBottom
-              >
-                {data?.title}
-              </Typography>
-              <Typography
-                variant={'lifiBodySmall'}
-                sx={{
-                  lineHeight: '24px',
-                  width: '240px',
-                  height: '48px',
-                  overflow: 'hidden',
-                  textOverflow: 'ellipsis',
-                }}
-              >
-                {data?.subtitle}
-              </Typography>
-              <CardActions sx={{ padding: 0, marginTop: theme.spacing(2) }}>
-                <Link
-                  href={data?.url || 'https://li.fi'}
+            {loading && error ? (
+              'loading'
+            ) : (
+              <>
+                <Typography
+                  variant={'lifiHeaderSmall'}
                   sx={{
-                    textDecoration: 'none',
-                    color:
-                      theme.palette.mode === 'dark'
-                        ? theme.palette.accent1Alt.main
-                        : theme.palette.primary.main,
+                    fontSize: '24px',
+                    lineHeight: '32px',
+                  }}
+                  gutterBottom
+                >
+                  {data?.title}
+                </Typography>
+                <Typography
+                  variant={'lifiBodySmall'}
+                  sx={{
+                    lineHeight: '24px',
+                    width: '240px',
+                    height: '48px',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
                   }}
                 >
-                  <Typography variant="lifiBodySmallStrong">
-                    {translate(`${i18Path}learnMore`)}
-                  </Typography>
-                </Link>
-              </CardActions>
-              <CardImage
-                component="img"
-                src={
-                  theme.palette.mode === 'dark'
-                    ? data?.imageDarkMode.url
-                    : data?.imageLightMode.url
-                }
-                alt="Feature Card Image"
-              />
-            </>
-          )}
-        </CardContent>
-      </Card>
-    </Slide>
+                  {data?.subtitle}
+                </Typography>
+                <CardActions sx={{ padding: 0, marginTop: theme.spacing(2) }}>
+                  <Link
+                    href={data?.url || 'https://li.fi'}
+                    sx={{
+                      textDecoration: 'none',
+                      color:
+                        theme.palette.mode === 'dark'
+                          ? theme.palette.accent1Alt.main
+                          : theme.palette.primary.main,
+                    }}
+                  >
+                    <Typography variant="lifiBodySmallStrong">
+                      {translate(`${i18Path}learnMore`)}
+                    </Typography>
+                  </Link>
+                </CardActions>
+                <CardImage
+                  component="img"
+                  src={
+                    theme.palette.mode === 'dark'
+                      ? data?.imageDarkMode.url
+                      : data?.imageLightMode.url
+                  }
+                  alt="Feature Card Image"
+                />
+              </>
+            )}
+          </CardContent>
+        </Card>
+      </Slide>
+    )
   );
 };
