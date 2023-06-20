c77c91e694f25402c136f4380cb28eadaaee6d47
dennyscode <sander.bewerbung@gmx.de>
2023-06-20 11:29:32 +0200

    fix: add buildUrl

--- packages/dapp/src/components/Widget/Widget.tsx
+++ packages/dapp/src/components/Widget/Widget.tsx
@@ -140,6 +140,7 @@ export function Widget({ starterVariant }) {
           maxPriceImpact: 0.4,
         },
       },
+      buildUrl: true,
       insurance: true,
       integrator: import.meta.env.VITE_WIDGET_INTEGRATOR,
     };
