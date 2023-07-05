980ec1cad7abc0d375cb1d0e860169f1821fa94d
dennyscode <sander.bewerbung@gmx.de>
2023-07-05 16:17:32 +0200

    Revert "chore: remove localStorageKeyPrefix"
    This reverts commit 10fbc50aae96aca59c44ec448779aca9296ad48f.

--- packages/dapp/src/components/Widget/Widget.tsx
+++ packages/dapp/src/components/Widget/Widget.tsx
@@ -156,6 +156,7 @@ export function Widget({ starterVariant }) {
           },
         },
       },
+      localStorageKeyPrefix: `jumper-${starterVariant}`,
       sdkConfig: {
         apiUrl: import.meta.env.VITE_LIFI_API_URL,
         rpcs,
