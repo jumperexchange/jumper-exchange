47eae9759a9d484429e22ebaa982b51f1364deba
Abhishek Kumar <abhishekkumar35962@gmail.com>
2023-07-11 19:19:09 +0530

    fix: minor fix

--- packages/dapp/src/const/MenuConfig/useWalletSelectContent.tsx
+++ packages/dapp/src/const/MenuConfig/useWalletSelectContent.tsx
@@ -106,6 +106,7 @@ export const useWalletSelectContent = () => {
     });
     return _output;
   }, [
+    availableWallets.length,
     isCurrentMultisigEnvironment,
     login,
     onCloseAllNavbarMenus,
