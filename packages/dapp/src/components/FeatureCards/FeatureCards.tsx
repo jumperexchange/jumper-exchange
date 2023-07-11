d1dd273542ef3fee2da92f4ecc25060dc7010594
dennyscode <sander.bewerbung@gmx.de>
2023-07-11 14:08:18 +0200

    feat: show feature cards on large screens

--- packages/dapp/src/components/FeatureCards/FeatureCards.tsx
+++ packages/dapp/src/components/FeatureCards/FeatureCards.tsx
@@ -1,23 +1,28 @@
 import { useQuery } from '@apollo/client';
+import { Breakpoint, useMediaQuery, useTheme } from '@mui/material';
 import { getFeatureCards } from '../../graphql/queries/featureCards';
 import { FeatureCard } from './FeatureCard';
 import { FeatureCardsContainer } from './index';
 
 export const FeatureCards = () => {
   const { loading, error, data } = useQuery(getFeatureCards);
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg' as Breakpoint));
 
   return (
-    <FeatureCardsContainer>
-      {data?.featureCardCollection?.items.map((cardData, index) => {
-        return (
-          <FeatureCard
-            error={error}
-            loading={loading}
-            data={cardData}
-            key={`feature-card-${index}`}
-          />
-        );
-      })}
-    </FeatureCardsContainer>
+    isDesktop && (
+      <FeatureCardsContainer>
+        {data?.featureCardCollection?.items.map((cardData, index) => {
+          return (
+            <FeatureCard
+              error={error}
+              loading={loading}
+              data={cardData}
+              key={`feature-card-${index}`}
+            />
+          );
+        })}
+      </FeatureCardsContainer>
+    )
   );
 };
