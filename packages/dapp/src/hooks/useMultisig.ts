a37362ffe87f01c3be5819bf42a43210a06491a8
Abhishek Kumar <abhishekkumar35962@gmail.com>
2023-07-12 14:13:30 +0530

    fix: early return if not safe

--- packages/dapp/src/hooks/useMultisig.ts
+++ packages/dapp/src/hooks/useMultisig.ts
@@ -15,7 +15,12 @@ export const useMultisig = () => {
   const { destinationChain } = useMultisigStore();
 
   const checkMultisigEnvironment = async () => {
+    // in Multisig env, window.parent is not equal to window
     const isIframeEnvironment = window?.parent !== window;
+
+    if (!isIframeEnvironment) {
+      return false;
+    }
 
     const sdk = new SafeAppsSDK();
     const accountInfo = await Promise.race([
