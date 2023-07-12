e52148e90edc370bc66c27b2bd9c4c6e71af53cc
Abhishek Kumar <abhishekkumar35962@gmail.com>
2023-07-12 18:04:08 +0530

    fix: removed persist

--- packages/dapp/src/stores/multisig/MultisigStore.ts
+++ packages/dapp/src/stores/multisig/MultisigStore.ts
@@ -1,40 +1,30 @@
-import { MultisigProps, MultisigState } from '@transferto/shared';
-import type { StateCreator } from 'zustand';
+import { MultisigProps } from '@transferto/shared';
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 // ----------------------------------------------------------------------
 
 /*--  Use Zustand  --*/
 
-export const useMultisigStore = create(
-  persist(
-    (set) => ({
-      setValue: (key, value) =>
-        set(() => ({
-          [key]: value,
-        })),
-      setValues: (values) =>
-        set((state) => {
-          const updatedState: MultisigProps = { ...state };
-          for (const key in values) {
-            if (Object.hasOwn(state, key)) {
-              updatedState[key] = values[key];
-            }
-          }
-          return updatedState;
-        }),
-
-      // Destination Chain
-      onDestinationChainSelected: (chainId: number) => {
-        set({
-          destinationChain: chainId,
-        });
-      },
+export const useMultisigStore = create((set) => ({
+  setValue: (key, value) =>
+    set(() => ({
+      [key]: value,
+    })),
+  setValues: (values) =>
+    set((state) => {
+      const updatedState: MultisigProps = { ...state };
+      for (const key in values) {
+        if (Object.hasOwn(state, key)) {
+          updatedState[key] = values[key];
+        }
+      }
+      return updatedState;
     }),
-    {
-      name: 'jumper-multisig-store', // name of the item in the storage (must be unique)
-      // storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
-    },
-  ) as unknown as StateCreator<MultisigState, [], [], MultisigState>,
-);
+
+  // Destination Chain
+  onDestinationChainSelected: (chainId: number) => {
+    set({
+      destinationChain: chainId,
+    });
+  },
+}));
