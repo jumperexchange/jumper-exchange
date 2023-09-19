89a22f5be37a0c345200c6a83442dbe1fc5eba52
Denny Sander <sander.bewerbung@gmx.de>
2023-09-19 21:13:21 +0200

    fix: set type of structuredClone polyfill to any

--- packages/shared/src/utils/structuredClone.ts
+++ packages/shared/src/utils/structuredClone.ts
@@ -1,17 +1,10 @@
 // Attach the polyfill as a Global function
 import structuredClone from '@ungap/structured-clone';
-import type { TransferListItem } from 'worker_threads';
 
 if (!('structuredClone' in globalThis)) {
   globalThis.structuredClone = ((
     value: StructuredSerializeOptions | undefined,
     options?: { lossy?: boolean | undefined } | undefined,
   ): StructuredSerializeOptions | undefined =>
-    structuredClone(value, options)) as {
-    <T = any>(value: T, options?: StructuredSerializeOptions | undefined): T;
-    <T>(
-      value: T,
-      transfer?: { transfer: readonly TransferListItem[] } | undefined,
-    ): T;
-  };
+    structuredClone(value, options)) as any;
 }
