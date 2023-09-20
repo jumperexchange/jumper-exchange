90aa9acc585fbe7513f13d09763e432db25f4014
Denny Sander <sander.bewerbung@gmx.de>
2023-09-20 12:17:16 +0200

    refactor: cleanup exports

--- packages/shared/src/atoms/illustrations/index.ts
+++ packages/shared/src/atoms/illustrations/index.ts
@@ -1,2 +1,1 @@
 export * from './BrandLogo';
-export * from './Discord';
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,4 @@ export * from './config';
 export * from './organisms';
 export * from './theme';
 export * from './types';
+export * from './utils';
