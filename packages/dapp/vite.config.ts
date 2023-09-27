14de4bd2472881c87859bd21b785775e9b468d43
Abhishek Kumar <abhishekkumar35962@gmail.com>
2023-09-27 11:39:18 +0530

    fix: updated assets

--- packages/dapp/vite.config.ts
+++ packages/dapp/vite.config.ts
@@ -2,58 +2,9 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 import react from '@vitejs/plugin-react';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      includeAssets: ['favicon.ico'],
-      manifest: {
-        name: 'Jumper.Exchange',
-        short_name: 'Jumper.Exchange',
-        description: 'Multi-Chain Bridging & Swapping (powered by LI.FI)',
-        theme_color: '#653BA3',
-        // icons: [
-        //   {
-        //     src: '/favicon.png',
-        //     sizes: '144x144',
-        //     type: 'image/png',
-        //     purpose: 'any',
-        //   },
-        //   {
-        //     src: 'favicon.png',
-        //     sizes: '512x512',
-        //     type: 'image/png',
-        //   },
-        // ],
-        shortcuts: [
-          {
-            name: 'Swap/Bridge',
-            short_name: 'swap/bridge',
-            description: 'Swap/bridge via Jumper',
-            url: '/?source=pwa',
-          },
-          {
-            name: 'Gas',
-            short_name: 'Gas',
-            description: 'Gas via Jumper',
-            url: '/gas?source=pwa',
-          },
-          {
-            name: 'Buy',
-            short_name: 'Buy',
-            description: 'Buy on Jumper',
-            url: '/buy?source=pwa',
-          },
-        ],
-      },
-      // registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
+  plugins: [react()],
   esbuild: {
     target: 'esnext',
   },
