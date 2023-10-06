666f20ae37c26fa2be4f84e4026a808f2f4687bd
Denny Sander <sander.bewerbung@gmx.de>
2023-10-06 10:25:31 +0200

    fix: blockchain explorer hook

--- packages/dapp/src/hooks/useBlockchainExplorerURL.ts
+++ packages/dapp/src/hooks/useBlockchainExplorerURL.ts
@@ -5,13 +5,9 @@ export const useBlockchainExplorerURL = () => {
   const { account } = useWallet();
   const { getChainById } = useChains();
 
-  if (account.isActive && account.chainId) {
-    try {
-      return `${
-        getChainById(account.chainId).metamask.blockExplorerUrls[0]
-      }address/${account.address}`;
-    } catch {
-      throw Error('No blockchain-explorer found');
-    }
+  if (account?.address && account?.chainId) {
+    const chain = getChainById(account.chainId);
+    if (chain?.metamask)
+      return `${chain.metamask.blockExplorerUrls[0]}address/${account.address}`;
   }
 };
