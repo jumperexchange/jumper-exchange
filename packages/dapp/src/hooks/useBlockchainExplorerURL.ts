85e4be7616f32d4288546ae830bbbe9c6ef78639
Denny Sander <sander.bewerbung@gmx.de>
2023-10-09 13:06:50 +0200

    refactor: add else clause to catch error

--- packages/dapp/src/hooks/useBlockchainExplorerURL.ts
+++ packages/dapp/src/hooks/useBlockchainExplorerURL.ts
@@ -7,7 +7,10 @@ export const useBlockchainExplorerURL = () => {
 
   if (account?.address && account?.chainId) {
     const chain = getChainById(account.chainId);
-    if (chain?.metamask)
+    if (chain?.metamask) {
       return `${chain.metamask.blockExplorerUrls[0]}address/${account.address}`;
+    } else {
+      console.error(`No blockchain explorer found for ${account.chainId}`);
+    }
   }
 };
