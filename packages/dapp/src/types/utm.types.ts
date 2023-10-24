01eed7a932bb70068e9b8f52bafc027a9387c0ac
Denny Sander <sander.bewerbung@gmx.de>
2023-10-24 19:35:29 +0200

    refactor: type names

--- packages/dapp/src/types/utm.types.ts
+++ packages/dapp/src/types/utm.types.ts
@@ -1,14 +1,14 @@
-type UTM_Medium_Types = 'feature_card' | 'landing_page' | 'menu' | 'testnet';
+type UtmMedium = 'feature_card' | 'landing_page' | 'menu' | 'testnet';
 
-type UTM_Campaign_Types =
+type UtmCampaign =
   | 'jumper_to_lifi'
   | 'jumper_to_docs'
   | 'jumper_to_explorer'
   | 'testnet_to_jumper';
 
 export interface UtmParams {
-  utm_medium?: UTM_Medium_Types;
-  utm_campaign?: UTM_Campaign_Types;
+  utm_medium?: UtmMedium;
+  utm_campaign?: UtmCampaign;
   utm_term?: string;
   utm_content?: string;
 }
