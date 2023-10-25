537a84a01816634d06badf936133903d63cce8de
Denny Sander <sander.bewerbung@gmx.de>
2023-10-25 12:53:24 +0200

    refactor: utility function

--- packages/shared/src/utils/append-utm-params-to-link.ts
+++ packages/shared/src/utils/append-utm-params-to-link.ts
@@ -3,9 +3,10 @@ import type { UtmParams } from '@transferto/dapp/src/types';
 
 export function appendUTMParametersToLink(link: string, utm: UtmParams) {
   const searchParams = new URLSearchParams(
-    Object.entries({ ...{ utm_source: UTM_SOURCE }, ...utm }).map(
-      ([key, value]) => [key, String(value)],
-    ),
+    Object.entries({ utm_source: UTM_SOURCE, ...utm }).map(([key, value]) => [
+      key,
+      String(value),
+    ]),
   );
   const updatedLink = link.charAt(link.length - 1) === '/' ? link : `${link}/`;
   const updatedUrl = `${updatedLink}?${searchParams.toString()}`;
