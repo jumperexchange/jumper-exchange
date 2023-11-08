28afecc6446ac41cde58d437e14070bc7494748b
Addminus <adrianweniger@gmail.com>
2023-11-08 12:28:01 +0100

    feat: add hover effect

--- packages/dapp/src/components/Widgets/Widgets.style.tsx
+++ packages/dapp/src/components/Widgets/Widgets.style.tsx
@@ -6,17 +6,6 @@ export interface WidgetContainerProps extends Omit<BoxProps, 'component'> {
   welcomeScreenClosed: boolean;
 }
 
-const opacityMap = {
-  dark: {
-    true: 0.48, //hover
-    false: 0.24, //no-hover
-  },
-  light: {
-    true: 0.34, //hover
-    false: 0.12, //no-hover
-  },
-};
-
 export const WidgetContainer = styled(Box, {
   shouldForwardProp: (prop) => prop !== 'isActive',
 })<WidgetContainerProps>(({ theme, isActive, welcomeScreenClosed }) => ({
@@ -40,50 +29,54 @@ export const WidgetContainer = styled(Box, {
   //   paddingTop: welcomeScreenHover && theme.spacing(0),
   // },
 
+  // darken widget when welcome screen open
   // Background-Gradients
-  '& .widget-wrapper > div:before, & > .onramper-wrapper:after': {
-    content: !welcomeScreenClosed && '" "',
-    position: 'absolute',
-    left: 0,
-    zIndex: 900,
-    right: 0,
-    bottom: 0,
-
-    background:
-      theme.palette.mode === 'dark'
-        ? 'linear-gradient(180deg, transparent 0%, #000000 50%, #000000 100%)'
-        : 'linear-gradient(180deg, transparent 0%, #ffffff 50%, #ffffff 100%)',
-
-    opacity: 0.5,
-    margin: 'auto',
-    transitionProperty: 'opacity, top, padding-top',
-    transitionDuration: '.3s',
-    transitionTimingFunction: 'ease-in-out',
-    borderTopRightRadius: '12px',
-    borderTopLeftRadius: '12px',
-  },
-
-  // '& > .widget-wrapper:before, & > .onramper-wrapper:before': {
+  // '& .widget-wrapper > div:before, & > .onramper-wrapper:after': {
   //   content: !welcomeScreenClosed && '" "',
-  //   transitionProperty: 'top, opacity',
-  //   transitionDuration: '.4s',
-  //   transitionTimingFunction: 'ease-in-out',
+  //   position: 'absolute',
+  //   left: 0,
+  //   zIndex: 900,
+  //   right: 0,
+  //   bottom: 0,
+
   //   background:
   //     theme.palette.mode === 'dark'
-  //       ? 'radial-gradient(50% 50% at 50% 50%, #6600FF 0%, rgba(255, 255, 255, 0) 100%);'
-  //       : 'radial-gradient(50% 50% at 50% 50%, #8700B8 0%, rgba(255, 255, 255, 0) 100%);',
-  //   position: 'absolute',
-  //   zIndex: -1,
-  //   pointerEvents: 'none',
-  //   width: '1080px',
-  //   height: '1080px',
-  //   maxWidth: '90vw',
-  //   maxHeight: '90vh',
-  //   transform: 'translate(-50%, -50%)',
-  //   left: '50%',
-  //   top: '50%',
-  //   opacity: welcomeScreenHover
-  //     ? opacityMap[theme.palette.mode]['true']
-  //     : opacityMap[theme.palette.mode]['false'],
+  //       ? 'linear-gradient(180deg, transparent 0%, #000000 50%, #000000 100%)'
+  //       : 'linear-gradient(180deg, transparent 0%, #ffffff 50%, #ffffff 100%)',
+
+  //   opacity: 0.5,
+  //   margin: 'auto',
+  //   transitionProperty: 'opacity, top, padding-top',
+  //   transitionDuration: '.3s',
+  //   transitionTimingFunction: 'ease-in-out',
+  //   borderTopRightRadius: '12px',
+  //   borderTopLeftRadius: '12px',
   // },
+
+  // radial shadow that animates on hover
+  '& > .widget-wrapper:before, & > .onramper-wrapper:before': {
+    content: !welcomeScreenClosed && '" "',
+    transitionProperty: 'top, opacity',
+    transitionDuration: '.4s',
+    transitionTimingFunction: 'ease-in-out',
+    background:
+      theme.palette.mode === 'dark'
+        ? 'radial-gradient(50% 50% at 50% 50%, #6600FF 0%, rgba(255, 255, 255, 0) 100%);'
+        : 'radial-gradient(50% 50% at 50% 50%, #8700B8 0%, rgba(255, 255, 255, 0) 100%);',
+    position: 'absolute',
+    zIndex: -1,
+    pointerEvents: 'none',
+    width: '1080px',
+    height: '1080px',
+    maxWidth: '90vw',
+    maxHeight: '90vh',
+    transform: 'translate(-50%, -50%)',
+    left: '50%',
+    top: '50%',
+    opacity: theme.palette.mode === 'dark' ? 0.24 : 0.12,
+  },
+  '& > .widget-wrapper:hover:before, & > .onramper-wrapper:hover:before': {
+    opacity: theme.palette.mode === 'dark' ? 0.48 : 0.34,
+    top: '45%',
+  },
 }));
