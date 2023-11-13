f6949b5ecb7c65ce827c02adfbfd6acee2d8a755
dennyscode <43956540+dennyscode@users.noreply.github.com>
2023-11-13 14:20:20 +0100

    chore: upgrade to react-query v5 (#714)

--- packages/dapp/src/hooks/useFetchDexsAndBridges.tsx
+++ packages/dapp/src/hooks/useFetchDexsAndBridges.tsx
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 
 export const useFetchDexsAndBridges = () => {
-  const { data, isLoading, error } = useQuery(['tools'], async () => {
-    const apiUrl = import.meta.env.VITE_LIFI_API_URL;
-    const response = await fetch(`${apiUrl}/tools`);
-    const result = await response.json();
-    return result;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['tools'],
+    queryFn: async () => {
+      const apiUrl = import.meta.env.VITE_LIFI_API_URL;
+      const response = await fetch(`${apiUrl}/tools`);
+      const result = await response.json();
+      return result;
+    },
   });
 
   return {
