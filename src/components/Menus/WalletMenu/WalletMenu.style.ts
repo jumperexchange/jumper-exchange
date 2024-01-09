64da3938ab02c8f51729f4b8946280c056722b1d
Denny San <sander.bewerbung@gmx.de>
2024-01-09 11:36:15 +0100

    style: fix avatar

--- src/components/Menus/WalletMenu/WalletMenu.style.ts
+++ src/components/Menus/WalletMenu/WalletMenu.style.ts
@@ -2,15 +2,11 @@ import { Avatar } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 export const WalletAvatar = styled(Avatar)(({ theme }) => ({
-  padding: theme.spacing(2.25),
+  padding: theme.spacing(1.5),
   background: '#F7F9FB', //todo: proper color-name
   margin: 'auto',
   height: '96px',
   width: '96px',
-  '> img': {
-    height: '58px',
-    width: '58px',
-  },
 }));
 
 export const ChainAvatar = styled(Avatar)(({ theme }) => ({
