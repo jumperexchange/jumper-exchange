c004f64c28edc8e09d3a57de988d143040416ecb
Addminus <adrianweniger@gmail.com>
2024-01-17 17:10:49 +0100

    chore: cleanup

--- src/components/Menus/WalletMenu/WalletCard.tsx
+++ src/components/Menus/WalletMenu/WalletCard.tsx
@@ -1,4 +1,4 @@
-import { Container, Stack, Typography, useTheme } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import {
