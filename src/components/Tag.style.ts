7d216c5c868bfd3981830ea3fa8e3cb6aadaa4a8
Denny San <sander.bewerbung@gmx.de>
2024-01-29 09:11:30 +0100

    style: tags

--- src/components/Tag.style.ts
+++ src/components/Tag.style.ts
@@ -1,5 +1,5 @@
 import type { TypographyProps } from '@mui/material';
-import { Typography, styled } from '@mui/material';
+import { Typography, keyframes, styled } from '@mui/material';
 
 export interface TagProps extends Omit<TypographyProps, 'component'> {
   component?: keyof JSX.IntrinsicElements;
@@ -7,6 +7,11 @@ export interface TagProps extends Omit<TypographyProps, 'component'> {
   color?: string;
 }
 
+const moveGradient = keyframes`
+50% {
+  background-position: 100% 50%;
+}
+`;
 export const Tag = styled(Typography, {
   shouldForwardProp: (prop) => prop !== 'color' && prop !== 'backgroundColor',
 })<TagProps>(({ theme, backgroundColor, color }) => ({
@@ -19,10 +24,15 @@ export const Tag = styled(Typography, {
     : theme.palette.mode === 'light'
       ? theme.palette.secondary.main
       : theme.palette.accent1Alt.main,
+  boxShadow:
+    theme.palette.mode === 'dark'
+      ? '0px 2px 4px rgba(0, 0, 0, 0.04), 0px 8px 16px rgba(0, 0, 0, 0.08)'
+      : '0px 2px 4px rgba(0, 0, 0, 0.04), 0px 8px 16px rgba(0, 0, 0, 0.04)',
+
   color: color
     ? color
     : theme.palette.mode === 'light'
-      ? theme.palette.primary.main
+      ? theme.palette.grey[700]
       : theme.palette.white.main,
   userSelect: 'none',
   borderRadius: '24px',
@@ -37,5 +47,37 @@ export const Tag = styled(Typography, {
   '&:before': {
     content: '"#"',
     marginRight: theme.spacing(0.5),
+  },
+
+  '--border-width': '1px',
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textTransform: 'uppercase',
+  background: theme.palette.surface1.main,
+
+  '&:after': {
+    position: 'absolute',
+    borderRadius: '20px',
+    content: '""',
+    top: 'calc(-1 * var(--border-width))',
+    left: 'calc(-1 * var(--border-width))',
+    zIndex: '-1',
+    width: 'calc(100% + var(--border-width) * 2)',
+    height: 'calc(100% + var(--border-width) * 2)',
+    background: `linear-gradient(
+        60deg,
+        hsl(277, 72%, 94%),
+        hsl(244, 100%, 94%),
+        hsl(259, 92%, 95%),
+        hsl(295, 57%, 92%),
+        hsl(253, 100%, 95%),
+        hsl(264, 47%, 88%),
+        hsl(264, 47%, 98%)
+      )`,
+    backgroundSize: '300% 300%',
+    backgroundPosition: '0 50%',
+    animation: `${moveGradient} 4s alternate infinite`,
   },
 }));
