fccbf1a43c69ea92330388d145916c3ec680b7f0
Denny San <sander.bewerbung@gmx.de>
2024-01-30 15:56:39 +0100

    refactor: split accordion into components

--- src/components/AccordionFAQ/AccordionFAQ.style.ts
+++ src/components/AccordionFAQ/AccordionFAQ.style.ts
@@ -1,24 +1,57 @@
-import { Container, type Breakpoint } from '@mui/material';
+import type { AccordionProps as MuiAccordionProps } from '@mui/material';
+import {
+  Container,
+  Accordion as MuiAccordion,
+  type Breakpoint,
+} from '@mui/material';
 
 import { styled } from '@mui/material/styles';
 
 export const AccordionContainer = styled(Container)(({ theme }) => ({
-  flexShrink: 0,
-  width: '100%',
-  border: 'unset',
-  padding: theme.spacing(1),
-  borderRadius: '24px',
-  background: 'transparent',
-  transition: 'background-color 250ms',
-  [theme.breakpoints.up('sm' as Breakpoint)]: {
-    minWidth: 250,
-    width: 420,
+  margin: 'auto',
+  marginTop: theme.spacing(4),
+  padding: theme.spacing(1, 2),
+  borderRadius: '8px',
+  background: theme.palette.surface1.main,
+  position: 'relative',
+  maxWidth: theme.breakpoints.values.md,
+  width: '100% !important',
+  boxShadow:
+    theme.palette.mode === 'dark'
+      ? '0px 2px 4px rgba(0, 0, 0, 0.04), 0px 8px 16px rgba(0, 0, 0, 0.08)'
+      : '0px 2px 4px rgba(0, 0, 0, 0.04), 0px 8px 16px rgba(0, 0, 0, 0.04)',
+
+  '&:before': {
+    content: '" "',
+    zIndex: '-1',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    background: 'linear-gradient(to top left, #fff 0%, #fff 100% )',
+    // transform: 'translate3d(0px, 20px, 0) scale(0.95)',
+    filter: 'blur(20px)',
+    opacity: 'var(0.7)',
+    transition: 'opacity 0.3s',
   },
-  '&:hover': {
-    cursor: 'pointer',
-    background:
-      theme.palette.mode === 'light'
-        ? theme.palette.alphaDark100.main
-        : theme.palette.alphaLight400.main,
+  [theme.breakpoints.up('sm' as Breakpoint)]: {
+    width: theme.breakpoints.values.md,
+    maxWidth: theme.breakpoints.values.md,
+  },
+}));
+
+export interface AccordionProps extends Omit<MuiAccordionProps, 'component'> {
+  show?: boolean;
+}
+
+export const Accordion = styled(MuiAccordion, {
+  shouldForwardProp: (prop) => prop !== 'backgroundImageUrl',
+})<AccordionProps>(({ theme, show }) => ({
+  background: 'transparent',
+  visibility: show ? 'visible' : 'hidden',
+  height: show ? 'auto' : 0,
+  '&:last-of-type': {
+    marginBottom: show ? theme.spacing(2) : 0,
   },
 }));
--- src/components/AccordionFAQ/AccordionFAQ.tsx
+++ src/components/AccordionFAQ/AccordionFAQ.tsx
@@ -1,14 +1,16 @@
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import type { Breakpoint } from '@mui/material';
-import { Box, Container, Divider, IconButton, useTheme } from '@mui/material';
-import Accordion from '@mui/material/Accordion';
+import { Box, Divider, IconButton, useTheme } from '@mui/material';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { QAJsonSchema } from 'src/components';
+import {
+  Accordion,
+  AccordionContainer as Container,
+  QAJsonSchema,
+} from 'src/components';
 import type { FaqMeta } from 'src/types';
 
 interface AccordionFAQProps {
@@ -24,41 +26,7 @@ export const AccordionFAQ = ({ content }: AccordionFAQProps) => {
   };
 
   return !!content?.length ? (
-    <Container
-      sx={{
-        margin: 'auto',
-        marginTop: theme.spacing(4),
-        padding: theme.spacing(1, 2),
-        borderRadius: '8px',
-        background: theme.palette.surface1.main,
-        position: 'relative',
-        maxWidth: theme.breakpoints.values.md,
-        width: '100% !important',
-        boxShadow:
-          theme.palette.mode === 'dark'
-            ? '0px 2px 4px rgba(0, 0, 0, 0.04), 0px 8px 16px rgba(0, 0, 0, 0.08)'
-            : '0px 2px 4px rgba(0, 0, 0, 0.04), 0px 8px 16px rgba(0, 0, 0, 0.04)',
-
-        '&:before': {
-          content: '" "',
-          zIndex: '-1',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          background: 'linear-gradient(to top left, #fff 0%, #fff 100% )',
-          // transform: 'translate3d(0px, 20px, 0) scale(0.95)',
-          filter: 'blur(20px)',
-          opacity: 'var(0.7)',
-          transition: 'opacity 0.3s',
-        },
-        [theme.breakpoints.up('sm' as Breakpoint)]: {
-          width: theme.breakpoints.values.md,
-          maxWidth: theme.breakpoints.values.md,
-        },
-      }}
-    >
+    <Container>
       <Box
         sx={{
           display: 'flex',
@@ -85,17 +53,7 @@ export const AccordionFAQ = ({ content }: AccordionFAQProps) => {
         </IconButton>
       </Box>
       {content?.map((el: FaqMeta, index: number) => (
-        <Accordion
-          key={`faq-item-${index}`}
-          sx={{
-            background: 'transparent',
-            visibility: show ? 'visible' : 'hidden',
-            height: show ? 'auto' : 0,
-            '&:last-of-type': {
-              marginBottom: show ? theme.spacing(2) : 0,
-            },
-          }}
-        >
+        <Accordion key={`faq-item-${index}`}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls={`panel${index}a-content`}
