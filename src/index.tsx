d919c857fbecdc27a65d9103b4f8cf928e634683
Thomas Cherret <b00705488@essec.edu>
2024-02-07 18:05:00 +0100

    automatic push

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,28 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
 root.render(
-  <StrictMode>
+<StrictMode>
+    <div
+      style={{
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        width: '100%',
+        textAlign: 'center',
+        padding: '10px',
+        background: 'rgb(101 0 254 / 10%)',
+        fontWeight: 700,
+      }}
+    >
+      we don't take a fee mfkers{' '}
+      <img
+        alt=""
+        width={24}
+        height={24}
+        style={{ marginLeft: 8 }}
+        src="https://cdn.discordapp.com/emojis/911220159967019068.webp?size=96&quality=lossless"
+      />
+    </div>
     <App />
   </StrictMode>,
 );
