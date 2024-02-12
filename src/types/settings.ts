909f52b0dc78db00fdfc3e5e1c689963aa0b21df
Thomas Cherret <b00705488@essec.edu>
2024-02-12 15:42:15 +0100

    feat: create personalised feature cards for identified users

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------
 
-import type { FeatureCardData, LanguageKey } from 'src/types';
+import type { LanguageKey } from 'src/types';
 
 export type ThemeModesSupported = 'light' | 'dark' | 'auto';
 export type WalletConnected = string;
@@ -11,7 +11,6 @@ export interface SettingsProps {
   activeWalletName: WalletConnected;
   clientWallets: string[];
   disabledFeatureCards: string[];
-  featureCards: FeatureCardData[];
   welcomeScreenClosed: boolean;
   [key: string]: any;
 }
@@ -34,9 +33,6 @@ export interface SettingsState extends SettingsProps {
 
   // Disable Feature Cards
   onDisableFeatureCard: (id: string) => void;
-
-  // Personnalised Feature Cards
-  onUpdateFeatureCards: (cards: FeatureCardData[]) => void;
 
   // Reset
   onResetSetting: VoidFunction;
