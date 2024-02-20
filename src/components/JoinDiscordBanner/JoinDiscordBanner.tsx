dba4460d7d07846eee72e1f8a45d72247cbeffac
Denny San <sander.bewerbung@gmx.de>
2024-02-20 15:49:11 +0100

    chore: update discord cta title

--- src/components/JoinDiscordBanner/JoinDiscordBanner.tsx
+++ src/components/JoinDiscordBanner/JoinDiscordBanner.tsx
@@ -6,8 +6,6 @@ import { useUserTracking } from 'src/hooks';
 import { EventTrackingTool } from 'src/types';
 import { openInNewTab } from 'src/utils';
 import { DiscordBanner } from '.';
-import { Button } from '../Button';
-import { Discord } from '../illustrations';
 
 export const JoinDiscordBanner = () => {
   const theme = useTheme();
@@ -48,26 +46,6 @@ export const JoinDiscordBanner = () => {
         >
           {t('discordBanner.ctaHeadline')}
         </Typography>
-        <Button
-          onClick={handleClick}
-          styles={{
-            padding: theme.spacing(0, 4.5),
-            margin: 'auto',
-            marginTop: theme.spacing(4),
-          }}
-          variant="primary"
-        >
-          <Discord
-            color={
-              theme.palette.mode === 'light'
-                ? theme.palette.white.main
-                : theme.palette.grey[100]
-            }
-          />
-          <Typography marginLeft={theme.spacing(0.5)}>
-            {t('discordBanner.ctaButton')}
-          </Typography>
-        </Button>
       </Box>
     </DiscordBanner>
   );
