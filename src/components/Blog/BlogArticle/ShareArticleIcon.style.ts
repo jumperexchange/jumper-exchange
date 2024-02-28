86e0af590f113a0fede7419b5105a9366321c146
Denny San <sander.bewerbung@gmx.de>
2024-02-28 17:11:07 +0100

    refactor: remove comment

--- src/components/Blog/BlogArticle/ShareArticleIcon.style.ts
+++ src/components/Blog/BlogArticle/ShareArticleIcon.style.ts
@@ -12,20 +12,4 @@ export const ShareButton = styled(IconButtonTertiary, {
   marginLeft: theme.spacing(1.5),
   width: expanded ? 'auto' : '48px',
   ...(expanded && { borderRadius: '24px' }),
-  // height: '48px',
-  // transition: 'background-color 0.3s',
-  // color:
-  //   theme.palette.mode === 'light'
-  //     ? theme.palette.grey[800]
-  //     : theme.palette.grey[300],
-  // backgroundColor:
-  //   theme.palette.mode === 'light'
-  //     ? theme.palette.alphaDark100.main
-  //     : theme.palette.alphaLight300.main,
-  // '&:hover': {
-  //   backgroundColor:
-  //     theme.palette.mode === 'light'
-  //       ? theme.palette.alphaDark300.main
-  //       : theme.palette.alphaLight500.main,
-  // },
 }));
