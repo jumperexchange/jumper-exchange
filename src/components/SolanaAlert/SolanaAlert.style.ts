22d968fecd2d990b34e72481886bdaacc808b6af
Denny San <sander.bewerbung@gmx.de>
2024-03-01 23:17:27 +0100

    style: center alert

--- src/components/SolanaAlert/SolanaAlert.style.ts
+++ src/components/SolanaAlert/SolanaAlert.style.ts
@@ -1,4 +1,4 @@
-import type { BoxProps } from '@mui/material';
+import type { BoxProps, Breakpoint } from '@mui/material';
 import { Box } from '@mui/material';
 
 import { styled } from '@mui/material/styles';
@@ -6,6 +6,7 @@ import { InfoMessageCard } from '../MessageCard';
 
 export const SolanaAlertContainer = styled(Box)<BoxProps>(({ theme }) => ({
   display: 'flex',
+  width: '100%',
   flexDirection: 'row',
   justifyContent: 'flex-start',
   position: 'fixed',
@@ -13,9 +14,14 @@ export const SolanaAlertContainer = styled(Box)<BoxProps>(({ theme }) => ({
   padding: theme.spacing(1.5),
   bottom: 0,
   zIndex: 2,
+  [theme.breakpoints.up('sm' as Breakpoint)]: {
+    width: 'auto',
+  },
 }));
 
 export const SolanaMessageCard = styled(InfoMessageCard)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'light' ? '#EBF3FF' : '#00317A', //todo: add to theme
-  width: 384,
+  [theme.breakpoints.up('sm' as Breakpoint)]: {
+    width: 384,
+  },
 }));
