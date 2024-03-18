176d848e8aa109c7f512d0696772aeddb9e82eda
Denny San <sander.bewerbung@gmx.de>
2024-03-18 13:33:06 +0100

    style: cleanup layout for loyalty-pass page

--- src/app/ui/loyalty-pass/LoyaltyPassPage.tsx
+++ src/app/ui/loyalty-pass/LoyaltyPassPage.tsx
@@ -1,12 +1,7 @@
 'use client';
 import { ProfilePage } from '@/components/ProfilePage';
 import '@/i18n/i18next-init-client';
-import { Layout } from 'src/Layout';
 
 export const LoyaltyPassPage = () => {
-  return (
-    <Layout navbarPageReload={true}>
-      <ProfilePage />
-    </Layout>
-  );
+  return <ProfilePage />;
 };
