85543ffeeed34c8e8a62423750e5b2a9243907f0
Thomas Cherret <b00705488@essec.edu>
2024-03-21 15:09:45 +0100

    chore: clean solana alert

--- src/components/Alerts/SolanaAlert.tsx
+++ src/components/Alerts/SolanaAlert.tsx
@@ -6,10 +6,19 @@ import { useChainTokenSelectionStore } from 'src/stores';
 
 export const SolanaAlert = () => {
   const { t } = useTranslation();
+  const { sourceChainToken, destinationChainToken } =
+    useChainTokenSelectionStore();
+
+  const solanaSelected = useMemo(() => {
+    const isSelected =
+      sourceChainToken?.chainId === ChainId.SOL ||
+      destinationChainToken?.chainId === ChainId.SOL;
+    return isSelected;
+  }, [destinationChainToken, sourceChainToken]);
 
   return (
     <InfoAlert
-      active={true}
+      active={solanaSelected}
       title={t('solanaAlert.title')}
       subtitle={t('solanaAlert.subtitle')}
     />
