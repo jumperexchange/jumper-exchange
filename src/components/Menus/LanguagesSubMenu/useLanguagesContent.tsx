deaaa1e9b0f3e9579b25e33428ea2af07a2c1849
Denny San <sander.bewerbung@gmx.de>
2024-03-21 15:13:14 +0100

    feat: add url replacement for locales on client side

--- src/components/Menus/LanguagesSubMenu/useLanguagesContent.tsx
+++ src/components/Menus/LanguagesSubMenu/useLanguagesContent.tsx
@@ -10,16 +10,14 @@ import * as supportedLanguages from '@/i18n/translations';
 import { useClientTranslation } from '@/i18n/useClientTranslation';
 import type { LanguageKey } from '@/types/i18n';
 import { EventTrackingTool } from '@/types/userTracking';
+import { replaceLocaleInUrl } from '@/utils/replaceLocaleInUrl';
+import { usePathname } from 'next/navigation';
 
 export const useLanguagesContent = () => {
-  // const { locale, asPath, pathname } = nextRouter();
-  // const router = useRouter();
+  const pathname = usePathname();
   const { i18n } = useClientTranslation();
   const { trackEvent } = useUserTracking();
   const handleSwitchLanguage = (newLanguage: LanguageKey) => {
-    // router.push(pathname, asPath, { locale: newLanguage });
-    i18n.changeLanguage(newLanguage);
-
     trackEvent({
       category: TrackingCategory.LanguageMenu,
       action: TrackingAction.SwitchLanguage,
@@ -27,6 +25,8 @@ export const useLanguagesContent = () => {
       data: { [TrackingEventParameter.SwitchedLanguage]: newLanguage },
       disableTrackingTool: [EventTrackingTool.ARCx, EventTrackingTool.Cookie3],
     });
+    i18n.changeLanguage(newLanguage);
+    replaceLocaleInUrl(pathname, newLanguage);
   };
 
   const languages = Object.entries(supportedLanguages)
