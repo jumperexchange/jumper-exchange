ea66b83df2d38f0b756881dc18abdcbcd968855b
Denny San <sander.bewerbung@gmx.de>
2024-04-08 11:57:27 +0200

    fix: error page

--- src/app/ui/error/ErrorPage.tsx
+++ src/app/ui/error/ErrorPage.tsx
@@ -1,9 +1,6 @@
 import { Button } from '@/components/Button/Button';
-import { LogoLink } from '@/components/Navbar';
-import { JumperLogo } from '@/components/illustrations/JumperLogo';
 import { useTheme } from '@mui/material';
 
-import { Logo } from '@/components/Navbar/Logo/Logo';
 import { Discord } from '@/components/illustrations/Discord';
 import { useAccounts } from '@/hooks/useAccounts';
 import { useUserTracking } from '@/hooks/userTracking/useUserTracking';
@@ -21,7 +18,6 @@ import { useTranslation } from 'react-i18next';
 import {
   CenteredContainer,
   ErrorMessage,
-  NavbarContainer,
   SupportMessage,
 } from './ErrorPage.style';
 
@@ -35,73 +31,62 @@ const ErrorPage = ({ reset }: FallbackErrorProps) => {
   const { t } = useTranslation();
   const { account } = useAccounts();
   return (
-    <>
-      <NavbarContainer>
-        <LogoLink>
-          <Logo
-            isConnected={!!account?.address}
-            theme={theme}
-            logo={<JumperLogo />}
-          />
-        </LogoLink>
-      </NavbarContainer>
-      <CenteredContainer>
-        <ErrorMessage variant={'lifiBodyLarge'}>
-          {t('error.message')}
-        </ErrorMessage>
-        <Button
-          variant="primary"
-          onClick={() => {
-            trackEvent({
-              category: TrackingCategory.Menu,
-              label: 'click-discord-link',
-              action: TrackingAction.OpenMenu,
-              data: { [TrackingEventParameter.Menu]: 'lifi_discord' },
-              disableTrackingTool: [
-                EventTrackingTool.ARCx,
-                EventTrackingTool.Cookie3,
-              ],
-            });
-            trackPageload({
-              source: TrackingCategory.Menu,
-              destination: 'discord-lifi',
-              url: DISCORD_URL,
-              pageload: true,
-              disableTrackingTool: [EventTrackingTool.Cookie3],
-            });
-            openInNewTab(DISCORD_URL);
-          }}
-          styles={{
-            width: 'auto',
-            margin: theme.spacing(1.5),
-            gap: '8px',
-            borderRadius: '24px',
-            padding: theme.spacing(1),
-            '> button:hover': {
-              backgroundColor: getContrastAlphaColor(theme, '4%'),
-            },
-            '> button:hover svg': {
-              fill:
-                theme.palette.mode === 'light'
-                  ? theme.palette.grey[700]
-                  : theme.palette.grey[300],
-            },
-          }}
-          fullWidth={true}
-        >
-          <Discord
-            color={
-              theme.palette.mode === 'dark'
-                ? theme.palette.white.main
-                : theme.palette.black.main
-            }
-          />
-          <SupportMessage variant="lifiBodyMediumStrong" component="span">
-            {t('navbar.navbarMenu.support')}
-          </SupportMessage>
-        </Button>
-      </CenteredContainer>
-    </>
+    <CenteredContainer>
+      <ErrorMessage variant={'lifiBodyLarge'}>
+        {t('error.message')}
+      </ErrorMessage>
+      <Button
+        variant="primary"
+        onClick={() => {
+          trackEvent({
+            category: TrackingCategory.Menu,
+            label: 'click-discord-link',
+            action: TrackingAction.OpenMenu,
+            data: { [TrackingEventParameter.Menu]: 'lifi_discord' },
+            disableTrackingTool: [
+              EventTrackingTool.ARCx,
+              EventTrackingTool.Cookie3,
+            ],
+          });
+          trackPageload({
+            source: TrackingCategory.Menu,
+            destination: 'discord-lifi',
+            url: DISCORD_URL,
+            pageload: true,
+            disableTrackingTool: [EventTrackingTool.Cookie3],
+          });
+          openInNewTab(DISCORD_URL);
+        }}
+        styles={{
+          width: 'auto',
+          margin: theme.spacing(1.5),
+          gap: '8px',
+          borderRadius: '24px',
+          padding: theme.spacing(1),
+          '> button:hover': {
+            backgroundColor: getContrastAlphaColor(theme, '4%'),
+          },
+          '> button:hover svg': {
+            fill:
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[700]
+                : theme.palette.grey[300],
+          },
+        }}
+        fullWidth={true}
+      >
+        <Discord
+          color={
+            theme.palette.mode === 'dark'
+              ? theme.palette.white.main
+              : theme.palette.black.main
+          }
+        />
+        <SupportMessage variant="lifiBodyMediumStrong" component="span">
+          {t('navbar.navbarMenu.support')}
+        </SupportMessage>
+      </Button>
+    </CenteredContainer>
   );
 };
 
