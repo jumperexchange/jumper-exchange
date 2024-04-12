cc550a56bc61b47b1c46b9642c0a7d71abb5d324
Denny San <sander.bewerbung@gmx.de>
2024-04-12 15:01:17 +0200

    refactor: add tracking provider

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -4,8 +4,8 @@ import { cookie3Analytics } from '@cookie3/analytics';
 import type { Resource } from 'i18next';
 import { type PropsWithChildren } from 'react';
 import { Layout } from 'src/Layout';
-import { UserTracking } from 'src/UserTracking';
 import { defaultNS } from 'src/i18n';
+import { TrackingProvider } from './TrackingProvider';
 import TranslationsProvider from './TranslationProvider';
 
 interface AppProviderProps {
@@ -23,18 +23,15 @@ export const AppProvider: React.FC<PropsWithChildren<AppProviderProps>> = ({
 
   return (
     <ThemeProvider>
-      <UserTracking />
-      {/* <ArcxAnalyticsProvider apiKey={`${process.env.NEXT_PUBLIC_ARCX_API_KEY}`}> */}
-      {/* <Cookie3Provider value={analytics}> */}
-      <TranslationsProvider
-        namespaces={[defaultNS]}
-        locale={lang}
-        resources={i18nResources}
-      >
-        <Layout>{children}</Layout>
-      </TranslationsProvider>
-      {/* </Cookie3Provider> */}
-      {/* </ArcxAnalyticsProvider> */}
+      <TrackingProvider>
+        <TranslationsProvider
+          namespaces={[defaultNS]}
+          locale={lang}
+          resources={i18nResources}
+        >
+          <Layout>{children}</Layout>
+        </TranslationsProvider>
+      </TrackingProvider>
     </ThemeProvider>
   );
 };
---
+++ src/providers/TrackingProvider.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { ArcxAnalyticsProvider } from '@arcxmoney/analytics';
+import { cookie3Analytics } from '@cookie3/analytics';
+import type { PropsWithChildren } from 'react';
+import { UserTracking } from 'src/UserTracking';
+import { cookie3Config } from 'src/const/cookie3';
+import { Cookie3Provider } from './Cookie3Provider';
+
+export const TrackingProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const analytics = cookie3Analytics(cookie3Config);
+
+  return (
+    <>
+      <UserTracking />
+      <ArcxAnalyticsProvider apiKey={process.env.NEXT_PUBLIC_ARCX_API_KEY}>
+        <Cookie3Provider value={analytics}>{children}</Cookie3Provider>
+      </ArcxAnalyticsProvider>
+    </>
+  );
+};
