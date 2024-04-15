9fcd7c7b904fe49d29b133de4e6d856cdc657032
Denny San <sander.bewerbung@gmx.de>
2024-04-15 16:33:03 +0200

    fix: moving powered by footer on app

--- src/components/Widgets/WidgetSkeleton.tsx
+++ src/components/Widgets/WidgetSkeleton.tsx
@@ -25,54 +25,56 @@ export const WidgetSkeleton = ({
   buttonTitle,
 }: WidgetSkeletonProps) => {
   return (
-    <WidgetSkeletonContainer welcomeScreenClosed={welcomeScreenClosed}>
-      <WidgetSkeletonHeader>
-        <Skeleton variant="text" width={164} height={40} />
-        {/* title could be here */}
-        {/* <WidgetSkeletonTitle>
+    <Box height="682px">
+      <WidgetSkeletonContainer welcomeScreenClosed={welcomeScreenClosed}>
+        <WidgetSkeletonHeader>
+          <Skeleton variant="text" width={164} height={40} />
+          {/* title could be here */}
+          {/* <WidgetSkeletonTitle>
           {title}
         </WidgetSkeletonTitle> */}
-        <Box
-          display="flex"
-          gap="4px"
-          sx={{
-            'div:last-of-type': {
-              marginRight: '-10px',
-            },
-          }}
-        >
-          <WidgetSkeletonIconButton>
-            <ReceiptLongIcon />
-          </WidgetSkeletonIconButton>
-          <WidgetSkeletonIconButton>
-            <SettingsIcon />
-          </WidgetSkeletonIconButton>
-        </Box>
-      </WidgetSkeletonHeader>
-      <WidgetSkeletonStyles variant="rounded" width={368} height={104} />
-      <WidgetSkeletonStyles
-        variant="rounded"
-        width={368}
-        height={104}
-        sx={{ marginTop: '16px' }}
-      />
-      <WidgetSkeletonStyles
-        variant="rounded"
-        width={368}
-        height={104}
-        sx={{ marginTop: '16px' }}
-      />
-      <WidgetFooter>
-        <WidgetSkeletonMainButton>
-          <WidgetSkeletonMainButtonTypography>
-            {/* A button title could be here */}
-            {/* {buttonTitle} */}
-          </WidgetSkeletonMainButtonTypography>
-        </WidgetSkeletonMainButton>
-        <WidgetSkeletonWalletButton>
-          <WalletIcon />
-        </WidgetSkeletonWalletButton>
-      </WidgetFooter>
-    </WidgetSkeletonContainer>
+          <Box
+            display="flex"
+            gap="4px"
+            sx={{
+              'div:last-of-type': {
+                marginRight: '-10px',
+              },
+            }}
+          >
+            <WidgetSkeletonIconButton>
+              <ReceiptLongIcon />
+            </WidgetSkeletonIconButton>
+            <WidgetSkeletonIconButton>
+              <SettingsIcon />
+            </WidgetSkeletonIconButton>
+          </Box>
+        </WidgetSkeletonHeader>
+        <WidgetSkeletonStyles variant="rounded" width={368} height={104} />
+        <WidgetSkeletonStyles
+          variant="rounded"
+          width={368}
+          height={104}
+          sx={{ marginTop: '16px' }}
+        />
+        <WidgetSkeletonStyles
+          variant="rounded"
+          width={368}
+          height={104}
+          sx={{ marginTop: '16px' }}
+        />
+        <WidgetFooter>
+          <WidgetSkeletonMainButton>
+            <WidgetSkeletonMainButtonTypography>
+              {/* A button title could be here */}
+              {/* {buttonTitle} */}
+            </WidgetSkeletonMainButtonTypography>
+          </WidgetSkeletonMainButton>
+          <WidgetSkeletonWalletButton>
+            <WalletIcon />
+          </WidgetSkeletonWalletButton>
+        </WidgetFooter>
+      </WidgetSkeletonContainer>
+    </Box>
   );
 };
