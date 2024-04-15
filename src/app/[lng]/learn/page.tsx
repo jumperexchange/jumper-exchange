4e1cb8733eed452b3b2d75a8c435a57bcf08a041
Denny San <sander.bewerbung@gmx.de>
2024-04-15 11:17:29 +0200

    refactor: remove unused lng prop

--- src/app/[lng]/learn/page.tsx
+++ src/app/[lng]/learn/page.tsx
@@ -3,13 +3,7 @@ import { getFeaturedArticle } from 'src/app/lib/getFeaturedArticle';
 import LearnPage from 'src/app/ui/learn/LearnPage';
 
 // `app/ui/learn/page.tsx` is the UI for the `/learn` URL
-export default async function Page({
-  params: { lng },
-}: {
-  params: {
-    lng: string;
-  };
-}) {
+export default async function Page() {
   const featuredArticle = await getFeaturedArticle();
   const carouselArticles = await getArticles(featuredArticle.data.id);
   return (
