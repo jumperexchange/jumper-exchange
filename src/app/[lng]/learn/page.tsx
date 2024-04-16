5cec9434e91e30985bf3864df00d5b3b3077df23
Thomas Cherret <b00705488@essec.edu>
2024-04-16 10:23:43 +0200

    feat

--- src/app/[lng]/learn/page.tsx
+++ src/app/[lng]/learn/page.tsx
@@ -2,8 +2,6 @@ import { getArticles } from '@/app/lib/getArticles';
 import { getFeaturedArticle } from '@/app/lib/getFeaturedArticle';
 import LearnPage from '@/app/ui/learn/LearnPage';
 
-export const dynamic = 'force-dynamic';
-
 // `app/ui/learn/page.tsx` is the UI for the `/learn` URL
 export default async function Page() {
   const featuredArticle = await getFeaturedArticle();
