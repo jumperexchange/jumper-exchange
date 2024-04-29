214d4d99b8160e69a265c6aa08616a48c3c161e3
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-04-29 16:33:39 +0700

    fix: lint

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -11,7 +11,7 @@ Sentry.init({
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
