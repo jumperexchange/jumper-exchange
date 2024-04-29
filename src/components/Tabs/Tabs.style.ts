8b308deacd418cee9aeb035ed221c762b3748270
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-04-29 17:59:45 +0700

    fix: tabs always centered

--- src/components/Tabs/Tabs.style.ts
+++ src/components/Tabs/Tabs.style.ts
@@ -8,6 +8,7 @@ import { Tab as MuiTab, Tabs, styled } from '@mui/material';
 export const TabsContainer = styled(Tabs, {
   shouldForwardProp: (prop) => prop !== 'styles',
 })<TabsProps>(({ theme }) => ({
+  margin: '0 auto',
   backgroundColor:
     theme.palette.mode === 'dark'
       ? getContrastAlphaColor(theme, '12%')
