702487a7fa6ee77fed4f35337cab5a7e307987d9
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-04-30 16:14:27 +0700

    fix: Disable server sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,6 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
+  enabled: false,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
