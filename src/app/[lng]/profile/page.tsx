217f62d8525e41d501fbb910c0cca4043afcd344
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-05-20 10:12:17 +0100

    feat: Remove force-dynamic

--- src/app/[lng]/profile/page.tsx
+++ src/app/[lng]/profile/page.tsx
@@ -1,7 +1,5 @@
 import ProfilePage from '../../ui/profile/ProfilePage';
 
-export const dynamic = 'force-dynamic';
-
 // `app/ui/learn/page.tsx` is the UI for the `/learn` URL
 export default async function Page() {
   return <ProfilePage />;
