88f722b70edcbb14906bd78e08fcda2f91c9d674
Thomas Cherret <b00705488@essec.edu>
2024-05-21 11:46:41 +0200

    chore: fix import error

--- src/app/ui/app/App.tsx
+++ src/app/ui/app/App.tsx
@@ -4,6 +4,7 @@ import { WelcomeScreen } from '@/components/WelcomeScreen/WelcomeScreen';
 import { Widgets } from '@/components/Widgets/Widgets';
 import type { StarterVariantType } from '@/types/internal';
 import { getCookies } from '@/app/lib/getCookies';
+import { PixelBg } from 'src/components/illustrations/PixelBg';
 export interface AppProps {
   starterVariant: StarterVariantType;
 }
