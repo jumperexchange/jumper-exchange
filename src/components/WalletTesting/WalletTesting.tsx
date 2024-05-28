0e9d4d4fdbc0a5ac1a67054fed90f3c6f2bc385b
Denny San <sander.bewerbung@gmx.de>
2024-05-28 10:24:58 +0200

    chore: add mobile check

--- src/components/WalletTesting/WalletTesting.tsx
+++ src/components/WalletTesting/WalletTesting.tsx
@@ -5,16 +5,25 @@ import { useEffect, useState } from 'react';
 const WalletTesting = () => {
   const [userAgent, setUserAgent] = useState<string>();
   const [provider, setProvider] = useState<string>();
+  const [mobile, setMobile] = useState<boolean>();
 
   useEffect(() => {
     typeof window !== 'undefined' && setUserAgent(window.navigator.userAgent);
     typeof window !== 'undefined' && setProvider((window as any)?.ethereum);
+    typeof window !== 'undefined' &&
+      setMobile((window as any)?.navigator.userAgentData.mobile);
   }, []);
 
   console.log(typeof window !== 'undefined' && (window as any)?.ethereum);
 
   return (
     <Box>
+      {mobile && (
+        <>
+          <Typography variant="lifiHeaderMedium">Mobile?</Typography>
+          <p>{mobile ? 'true' : 'false'}</p>
+        </>
+      )}
       {userAgent && (
         <>
           <Typography variant="lifiHeaderMedium">UserAgent</Typography>
