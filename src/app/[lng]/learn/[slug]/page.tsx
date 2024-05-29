412f137bf074788c1326623d7fd0f55d71c59a67
Thomas Cherret <b00705488@essec.edu>
2024-05-29 16:42:23 +0200

    feat: make the html title variable by page

--- src/app/[lng]/learn/[slug]/page.tsx
+++ src/app/[lng]/learn/[slug]/page.tsx
@@ -1,8 +1,33 @@
 import { getArticleBySlug } from '@/app/lib/getArticleBySlug';
 import { getArticlesByTag } from '@/app/lib/getArticlesByTag';
 import LearnArticlePage from '@/app/ui/learn/LearnArticlePage';
-import type { BlogArticleData } from '@/types/strapi';
+import type { BlogArticleAttributes, BlogArticleData } from '@/types/strapi';
 import { getCookies } from '@/app/lib/getCookies';
+import { Metadata } from 'next';
+import { sliceStrToXChar } from 'src/utils/splitStringToXChar';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const article = await getArticleBySlug(params.slug);
+
+  if (!article.data || !article.data.data?.[0]) {
+    return {
+      title: `Jumper Learn | ${sliceStrToXChar(params.slug.replaceAll('-', ' '), 45)}`,
+      description: `This is the description for the article "${params.slug.replaceAll('-', ' ')}".`,
+    };
+  }
+
+  const articleData = article.data.data?.[0]
+    .attributes as BlogArticleAttributes;
+
+  return {
+    title: `Jumper Learn | ${sliceStrToXChar(articleData.Title, 45)}`,
+    description: articleData.Subtitle,
+  };
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const article = await getArticleBySlug(params.slug);
