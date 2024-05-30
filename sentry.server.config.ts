556c1ce94fb0b1894c9b4aad4d51361e2f112a87
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-05-30 12:24:20 +0100

    chore: Optimize sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,13 +1,10 @@
-// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
-// The config you add here will be used whenever one of the edge features is loaded.
-// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
-import { isProduction } from '@/utils/isProduction';
 
 Sentry.init({
-  enabled: isProduction,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
@@ -15,4 +12,7 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
 });
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,18 +1,9 @@
-import * as Sentry from '@sentry/nextjs';
-import { isProduction } from './utils/isProduction';
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('../sentry.server.config');
+  }
 
-export function register() {
-  Sentry.init({
-    enabled: isProduction,
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-
-    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: process.env.NODE_ENV === 'development',
-  });
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('../sentry.edge.config');
+  }
 }
