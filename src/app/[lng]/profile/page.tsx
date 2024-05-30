e4d89e2321f943255017e0d6c91eb739676b5f25
Thomas Cherret <b00705488@essec.edu>
2024-05-30 11:10:08 +0200

    feat: testing og sharing

--- src/app/[lng]/profile/page.tsx
+++ src/app/[lng]/profile/page.tsx
@@ -5,7 +5,27 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: 'Jumper Profile',
     description: 'Jumper Profile is the profile page of Jumper exchange.',
-    openGraph: {},
+    openGraph: {
+      title: 'Jumper Profile',
+      description: 'Jumper Profile page where you can see your XP and level.',
+      url: 'https://jumper.exchange/profile',
+      images: [
+        {
+          url: 'https://strapi.li.finance/uploads/large_Save_on_Gas_Fees_518a5edcc9.jpg', // Must be an absolute URL
+          width: 800,
+          height: 600,
+          alt: 'banner image',
+        },
+        {
+          url: 'https://strapi.li.finance/uploads/large_Save_on_Gas_Fees_518a5edcc9.jpg', // Must be an absolute URL
+          width: 1800,
+          height: 1600,
+          alt: 'banner image',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
   };
 }
 
