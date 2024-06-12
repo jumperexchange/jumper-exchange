a24a285f29b0da660f3f9220dfb6927e6829fa0b
Thomas Cherret <b00705488@essec.edu>
2024-06-12 18:27:42 +0200

    automatic push

--- src/components/Superfest/Rewards/RewardsCarousel.tsx
+++ src/components/Superfest/Rewards/RewardsCarousel.tsx
@@ -6,8 +6,13 @@ import {
 } from './RewardsCarousel.style';
 import { RewardsBox } from './RewardsBox/RewardsBox';
 import { ClaimingButton } from './ClaimingButton/ClaimingButton';
+import { useMerklRewards } from 'src/hooks/useMerklRewards';
 
 export const RewardsCarousel = ({}) => {
+  const {} = useMerklRewards({
+    userAddress: '0x55048e0d46f66fa00cae12905f125194cd961581',
+  });
+
   return (
     <RewardsCarouselContainer>
       <RewardsCarouselHeader>
--- src/hooks/useMerklRewards.ts
+++ src/hooks/useMerklRewards.ts
@@ -13,55 +13,92 @@ export interface UseMerklRewardsProps {
   userAddress?: string;
 }
 
-const JUMPER_QUEST_ID = [];
+const JUMPER_QUEST_ID = ['0x1C6A6Ee7D2e0aC0D2E3de4a69433553e0cb52777'];
 
-const MEKRL_API = 'https://api.merkl.xyz/v3';
+const ACTIVE_CHAINS = ['42161', '10', '8453', '34443'];
+
+const MERKL_API = 'https://api.merkl.xyz/v3';
 
 export const useMerklRewards = ({
   userAddress,
 }: UseMerklRewardsProps): UseMerklRes => {
-  // In this readContract call, we enforce the type as 0xstring to adhere to the viem type,
-  // and we ensure the variable isn't undefined due to the check within enabled
-  const tokenId = useReadContract({
-    abi: MercleNFTABI,
-    address: MERCLE_ADDRESS,
-    functionName: 'getActiveTokenId',
-    args: [userAddress as `0xstring`],
-    chainId: base.id,
-    query: {
-      enabled: !!userAddress,
-    },
-  });
-  const id = tokenId.data as bigint;
+  // state
+  let userTVL = 0;
+  //test
+  console.log('-------------');
 
   // Call to get the active positions
+  // To do -> use the label to get only
+  const MERKL_POSITIONS_API = `${MERKL_API}/positions?chainId=${ACTIVE_CHAINS.join(',')}&user=${userAddress}`;
+  const { data: positionsData, isSuccess: positionsIsSuccess } = useQuery({
+    queryKey: ['MerklPositions'],
+
+    queryFn: async () => {
+      const response = await fetch(MERKL_POSITIONS_API, {
+        // headers: {
+        //   Authorization: `Bearer ${apiAccesToken}`,
+        // },
+      });
+      const result = await response.json();
+      console.log(result);
+      return result;
+    },
+    // enabled: !!account?.address && account.chainType === 'EVM',
+    refetchInterval: 1000 * 60 * 60,
+  });
+  // loop through the position and sum the TVL USD
+  if (positionsData) {
+    for (const [key, data] of Object.entries(positionsData)) {
+      if (
+        JUMPER_QUEST_ID.includes(key.split('_')[1]) &&
+        (data as any)?.userTVL
+      ) {
+        userTVL += (data as any)?.userTVL;
+      }
+    }
+  }
+
+  // check the user positions for the interesting campaign
+
   // Call to get the available rewards
+  const MERKL_REWARDS_API = `${MERKL_API}/rewards?chainIds=10&user=${userAddress}`;
+  const { data: rewardsData, isSuccess: rewardsIsSuccess } = useQuery({
+    queryKey: ['MerklRewards'],
 
-  //   const tokenUri = useReadContract({
-  //     abi: MercleNFTABI,
-  //     address: MERCLE_ADDRESS,
-  //     functionName: 'tokenURI',
-  //     args: [id],
-  //     chainId: base.id,
-  //     query: {
-  //       enabled: !!id,
-  //     },
-  //   });
-  //   const uri = tokenUri?.data as string;
+    queryFn: async () => {
+      const response = await fetch(MERKL_REWARDS_API, {
+        // headers: {
+        //   Authorization: `Bearer ${apiAccesToken}`,
+        // },
+      });
+      const result = await response.json();
+      return result;
+    },
+    // enabled: !!account?.address && account.chainType === 'EVM',
+    refetchInterval: 1000 * 60 * 60,
+  });
+  console.log(rewardsData);
+  // transform the rewards to get the total in the token that we want // OP
+  // transform the result to know what is coming from Jumper campaigns
+  // transform to know what is not coming from Jumper campaigns
+  if (rewardsData) {
+    const tokenData = rewardsData['10']?.tokenData;
+    let rewardData = [];
+    if (tokenData) {
+      const rewardsData = Object.entries(positionsData).map((elem) => {
+        console.log('---');
+        console.log(elem);
+      });
+    }
+  }
 
-  //   const { data } = useQuery({
-  //     queryKey: ['mercleNFT'],
-  //     queryFn: async () => {
-  //       const response = await fetch(uri);
-  //       const result = await response.json();
-  //       return result.image;
-  //     },
-  //     refetchInterval: 1000 * 60 * 60,
-  //     enabled: !!uri,
-  //   });
+  console.log('-------------');
+  console.log(userTVL);
 
   return {
-    activePosition: {},
+    activePosition: {
+      userTVL,
+    },
     availableRewards: {},
   };
 };
