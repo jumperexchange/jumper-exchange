0eb26d61ae87185e55b7e9e1adfdb20530173b2c
Denny San <sander.bewerbung@gmx.de>
2024-06-12 11:23:08 +0200

    refactor: remove comment

--- src/stores/settings/SettingsStore.tsx
+++ src/stores/settings/SettingsStore.tsx
@@ -34,7 +34,6 @@ export const useSettingsStore = createWithEqualityFn(
           });
       },
 
-      // Installed Wallets
       setPartnerTheme: (partnerTheme: PartnerThemesData) => {
         set({
           partnerTheme: partnerTheme,
