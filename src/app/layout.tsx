1ceabfa6bbf0870dbf53be0ea827db16ec7893d3
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-07-02 10:02:01 +0100

    fix: missing metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default async function RootLayout({
   return (
     <html lang={fallbackLng}>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Script
           async
