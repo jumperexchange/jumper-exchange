2d95af6ccf34cf657cfc1dbd0a23ecebea902f2d
Thomas Cherret <b00705488@essec.edu>
2024-07-04 11:01:11 +0200

    feat: first filters version

--- src/components/Superfest/AvailableMissionsList/AvailableMissionsList.tsx
+++ src/components/Superfest/AvailableMissionsList/AvailableMissionsList.tsx
@@ -1,4 +1,3 @@
-import { useAccounts } from '@/hooks/useAccounts';
 import type { Quest } from '@/types/loyaltyPass';
 import { QuestCard } from '../QuestCard/QuestCard';
 import { QuestCardSkeleton } from '../QuestCard/QuestCardSkeleton';
@@ -9,44 +8,21 @@ import {
   AvailableMissionsTitle,
 } from './AvailableMissionsList.style';
 import { useOngoingFestMissions } from 'src/hooks/useOngoingFestMissions';
-import { Box, Theme, useTheme } from '@mui/material';
-import { useEffect, useState } from 'react';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import ListItemText from '@mui/material/ListItemText';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import Checkbox from '@mui/material/Checkbox';
-import { SoraTypography } from '../Superfest.style';
-import Image from 'next/image';
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-      backgroundColor: '#fff0ca',
-    },
-  },
-};
+import {
+  Box,
+  SelectChangeEvent,
+  Theme,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import { useState } from 'react';
+import { MissionsFilter } from '../MissionsFilter/MissionsFilter';
 
 const chains = ['Optimism', 'Base', 'Mode', 'Fraxtal'];
 
-const Rewards_list = ['OP Rewards', 'No OP Rewards'];
+const rewards_list = ['OP Rewards', 'No OP Rewards'];
 
 const category = ['Lending', 'Liquidity Pool', 'Staking', 'Stablecoins'];
-
-function getStyles(name: string, personName: readonly string[], theme: Theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
 
 interface QuestCompletedListProps {
   quests?: Quest[];
@@ -57,157 +33,78 @@ export const AvailableMissionsList = ({
   quests,
   loading,
 }: QuestCompletedListProps) => {
-  const theme = useTheme();
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down('md'),
+  );
   const [chainsFilter, setChainsFilter] = useState<string[]>([]);
   const [rewardsFilter, setRewardsFilter] = useState<string[]>([]);
   const [categoryFilter, setCategoryFilter] = useState<string[]>([]);
   const { url } = useOngoingFestMissions();
 
-  const [personName, setPersonName] = useState<string[]>([]);
-
-  const handleChainChange = (event: SelectChangeEvent<typeof personName>) => {
+  const handleChainChange = (event: SelectChangeEvent<typeof chainsFilter>) => {
     const {
       target: { value },
     } = event;
-    setChainsFilter(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+    setChainsFilter(typeof value === 'string' ? value.split(',') : value);
   };
 
   const handleCategoryChange = (
-    event: SelectChangeEvent<typeof personName>,
+    event: SelectChangeEvent<typeof categoryFilter>,
   ) => {
     const {
       target: { value },
     } = event;
-    console.log(value);
-    setCategoryFilter(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+    setCategoryFilter(typeof value === 'string' ? value.split(',') : value);
   };
 
-  const handleRewardChange = (event: SelectChangeEvent<typeof personName>) => {
+  const handleRewardChange = (
+    event: SelectChangeEvent<typeof rewardsFilter>,
+  ) => {
     const {
       target: { value },
     } = event;
-    setRewardsFilter(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+    setRewardsFilter(typeof value === 'string' ? value.split(',') : value);
   };
 
   return (
     <AvailableMissionsContainer>
       <AvailableMissionsHeader>
         <AvailableMissionsTitle>{'Available Missions'}</AvailableMissionsTitle>
-        <Box>
-          <FormControl sx={{ width: 120 }}>
-            <Select
-              multiple
-              displayEmpty
-              value={chainsFilter}
-              onChange={handleChainChange}
-              input={<OutlinedInput />}
-              renderValue={(selected) => {
-                return <SoraTypography fontWeight={700}>Chains</SoraTypography>;
-              }}
-              MenuProps={MenuProps}
-              inputProps={{ 'aria-label': 'Without label' }}
-              onClose={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-              }}
-            >
-              {chains.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name}
-                  style={getStyles(name, chainsFilter, theme)}
-                >
-                  <Checkbox checked={chainsFilter.indexOf(name) > -1} />
-                  <Image
-                    src={`https://strapi.li.finance/uploads/fraxtal_bbaa1cb0cd.png`}
-                    alt="Effigy Wallet Icon"
-                    width={16}
-                    height={16}
-                    priority={false}
-                    style={{ marginRight: '4px' }}
-                  />
-                  <ListItemText primary={name} />
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ width: 120, ml: '4px' }}>
-            <Select
-              multiple
-              displayEmpty
-              value={rewardsFilter}
-              onChange={handleRewardChange}
-              input={<OutlinedInput />}
-              renderValue={(selected) => {
-                return (
-                  <SoraTypography fontWeight={700}>Rewards</SoraTypography>
-                );
-              }}
-              MenuProps={MenuProps}
-              inputProps={{ 'aria-label': 'Without label' }}
-              onClose={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-              }}
-            >
-              {Rewards_list.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name}
-                  style={getStyles(name, rewardsFilter, theme)}
-                >
-                  <Checkbox checked={rewardsFilter.indexOf(name) > -1} />
-                  <ListItemText primary={name} />
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ width: 120, ml: '4px' }}>
-            <Select
-              multiple
-              displayEmpty
-              value={categoryFilter}
-              onChange={handleCategoryChange}
-              input={<OutlinedInput />}
-              renderValue={(selected) => {
-                return (
-                  <SoraTypography fontWeight={700}>Category</SoraTypography>
-                );
-              }}
-              MenuProps={MenuProps}
-              inputProps={{ 'aria-label': 'Without label' }}
-              onClose={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-              }}
-            >
-              {category.map((name) => (
-                <MenuItem
-                  key={name}
-                  value={name.toLowerCase().replaceAll(' ', '-')}
-                  style={getStyles(name, categoryFilter, theme)}
-                >
-                  <Checkbox
-                    checked={
-                      categoryFilter.indexOf(
-                        name.toLowerCase().replaceAll(' ', '-'),
-                      ) > -1
-                    }
-                  />
-                  <ListItemText primary={name} />
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+        <Box display="flex">
+          {isMobile ? (
+            <MissionsFilter
+              title="Category"
+              options={category}
+              activeChoices={categoryFilter}
+              handleChange={handleCategoryChange}
+            />
+          ) : (
+            <>
+              {' '}
+              <MissionsFilter
+                title="Chains"
+                options={chains}
+                activeChoices={chainsFilter}
+                handleChange={handleChainChange}
+              />
+              <Box marginLeft={'4px'}>
+                <MissionsFilter
+                  title="Rewards"
+                  options={rewards_list}
+                  activeChoices={rewardsFilter}
+                  handleChange={handleRewardChange}
+                />
+              </Box>
+              <Box marginLeft={'4px'}>
+                <MissionsFilter
+                  title="Category"
+                  options={category}
+                  activeChoices={categoryFilter}
+                  handleChange={handleCategoryChange}
+                />
+              </Box>
+            </>
+          )}
         </Box>
       </AvailableMissionsHeader>
       <AvailableMissionsStack
@@ -255,15 +152,6 @@ export const AvailableMissionsList = ({
                   return undefined;
                 }
               }
-              console.log('---------');
-              console.log(categoryFilter);
-              console.log(quest.attributes.Category);
-              console.log(
-                categoryFilter &&
-                  categoryFilter.length > 0 &&
-                  quest.attributes.Category &&
-                  !categoryFilter.includes(quest.attributes.Category),
-              );
               if (
                 categoryFilter &&
                 categoryFilter.length > 0 &&
