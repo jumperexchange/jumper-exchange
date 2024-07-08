528c9969fe2f780a628561069daf4d7e5622e2b0
Denny San <sander.bewerbung@gmx.de>
2024-07-08 10:24:19 +0200

    chore: adjust urls and append slash

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 // middleware.ts
 import acceptLanguage from 'accept-language';
 import { i18nRouter } from 'next-i18n-router';
+import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import i18nConfig from '../i18nconfig';
 import { locales } from './i18n/i18next-locales';
@@ -8,6 +9,15 @@ import { locales } from './i18n/i18next-locales';
 acceptLanguage.languages(locales);
 
 export function middleware(request: NextRequest) {
+  const url = new URL(request.url);
+
+  // Check if the pathname ends with a slash or if it's a file (contains a dot)
+  if (!url.pathname.endsWith('/') && !url.pathname.includes('.')) {
+    url.pathname += '/';
+    return NextResponse.redirect(url);
+  }
+
+  // If the URL is already correctly formatted, proceed with i18nRouter
   return i18nRouter(request, i18nConfig);
 }
 
