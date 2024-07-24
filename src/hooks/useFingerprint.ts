bc55b6de3f069eb5128b04fdeadfcbabfc247c61
Denny San <sander.bewerbung@gmx.de>
2024-07-24 17:50:20 +0200

    refactor: clean unused

--- src/hooks/useFingerprint.ts
+++ src/hooks/useFingerprint.ts
@@ -3,13 +3,12 @@ import { useEffect, useState } from 'react';
 
 export const useFingerprint = () => {
   const [fingerprint, setFingerprint] = useState<string>();
-  // Create a new ClientJS object
 
   useEffect(() => {
     // Get the client's fingerprint id;
-    const newFingerprint = 'abcd';
-    console.log(newFingerprint);
-    setFingerprint(`${newFingerprint}`);
+    // const newFingerprint = 'abcd';
+    // console.log(newFingerprint);
+    // setFingerprint(`${newFingerprint}`);
   }, []);
 
   return fingerprint;
