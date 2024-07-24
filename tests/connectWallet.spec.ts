6c31db0bbcd11eb987ed5460a7ad71227fb9b515
Bojan Kopunovic <bkopunovic93@gmail.com>
2024-07-24 12:28:12 +0200

    make sure that connect button is visible

--- tests/connectWallet.spec.ts
+++ tests/connectWallet.spec.ts
@@ -7,7 +7,7 @@ const test = testWithSynpress(metaMaskFixtures(basicSetup));
 
 const { expect } = test;
 
-test('should connect wallet to LI.FI Scan', async ({
+test('should connect wallet to Jumper', async ({
   context,
   page,
   extensionId,
@@ -20,9 +20,10 @@ test('should connect wallet to LI.FI Scan', async ({
   );
   const connectWalletButton = page.locator('#connect-wallet-button');
   const connectedWalletButton = page.locator('#wallet-digest-button');
+  expect(connectWalletButton).toBeEnabled()
   await connectWalletButton.click();
   await page.locator('(//img[@alt="io.metamask-wallet-logo"])[1]').click();
   await page.locator('xpath=(//button[@type="button"])[2]').click();
-  // await metamask.connectToDapp(['Account 1']);
+  await metamask.connectToDapp(['Account 1']);
   await expect(connectedWalletButton).not.toHaveText('Connect');
 });
