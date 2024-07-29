0c0c2a89ab48e0e0aa346c5c6f8ce04dde83fb55
Adam <info@admsk2.pl>
2024-07-29 11:33:05 +0200

    fix: Discord invite link

--- src/const/urls.ts
+++ src/const/urls.ts
@@ -4,6 +4,7 @@ export const JUMPER_URL = 'https://jumper.exchange';
 export const LIFI_URL = 'https://li.fi';
 export const EXPLORER_URL = 'https://explorer.li.fi';
 export const DISCORD_URL = 'https://discord.gg/jumperexchange';
+export const DISCORD_URL_INVITE = 'https://discord.com/invite/jumperexchange';
 export const X_URL = 'https://x.com/JumperExchange';
 export const GITHUB_URL = 'https://github.com/jumperexchange';
 export const DOCS_URL = 'https://docs.li.fi/';
