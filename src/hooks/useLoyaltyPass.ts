4e450c826fdfb1725f97f24b5d495519a16783a3
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-07-30 14:28:43 +0100

    feat: implement backend

--- src/hooks/useLoyaltyPass.ts
+++ src/hooks/useLoyaltyPass.ts
@@ -1,8 +1,6 @@
 import { useLoyaltyPassStore } from '@/stores/loyaltyPass';
 import type { PDA } from '@/types/loyaltyPass';
 import { useQuery } from '@tanstack/react-query';
-import request from 'graphql-request';
-import { getAllPDAs } from './querries/pdas';
 import { useAccounts } from './useAccounts';
 
 export interface UseLoyaltyPassProps {
@@ -11,10 +9,6 @@ export interface UseLoyaltyPassProps {
   points?: number;
   tier?: string;
   pdas?: PDA[];
-}
-
-interface IGatewayAPI {
-  issuedPDAs: PDA[];
 }
 
 const SECONDS_IN_A_DAY = 86400;
@@ -41,52 +35,28 @@ export const useLoyaltyPass = (): UseLoyaltyPassProps => {
       account?.address?.toLowerCase() !== storedAddress?.toLowerCase());
 
   // query
-  const apiBaseUrl = process.env.NEXT_PUBLIC_GATEWAY_URL;
-  const apiUrl = new URL(`${apiBaseUrl}`);
-  const apiKey = process.env.NEXT_PUBLIC_GATEWAY_API_KEY;
-  const apiAccesToken = process.env.NEXT_PUBLIC_GATEWAY_API_TOKEN;
-  const headers = {
-    'x-api-key': `${apiKey}`,
-    Authorization: `Bearer ${apiAccesToken}`,
-  };
-
+  const apiBaseUrl = process.env.NEXT_PUBLIC_JUMPER_API;
   const { data, isSuccess, isLoading } = useQuery({
     queryKey: ['loyalty-pass'],
     queryFn: async () => {
-      const res = await request(
-        decodeURIComponent(apiUrl.href),
-        getAllPDAs,
-        { EVMAddress: account?.address },
-        headers,
-      );
+      const res = await fetch(`${apiBaseUrl}/users/${account?.address}`);
 
-      if (res && account?.address) {
-        let points = 0;
-        let tier = '';
-        const { issuedPDAs: pdas } = res as IGatewayAPI;
-        // filter to remove loyalty pass from pda
-        const pdasWithoutLoyalty = pdas.filter((pda: PDA) => {
-          if (pda.dataAsset.title === 'LI.FI Loyalty Pass') {
-            points = pda.dataAsset.claim.points;
-            tier = pda.dataAsset.claim.tier;
-            return false;
-          }
-          return true;
-        });
+      const data = await res.json();
 
+      if (data && account?.address) {
         setLoyaltyPassData(
           account.address,
-          points,
-          tier,
-          pdasWithoutLoyalty,
+          data.sum,
+          data.currentLevel,
+          data.walletRewards,
           t,
         );
 
         return {
           address: account.address,
-          points: points,
-          tier: tier,
-          pdas: pdasWithoutLoyalty,
+          points: data.sum,
+          tier: data.currentLevel,
+          pdas: data.walletRewards,
         };
       } else {
         return undefined;
