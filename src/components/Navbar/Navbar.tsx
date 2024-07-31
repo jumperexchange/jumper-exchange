e2e6760047e631d8aaaf5d4479d70e438459f8fc
Denny San <sander.bewerbung@gmx.de>
2024-07-31 13:10:39 +0200

    fix: reload page on scan logo click

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -37,7 +37,7 @@ export const Navbar = ({ disableNavbar = false }) => {
     if (isLearnPage) {
       router.push(JUMPER_LEARN_PATH);
     } else if (isScanPage) {
-      router.push(JUMPER_SCAN_PATH);
+      window.open(JUMPER_SCAN_PATH, '_self');
     } else {
       router.push('/');
     }
