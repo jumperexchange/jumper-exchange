39a0a67718fb8369aadb4664cd754cb575e2b16a
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-08-06 12:34:19 +0100

    feat: Optimize i18n SEO

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,14 +1,14 @@
 import initTranslations from '@/app/i18n';
 import TranslationsProvider from '@/providers/TranslationProvider';
 import i18nConfig from 'i18nconfig';
-import React from 'react';
+import type { ReactNode } from 'react';
 import { defaultNS, fallbackLng, namespaces } from 'src/i18n';
 
 export default async function RootLayout({
   children,
   params: { lng },
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
   params: { lng: string };
 }) {
   const { resources } = await initTranslations(lng || fallbackLng, namespaces);
