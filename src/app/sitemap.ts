10991a6beb86e8ee9505d098981e3de2f05c9052
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-08-06 15:07:27 +0100

    Try enabling back en language

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,4 @@
-import { JUMPER_LEARN_PATH, JUMPER_URL, pages } from '@/const/urls';
+import { JUMPER_LEARN_PATH, pages } from '@/const/urls';
 import type { ChangeFrequency, SitemapPage } from '@/types/sitemap';
 import type { BlogArticleData, StrapiResponse } from '@/types/strapi';
 import type { MetadataRoute } from 'next';
@@ -12,89 +12,75 @@ function withTrallingSlash(url: string) {
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   // paths
-  const routes = pages
-    .filter((s) => s.priority === 1)
-    .flatMap((route: SitemapPage) => {
-      return locales.map((locale) => {
-        const url = withTrallingSlash(
+  const routes = pages.flatMap((route: SitemapPage) => {
+    return locales.map((locale) => {
+      return {
+        url: withTrallingSlash(
           `${process.env.NEXT_PUBLIC_SITE_URL}${locale !== 'en' ? `/${locale}` : ''}${route.path}`,
-        );
-        return {
-          url,
-          lastModified: new Date().toISOString().split('T')[0],
-          changeFrequency: 'weekly' as ChangeFrequency,
-          alternates: {
-            languages: locales.reduce(
+        ),
+        lastModified: new Date().toISOString().split('T')[0],
+        changeFrequency: 'weekly' as ChangeFrequency,
+        alternates: {
+          languages: {
+            ...locales.reduce(
               (acc, loc) => ({
                 ...acc,
-                [loc !== 'en' ? loc : 'x-default']: withTrallingSlash(
+                [loc]: withTrallingSlash(
                   `${process.env.NEXT_PUBLIC_SITE_URL}${loc !== 'en' ? `/${loc}` : ''}${route.path}`,
                 ),
               }),
               {},
             ),
+            'x-default': withTrallingSlash(
+              `${process.env.NEXT_PUBLIC_SITE_URL}${route.path}`,
+            ),
           },
-          priority: route.priority,
-        };
-      });
+        },
+        priority: route.priority,
+      };
     });
+  });
 
   console.log('dd', util.inspect(routes, false, 3));
 
-  return [...routes];
-  /*
-
-  // paths
-  const routes = pages.filter((s) => s.priority === 1).map((route: SitemapPage) => ({
-    url: withTrallingSlash(`${process.env.NEXT_PUBLIC_SITE_URL}${route.path}`),
-    lastModified: new Date().toISOString().split('T')[0],
-    changeFrequency: 'weekly' as ChangeFrequency,
-    // todo: enable alternates once xml formatting is fixed
-    alternates: {
-      languages: locales.reduce(
-        (acc, locale) => ({
-          ...acc,
-          [locale !== 'en' ? locale : 'x-default']: withTrallingSlash(
-            `${process.env.NEXT_PUBLIC_SITE_URL}/${locale !== 'en' ? locale : ''}${locale === 'en' ? route.path.slice(1) : route.path}`,
-          ),
-        }),
-        {},
-      ),
-    },
-    priority: route.priority,
-  }));
-
-  console.log('dd', util.inspect(routes, false, 3))
-
   // articles by slug
   const articles = await getArticles().then(
-    (article: StrapiResponse<BlogArticleData>) =>
-      article.data.map((el) => ({
-        url: withTrallingSlash(
-          `${process.env.NEXT_PUBLIC_SITE_URL}${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
-        ),
-        lastModified: new Date(
-          el.attributes.updatedAt || el.attributes.publishedAt || Date.now(),
-        )
-          .toISOString()
-          .split('T')[0],
-        changeFrequency: 'weekly' as ChangeFrequency,
-        // todo: enable alternates once xml formatting is fixed
-        alternates: {
-          languages: locales.reduce((acc, locale) => {
-            return {
-              ...acc,
-              [locale !== 'en' ? locale : 'x-default']: withTrallingSlash(
-                `${process.env.NEXT_PUBLIC_SITE_URL}${locale !== 'en' ? `/${locale}` : ''}${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
-              ),
-            };
-          }, {}),
-        },
-        priority: 0.8,
-      })),
+    (article: StrapiResponse<BlogArticleData>) => {
+      return locales.flatMap((locale) => {
+        return article.data.map((el) => {
+          return {
+            url: withTrallingSlash(
+              `${process.env.NEXT_PUBLIC_SITE_URL}${locale !== 'en' ? `/${locale}` : ''}${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
+            ),
+            lastModified: new Date(
+              el.attributes.updatedAt ||
+                el.attributes.publishedAt ||
+                Date.now(),
+            )
+              .toISOString()
+              .split('T')[0],
+            changeFrequency: 'weekly' as ChangeFrequency,
+            alternates: {
+              languages: {
+                ...locales.reduce((acc, loc) => {
+                  return {
+                    ...acc,
+                    [loc]: withTrallingSlash(
+                      `${process.env.NEXT_PUBLIC_SITE_URL}${loc !== 'en' ? `/${loc}` : ''}${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
+                    ),
+                  };
+                }, {}),
+                'x-default': withTrallingSlash(
+                  `${process.env.NEXT_PUBLIC_SITE_URL}${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
+                ),
+              },
+            },
+            priority: 0.8,
+          };
+        });
+      });
+    },
   );
-*/
 
-  // return [...routes, ...articles];
-  // return [...routes];
+  return [...routes, ...articles];
 }
