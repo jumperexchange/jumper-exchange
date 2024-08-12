71e48a76b0b123661928d131462565475c80e7aa
Denny San <sander.bewerbung@gmx.de>
2024-08-12 18:32:58 +0200

    refactor: cleanup

--- src/hooks/useIsGasVariant.ts
+++
@@ -1,9 +0,0 @@
-import { TabsMap } from 'src/const/tabsMap';
-import { useActiveTabStore } from 'src/stores/activeTab';
-
-export const useIsGasVariant = () => {
-  const { activeTab } = useActiveTabStore();
-  const isGasVariant = activeTab === TabsMap.Refuel.index;
-
-  return isGasVariant;
-};
