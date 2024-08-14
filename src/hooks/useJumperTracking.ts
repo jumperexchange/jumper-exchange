28bd53dc4ca9dc540ae665c5289d73fe9a9004d0
Denny San <sander.bewerbung@gmx.de>
2024-08-14 10:00:45 +0200

    chore: add env for jumper be api

--- src/hooks/useJumperTracking.ts
+++ src/hooks/useJumperTracking.ts
@@ -22,7 +22,7 @@ interface JumperDataTrackEventProps {
 const track = async (data: object, path: string) => {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_JUMPER_API}${path}`,
+      `${process.env.NEXT_PUBLIC_JUMPER_BE_API}${path}`,
       {
         method: 'POST',
         headers: {
