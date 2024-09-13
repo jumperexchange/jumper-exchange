10e5b5fca3f31e7212490ee608d8bccb73cece48
Denny San <sander.denny@gmx.de>
2024-09-13 16:38:48 +0200

    refactor: re split into seperate files

--- src/components/Blog/BlogArticlesCollections/BlogArticlesCollections.tsx
+++ src/components/Blog/BlogArticlesCollections/BlogArticlesCollections.tsx
@@ -1,24 +1,9 @@
 'use client';
 
-import type {
-  BlogArticleData,
-  StrapiMetaPagination,
-  TagAttributes,
-} from '@/types/strapi';
-import type { Breakpoint } from '@mui/material';
-import { Box, useTheme } from '@mui/material';
-import { useState } from 'react';
+import type { BlogArticleData } from '@/types/strapi';
 import type { GetTagsResponse } from 'src/app/lib/getTags';
-import { TrackingCategory } from 'src/const/trackingKeys';
-import { BlogArticleCard } from '../BlogArticleCard';
 import { BlogArticleCardSkeleton } from '../BlogArticleCard/BlogArticleCardSkeleton';
-import {
-  BlogArticlesBoardHeader,
-  BlogArticlesBoardTitle,
-} from '../BlogArticlesBoard/BlogArticlesBoard.style';
-import { BlogArticlesCollectionsContainer } from './BlogArticlesCollections.style';
-import { BlogArticlesTab } from './BlogArticlesTab';
-import { BlogArticlesCollectionsPagination as Pagination } from './Pagination';
+import { BlogArticlesTabs } from './BlogArticlesTabs';
 
 interface BlogArticlesCollectionsProps {
   data: BlogArticleData[];
@@ -76,12 +61,12 @@ export const BlogArticlesCollections = ({
         total: tag.attributes.blog_articles.data.length,
       };
       return (
-        <CategoryTabPanel
+        <BlogArticlesTabs
+          ariaLabel={ariaLabel}
           index={tagIndex}
           tag={tag}
           tags={tags}
           pagination={pagination}
-          ariaLabel={ariaLabel}
           data={tag.attributes.blog_articles.data}
         />
       );
@@ -90,92 +75,3 @@ export const BlogArticlesCollections = ({
     <p>No Content</p> //todo: find better option
   );
 };
-
-interface CategoryTabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  tag: TagAttributes;
-  tags: GetTagsResponse;
-  pagination: StrapiMetaPagination;
-  ariaLabel: string;
-  data: BlogArticleData[];
-}
-
-function CategoryTabPanel({
-  children,
-  index,
-  tag,
-  tags,
-  data,
-  pagination,
-}: CategoryTabPanelProps) {
-  const theme = useTheme();
-  const [pageTab, setPageTab] = useState(pagination.page);
-  const chunkedPages = chunkArray(data, pagination.pageSize);
-
-  return (
-    <BlogArticlesCollectionsContainer>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <BlogArticlesBoardHeader>
-          <BlogArticlesBoardTitle variant="headerMedium">
-            {tag.attributes.Title}
-          </BlogArticlesBoardTitle>
-        </BlogArticlesBoardHeader>
-        {chunkedPages.map((page, pageIndex) => (
-          <BlogArticlesTab pageTab={pageTab} index={pageIndex}>
-            {page.map((article, articleIndex: number) => (
-              <BlogArticleCard
-                styles={{
-                  display: 'inline-block',
-                  [theme.breakpoints.up('sm' as Breakpoint)]: {
-                    width: 'unset',
-                  },
-                }}
-                baseUrl={tags.url}
-                id={article.id}
-                key={`blog-articles-collection}-${index}-${articleIndex}`}
-                image={article.attributes.Image}
-                title={article.attributes.Title}
-                slug={article.attributes.Slug}
-                trackingCategory={TrackingCategory.BlogArticlesCollection}
-                content={article.attributes.Content}
-                publishedAt={article.attributes.publishedAt}
-                createdAt={article.attributes.createdAt}
-                tags={article.attributes.tags}
-              />
-            ))}
-          </BlogArticlesTab>
-        ))}
-      </Box>
-      {
-        /* todo: enable pagination*/
-        pagination.pageCount > 0 ? (
-          <Pagination
-            isEmpty={pagination.pageCount <= 1}
-            page={pageTab}
-            setPage={setPageTab}
-            pagination={pagination}
-            categoryId={index}
-          />
-        ) : null
-      }
-    </BlogArticlesCollectionsContainer>
-  );
-}
-
-function chunkArray(array: any[], chunkSize: number): any[][] {
-  const result = [];
-
-  for (let i = 0; i < array.length; i += chunkSize) {
-    const chunk = array.slice(i, i + chunkSize);
-    result.push(chunk);
-  }
-
-  return result;
-}
