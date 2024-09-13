472a37b309c2f7e39c529235ccb9e504c7048a58
Denny San <sander.denny@gmx.de>
2024-09-13 15:05:38 +0200

    chore: sort category order

--- src/components/Blog/BlogArticlesCollections/BlogArticlesCollections.tsx
+++ src/components/Blog/BlogArticlesCollections/BlogArticlesCollections.tsx
@@ -31,11 +31,39 @@ interface BlogArticlesCollectionsProps {
 
 const ariaLabel = 'blog-articles-board-tabs';
 const pageSize = 6;
+// Predefined order array
+const predefinedOrder = ['Announcement', 'Partner', 'Bridge'];
+
+// Function to sort based on predefined order
+const sortTags = (tags: GetTagsResponse) => {
+  return tags.data.sort((a, b) => {
+    const titleA = a.attributes.Title;
+    const titleB = b.attributes.Title;
+
+    const indexA = predefinedOrder.indexOf(titleA);
+    const indexB = predefinedOrder.indexOf(titleB);
+
+    // Categories in predefinedOrder come first, rest keep original order
+    if (indexA === -1 && indexB === -1) {
+      return 0;
+    } // Both are irrelevant
+    if (indexA === -1) {
+      return 1;
+    } // `a` is irrelevant
+    if (indexB === -1) {
+      return -1;
+    } // `b` is irrelevant
+    return indexA - indexB; // Sort by predefined order
+  });
+};
+
 export const BlogArticlesCollections = ({
   data,
   url,
   tags,
 }: BlogArticlesCollectionsProps) => {
+  // Apply sorting function
+  sortTags(tags);
   return !data ? (
     Array.from({ length: tags.meta.pagination.pageSize }).map((_, index) => (
       <BlogArticleCardSkeleton key={`blog-article-card-skeleton-${index}`} />
