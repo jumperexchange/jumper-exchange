fc75aac783a9fb82d779fde07121a754b2e8ea9f
Thomas Cherret <b00705488@essec.edu>
2024-09-17 15:15:46 +0200

    automatic push

--- src/const/partnerRewardsTheme.ts
+++ src/const/partnerRewardsTheme.ts
@@ -1,5 +1,5 @@
-export const PROFILE_CAMPAIGN_DARK_COLOR = '#ECEEF0';
-export const PROFILE_CAMPAIGN_LIGHT_COLOR = '#9E1E1A';
+export const PROFILE_CAMPAIGN_DARK_COLOR = '#653ba3'; //#ECEEF0';
+export const PROFILE_CAMPAIGN_LIGHT_COLOR = '#653ba3'; //#9E1E1A';
 
 export const PROFILE_CAMPAIGN_DARK_TOKEN =
   'https://strapi.li.finance/uploads/Sei_Symbol_Gradient_127605e2de.png';
