80c8a1f217bfcbebbebb14d14529316989b18d37
Tche <45823872+tcheee@users.noreply.github.com>
2024-09-18 12:19:30 +0200

    feat: remove campaign stuff (#1314)

--- src/const/partnerRewardsTheme.ts
+++ src/const/partnerRewardsTheme.ts
@@ -1,5 +1,6 @@
-export const PROFILE_CAMPAIGN_DARK_COLOR = '#ECEEF0';
-export const PROFILE_CAMPAIGN_LIGHT_COLOR = '#9E1E1A';
+export const PROFILE_CAMPAIGN_DARK_COLOR = '#FFFFFF'; //#ECEEF0';
+export const PROFILE_CAMPAIGN_LIGHT_COLOR = '#31007a'; //#9E1E1A';
+export const PROFILE_CAMPAIGN_FLASHY_APY_COLOR = '#653ba3'; //#9E1E1A';
 
 export const PROFILE_CAMPAIGN_DARK_TOKEN =
   'https://strapi.li.finance/uploads/Sei_Symbol_Gradient_127605e2de.png';
