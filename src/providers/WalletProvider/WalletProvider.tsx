3a34985c81f049a9439884b6697887f39ad7a72c
Thomas Cherret <b00705488@essec.edu>
2024-09-25 18:31:36 +0200

    automatic push

--- src/providers/WalletProvider/WalletProvider.tsx
+++ src/providers/WalletProvider/WalletProvider.tsx
@@ -1,9 +1,23 @@
 'use client';
-import { type FC, type PropsWithChildren } from 'react';
+import { type FC, type PropsWithChildren, useEffect } from 'react';
 import { EVMProvider } from './EVMProvider';
 import { SolanaProvider } from './SolanaProvider';
+import { createConfig, EVM, Solana } from '@lifi/sdk';
+import { publicRPCList } from '@/const/rpcList';
 
 export const WalletProvider: FC<PropsWithChildren> = ({ children }) => {
+  useEffect(() => {
+    createConfig({
+      providers: [EVM(), Solana()],
+      integrator: process.env.NEXT_PUBLIC_WIDGET_INTEGRATOR,
+      rpcUrls: {
+        ...JSON.parse(process.env.NEXT_PUBLIC_CUSTOM_RPCS),
+        ...publicRPCList,
+      },
+      preloadChains: true,
+    });
+  }, []);
+
   return (
     <SolanaProvider>
       <EVMProvider>{children}</EVMProvider>
