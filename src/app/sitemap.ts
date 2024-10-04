e67c869499ec755889e0430c3cb17ee422700551
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-10-04 13:31:18 +0100

    Remove multilingual support seo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,47 +9,27 @@ function withoutTrailingSlash(url: string) {
   return url.endsWith('/') ? url.slice(0, -1) : url;
 }
 
-function generateAlternates(path: string) {
-  return {
-    languages: {
-      ...locales.reduce((acc, loc) => {
-        const pages = {
-          ...acc,
-          [loc]: withoutTrailingSlash(
-            `${process.env.NEXT_PUBLIC_SITE_URL}${loc !== 'en' ? `/${loc}` : ''}${path}`,
-          ),
-        };
-
-        return pages;
-      }, {}),
-    },
-  };
-}
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   // paths
   const routes = pages.flatMap((route: SitemapPage) => {
-    return locales.map((locale) => {
       return {
         url: withoutTrailingSlash(
-          `${process.env.NEXT_PUBLIC_SITE_URL}${locale !== 'en' ? `/${locale}` : ''}${route.path}`,
+          `${process.env.NEXT_PUBLIC_SITE_URL}${route.path}`,
         ),
         lastModified: new Date().toISOString().split('T')[0],
         changeFrequency: 'weekly' as ChangeFrequency,
-        alternates: generateAlternates(route.path),
         priority: route.priority,
       };
-    });
   });
 
   // articles by slug
   const articles = await getArticles().then(
     (article: StrapiResponse<BlogArticleData>) => {
-      return locales.flatMap((locale) => {
         return article.data.map((el) => {
           return {
             url: withoutTrailingSlash(
-              `${process.env.NEXT_PUBLIC_SITE_URL}${locale !== 'en' ? `/${locale}` : ''}${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
+              `${process.env.NEXT_PUBLIC_SITE_URL}${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
             ),
             lastModified: new Date(
               el.attributes.updatedAt ||
@@ -59,13 +39,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
               .toISOString()
               .split('T')[0],
             changeFrequency: 'weekly' as ChangeFrequency,
-            alternates: generateAlternates(
-              `${JUMPER_LEARN_PATH}${el.attributes.Slug}`,
-            ),
             priority: 0.8,
           };
         });
-      });
     },
   );
 
