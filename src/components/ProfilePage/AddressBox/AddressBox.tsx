f34811623d32ef4af4b362cbc5f3317c420bacc6
dennyscode <43956540+dennyscode@users.noreply.github.com>
2024-10-14 14:47:50 +0200

    chore: show sol address and improve empty states (#1399)

--- src/components/ProfilePage/AddressBox/AddressBox.tsx
+++ src/components/ProfilePage/AddressBox/AddressBox.tsx
@@ -5,6 +5,7 @@ import { useTheme } from '@mui/material';
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 import { useMercleNft } from 'src/hooks/useMercleNft';
+import { getAddressLabel } from 'src/utils/getAddressLabel';
 import type { Address } from 'viem';
 import { useEnsName } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
@@ -36,18 +37,11 @@ export const AddressBox = ({ address, isEVM }: AddressBoxProps) => {
     setSnackbarState(true, t('navbar.walletMenu.copiedMsg'), 'success');
   };
 
-  const getAddressOrENSString = (): string => {
-    if (isSuccess && ensName) {
-      return String(ensName).length > 20
-        ? `${ensName.slice(0, 13)}...eth`
-        : ensName;
-    }
-    return address && isEVM
-      ? address?.slice(0, 6) +
-          '...' +
-          address?.slice(address.length - 4, address.length)
-      : '0x0000...0000';
-  };
+  const addressLabel = getAddressLabel({
+    isSuccess,
+    ensName,
+    address,
+  });
 
   const imgLink = imageLink
     ? imageLink
@@ -92,7 +86,7 @@ export const AddressBox = ({ address, isEVM }: AddressBoxProps) => {
           width={'100%'}
           textAlign={'center'}
         >
-          {getAddressOrENSString()}
+          {addressLabel}
         </NoSelectTypography>
         <ProfileIconButton onClick={() => handleCopyButton()}>
           <ContentCopyIcon sx={{ height: '16px' }} />
