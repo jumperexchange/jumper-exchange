7b177d3240799fa09560c6acbaf6c54b4790b113
dennyscode <43956540+dennyscode@users.noreply.github.com>
2024-10-14 15:13:13 +0200

    chore: add no-console lint rule (#1302)

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -117,7 +117,7 @@ test.describe('Jumper full e2e flow', () => {
     const sectionName = [
       'Announcements',
       'Partnerships',
-      'Tutorial', 
+      'Tutorial',
       'Knowledge',
     ];
     const socialNetworks = ['LinkedIn', 'Facebook', 'X'];
@@ -142,14 +142,15 @@ test.describe('Jumper full e2e flow', () => {
   });
 
   test('Should be able to navigate to LI.FI Scan', async ({ page }) => {
-    const searchBar= await page.locator('xpath=//div[@class="MuiBox-root mui-1nhlr6a"]');
+    const searchBar = await page.locator(
+      'xpath=//div[@class="MuiBox-root mui-1nhlr6a"]',
+    );
     await openOrCloseMainMenu(page);
     await expectMenuToBeVisible(page);
     await itemInMenu(page, 'Jumper Scan');
     // const newPage = await page.waitForEvent('popup', { timeout: 15000 });
     await expect(page).toHaveURL(values.localJumperScanURL);
     await expect(searchBar).toBeVisible();
-
   });
 
   test.skip('Should be able to navigate to Supefest', async ({ page }) => {
