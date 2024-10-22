8771d10e331cf25fc2494448fcc37539920ef0ee
Majorfi <major@ycorpo.com>
2024-10-22 18:42:18 +0200

    chores: testing plugin

--- src/wash/contexts/useWashTrading.tsx
+++ src/wash/contexts/useWashTrading.tsx
@@ -12,7 +12,7 @@ import { useWidgetEvents, WidgetEvent } from '@lifi/widget';
 import type { ReactElement } from 'react';
 import type { TGetCollection } from '../hooks/useGetCollection';
 import type { TRevealHook } from '../hooks/useReveal';
-import type { Process, Route } from '@lifi/widget';
+import type { Process, Route, RouteExecutionUpdate } from '@lifi/widget';
 import type { TGetQuests } from '../hooks/useGetQuests';
 import { useGetQuests } from '../hooks/useGetQuests';
 
@@ -88,34 +88,73 @@ export function WashTradingContextApp({
   const widgetEvents = useWidgetEvents();
 
   useEffect(() => {
-    const onRouteExecutionCompleted = (props: {
-      process: Process;
-      route: Route;
-    }): void => {
-      console.warn('route', props.route);
-      fetch('/api/wash', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id: props.route.id,
-          fromAddress: props.route.fromAddress,
-          fromAmount: props.route.fromAmount,
-          fromAmountUSD: props.route.fromAmountUSD,
-          fromChainID: props.route.fromChainId,
-          toAddress: props.route.toAddress,
-          toAmount: props.route.toAmount,
-          toAmountUSD: props.route.toAmountUSD,
-          toChainID: props.route.toChainId,
-        }),
-      });
+    const onRouteExecutionFailed = (props: RouteExecutionUpdate): void => {
+      console.warn('Failed', props);
+      const txHash = props.process.txHash;
+      if (txHash) {
+        fetch('/api/wash', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id: props.route.id,
+            txHash,
+            fromAddress: props.route.fromAddress,
+            fromAmount: props.route.fromAmount,
+            fromAmountUSD: props.route.fromAmountUSD,
+            fromChainID: props.route.fromChainId,
+            toAddress: props.route.toAddress,
+            toAmount: props.route.toAmount,
+            toAmountUSD: props.route.toAmountUSD,
+            toChainID: props.route.toChainId,
+          }),
+        });
+      }
+    };
+
+    const onRouteExecutionCompleted = (route: Route): void => {
+      console.warn('Success', route);
+      let txHash: string | undefined = undefined;
+      if (route.steps.length > 0) {
+        const firstStep = route.steps[0] as LiFiStep & {
+          execution: { process: Process[] };
+        };
+        if ((firstStep?.execution?.process || []).length > 0) {
+          const firstExecution = firstStep.execution.process[0];
+          if (firstExecution) {
+            txHash = firstExecution.txHash;
+          }
+        }
+      }
+
+      if (txHash) {
+        fetch('/api/wash', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id: route.id,
+            txHash,
+            fromAddress: route.fromAddress,
+            fromAmount: route.fromAmount,
+            fromAmountUSD: route.fromAmountUSD,
+            fromChainID: route.fromChainId,
+            toAddress: route.toAddress,
+            toAmount: route.toAmount,
+            toAmountUSD: route.toAmountUSD,
+            toChainID: route.toChainId,
+          }),
+        });
+      }
     };
 
     widgetEvents.on(
-      WidgetEvent.RouteExecutionUpdated,
+      WidgetEvent.RouteExecutionCompleted,
       onRouteExecutionCompleted,
     );
+    widgetEvents.on(WidgetEvent.RouteExecutionFailed, onRouteExecutionFailed);
 
     return () =>
       widgetEvents.off(
