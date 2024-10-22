e95b38d1ed608b3f653d1b7db8679a28ad28da51
Majorfi <major@ycorpo.com>
2024-10-22 19:21:45 +0200

    fix: widgetEvents.off

--- src/wash/contexts/useWashTrading.tsx
+++ src/wash/contexts/useWashTrading.tsx
@@ -113,7 +113,11 @@ export function WashTradingContextApp({
       onRouteExecutionCompleted,
     );
 
-    return () => widgetEvents.all.clear();
+    return () =>
+      widgetEvents.off(
+        WidgetEvent.RouteExecutionCompleted,
+        onRouteExecutionCompleted,
+      );
   }, [widgetEvents]);
 
   return (
