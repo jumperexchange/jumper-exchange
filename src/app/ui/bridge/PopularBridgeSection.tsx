30a36630c2804a3b9263315d6a46d2911dd6be8a
BRAMILLE Sébastien <2752200+oktapodia@users.noreply.github.com>
2024-10-22 13:56:02 +0100

    fix: seo pages broken links (#1432)

--- src/app/ui/bridge/PopularBridgeSection.tsx
+++ src/app/ui/bridge/PopularBridgeSection.tsx
@@ -33,6 +33,18 @@ const PopularBridgeSection = ({
           .filter((token) => {
             return token.chainId !== destinationChain.id;
           })
+          .filter((token) => {
+            const isTokenExistsInSourceChain = tokens[token.chainId].some(
+              (tk) => tk.symbol === sourceToken.symbol,
+            );
+            const isTokenExistsInDestinationChain = tokens[
+              destinationChain.id
+            ].some((tk) => tk.symbol === destinationToken.symbol);
+
+            return (
+              isTokenExistsInDestinationChain && isTokenExistsInSourceChain
+            );
+          })
           .map((token) => (
             <MuiLink
               width="50%"
