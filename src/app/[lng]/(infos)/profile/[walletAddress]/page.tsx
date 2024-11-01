d527ecd719797d0cc88c174bdeb761121c663adb
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2024-11-01 15:28:04 +0000

    Add metadata

--- src/app/[lng]/(infos)/profile/[walletAddress]/page.tsx
+++ src/app/[lng]/(infos)/profile/[walletAddress]/page.tsx
@@ -1,4 +1,21 @@
 import ProfilePage from '@/app/ui/profile/ProfilePage';
+import type { Metadata } from 'next';
+
+type Props = {
+  params: { walletAddress: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    openGraph: {
+      title: `Profile of ${params.walletAddress}`,
+      description: `Profile of ${params.walletAddress}`,
+      type: 'profile',
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/profile/${params.walletAddress}`,
+      images: `${process.env.NEXT_PUBLIC_SITE_URL}/api/profile/${params.walletAddress}`,
+    },
+  };
+}
 
 export default async function Page({
   params,
