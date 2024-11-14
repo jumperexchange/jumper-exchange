3c3b84652b2f8c6d2fedda760388ac13038c2793
Denny San <sander.denny@gmx.de>
2024-11-14 09:53:05 +0100

    refactor: ternaries

--- src/app/[lng]/(infos)/learn/[slug]/page.tsx
+++ src/app/[lng]/(infos)/learn/[slug]/page.tsx
@@ -61,10 +61,10 @@ export default async function Page({ params }: { params: { slug: string } }) {
   const { activeThemeMode } = getCookies();
 
   const currentTags = (
-    article?.data.data[0] as BlogArticleData
-  ).attributes?.tags.data.map((el) => el?.id);
+    article?.data?.data[0] as BlogArticleData
+  )?.attributes?.tags.data.map((el) => el?.id);
   const relatedArticles = await getArticlesByTag(
-    article.data.data[0]?.id,
+    article?.data?.data[0]?.id,
     currentTags,
   );
 
