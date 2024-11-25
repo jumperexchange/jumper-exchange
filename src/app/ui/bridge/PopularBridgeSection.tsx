c3630daf9d5a94c908dd76b7cbda000a9c009edf
BRAMILLE Sébastien <2752200+oktapodia@users.noreply.github.com>
2024-11-25 14:57:11 +0000

    feat: Enhance popular bridges (#1521)

--- src/app/ui/bridge/PopularBridgeSection.tsx
+++ src/app/ui/bridge/PopularBridgeSection.tsx
@@ -15,6 +15,8 @@ interface PopularBridgeProps {
   tokens: TokensResponse['tokens'];
 }
 
+const NUMBER_OF_TOKENS = 11;
+
 const PopularBridgeSection = ({
   sourceChain,
   sourceToken,
@@ -23,41 +25,52 @@ const PopularBridgeSection = ({
   chains,
   tokens,
 }: PopularBridgeProps) => {
+  const sameSymbolTokens = Object.values(tokens)
+    .flat()
+    .filter(
+      (token) =>
+        token.symbol === sourceToken.symbol &&
+        token.chainId !== destinationChain.id,
+    );
+
+  const otherTokens = Object.values(tokens)
+    .flat()
+    .filter(
+      (token) =>
+        token.chainId !== destinationChain.id &&
+        token.symbol !== sourceToken.symbol,
+    )
+    .filter(
+      (token, index, self) =>
+        self.findIndex((t) => t.chainId === token.chainId) === index,
+    );
+  const popularBridges = [...sameSymbolTokens, ...otherTokens]
+    .slice(0, NUMBER_OF_TOKENS)
+    .filter(
+      (token) =>
+        token.symbol !== sourceToken.symbol ||
+        token.chainId !== sourceToken.chainId,
+    );
+
   return (
     <BridgePageContainer width="100%">
       <Typography variant="h3" marginY={2}>
         Popular bridges
       </Typography>
       <Stack direction="row" flexWrap="wrap">
-        {getTokenByName(tokens, destinationToken?.name ?? '')
-          .filter((token) => {
-            return token.chainId !== destinationChain.id;
-          })
-          .filter((token) => {
-            const isTokenExistsInSourceChain = tokens[token.chainId].some(
-              (tk) => tk.symbol === sourceToken.symbol,
-            );
-            const isTokenExistsInDestinationChain = tokens[
-              destinationChain.id
-            ].some((tk) => tk.symbol === destinationToken.symbol);
-
-            return (
-              isTokenExistsInDestinationChain && isTokenExistsInSourceChain
-            );
-          })
-          .map((token) => (
-            <MuiLink
-              width="50%"
-              color="text.primary"
-              key={generateKey(token.address)}
-              component={Link}
-              href={`/bridge/${getChainById(chains, token.chainId)?.name}-${sourceToken.symbol}-to-${destinationChain?.name}-${destinationToken?.symbol}`.toLowerCase()}
-            >
-              Bridge from {sourceToken.symbol} on{' '}
-              {getChainById(chains, token.chainId)?.name} to{' '}
-              {destinationToken?.symbol} on {destinationChain?.name}
-            </MuiLink>
-          ))}
+        {popularBridges.map((token) => (
+          <MuiLink
+            width="50%"
+            color="text.primary"
+            key={generateKey(token.address)}
+            component={Link}
+            href={`/bridge/${getChainById(chains, token.chainId)?.name}-${token.symbol}-to-${destinationChain?.name}-${destinationToken?.symbol}`.toLowerCase()}
+          >
+            Bridge from {token.symbol} on{' '}
+            {getChainById(chains, token.chainId)?.name} to{' '}
+            {destinationToken?.symbol} on {destinationChain?.name}
+          </MuiLink>
+        ))}
       </Stack>
     </BridgePageContainer>
   );
