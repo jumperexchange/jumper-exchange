d8126ca3d4e0a003403b929ef878c49ef951a67d
Thomas Cherret <b00705488@essec.edu>
2024-11-27 18:32:14 +0100

    automatic push

--- src/components/Berachain/const/berachainExampleData.ts
+++ src/components/Berachain/const/berachainExampleData.ts
@@ -2,7 +2,7 @@ import type { BerachainProtocolInfo } from '../berachain.types';
 
 export const berachainMarkets: BerachainProtocolInfo[] = [
   {
-    slug: 'dolomite-vault-market-weth',
+    slug: 'dolomite-vault-market-dai',
     chain: 1,
     tokens: [
       '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
@@ -55,7 +55,7 @@ export const berachainMarkets: BerachainProtocolInfo[] = [
     },
   },
   {
-    slug: 'infrared-vault-market-usdc',
+    slug: 'infrared-vault-market-dai',
     chain: 1,
     tokens: ['0x6B175474E89094C44Da98b954EedeAC495271d0F'],
     tvl: '$21.00',
@@ -105,7 +105,7 @@ export const berachainMarkets: BerachainProtocolInfo[] = [
     },
   },
   {
-    slug: 'zerolend-vault-market-dai',
+    slug: 'zerolend-vault-market-usdc',
     chain: 1,
     tokens: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
     tvl: '$2.00',
