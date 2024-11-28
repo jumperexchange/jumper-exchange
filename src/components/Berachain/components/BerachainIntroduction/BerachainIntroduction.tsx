384faca2cc86dc232570d2181dfbf7c99385f96c
Thomas Cherret <b00705488@essec.edu>
2024-11-28 11:38:33 +0100

    automatic push

--- src/components/Berachain/components/BerachainIntroduction/BerachainIntroduction.tsx
+++ src/components/Berachain/components/BerachainIntroduction/BerachainIntroduction.tsx
@@ -18,54 +18,61 @@ export const BerachainIntroduction = () => {
         How does it work?
       </BerachainIntroductionTitle>
       <BerachainIntroductionSteps>
-        <BerachainIntroductionStep>
-          <BerachainIntroductionStepContent>
-            <Typography variant="urbanistBody2XLarge">
-              1. Browse markets
-            </Typography>
-            <Typography variant="bodyMedium">
-              Browse incentivised Boyco markets on mainnet.
-            </Typography>
-          </BerachainIntroductionStepContent>
-          <BerachainBearTyping
-            src={'/berachain/berachain-bear-typing.png'}
-            alt={'Berachain on typing machine'}
-            width={385}
-            height={385}
-          />
-        </BerachainIntroductionStep>
-        <BerachainIntroductionStep>
-          <BerachainIntroductionStepContent>
-            <Typography variant="urbanistBody2XLarge">
-              2. Deposit tokens
-            </Typography>
-            <Typography variant="bodyMedium">
-              Select the dApp and pool you would like to deposit liquidity into.
-            </Typography>
-          </BerachainIntroductionStepContent>
-          <BerachainIntroductionStepIllustration
-            src={'/berachain/berachain-bear-loves-honey.png'}
-            alt={'Berachain on typing machine'}
-            width={385}
-            height={385}
-          />
-        </BerachainIntroductionStep>
-        <BerachainIntroductionStep>
-          <BerachainIntroductionStepContent>
-            <Typography variant="urbanistBody2XLarge">
-              3. Receive incentives
-            </Typography>
-            <Typography variant="bodyMedium">
-              Sit back, relax, earn incentives while Berachain goes live.
-            </Typography>
-          </BerachainIntroductionStepContent>
-          <BerachainIntroductionStepIllustration
-            src={'/berachain/berachain-bear-chillin.png'}
-            alt={'Berachain on typing machine'}
-            width={385}
-            height={385}
-          />
-        </BerachainIntroductionStep>
+        <Link href="/berachain/explore">
+          <BerachainIntroductionStep>
+            <BerachainIntroductionStepContent>
+              <Typography variant="urbanistBody2XLarge">
+                1. Browse markets
+              </Typography>
+              <Typography variant="bodyMedium">
+                Browse incentivised Boyco markets on mainnet.
+              </Typography>
+            </BerachainIntroductionStepContent>
+            <BerachainBearTyping
+              src={'/berachain/berachain-bear-typing.png'}
+              alt={'Berachain on typing machine'}
+              width={385}
+              height={385}
+            />
+          </BerachainIntroductionStep>
+        </Link>
+        <Link href="/berachain/explore">
+          <BerachainIntroductionStep>
+            <BerachainIntroductionStepContent>
+              <Typography variant="urbanistBody2XLarge">
+                2. Deposit tokens
+              </Typography>
+              <Typography variant="bodyMedium">
+                Select the dApp and pool you would like to deposit liquidity
+                into.
+              </Typography>
+            </BerachainIntroductionStepContent>
+            <BerachainIntroductionStepIllustration
+              src={'/berachain/berachain-bear-loves-honey.png'}
+              alt={'Berachain on typing machine'}
+              width={385}
+              height={385}
+            />
+          </BerachainIntroductionStep>
+        </Link>
+        <Link href="/berachain/explore">
+          <BerachainIntroductionStep>
+            <BerachainIntroductionStepContent>
+              <Typography variant="urbanistBody2XLarge">
+                3. Receive incentives
+              </Typography>
+              <Typography variant="bodyMedium">
+                Sit back, relax, earn incentives while Berachain goes live.
+              </Typography>
+            </BerachainIntroductionStepContent>
+            <BerachainIntroductionStepIllustration
+              src={'/berachain/berachain-bear-chillin.png'}
+              alt={'Berachain on typing machine'}
+              width={385}
+              height={385}
+            />
+          </BerachainIntroductionStep>
+        </Link>
       </BerachainIntroductionSteps>
       <Link href="/berachain/explore">
         <BerachainWelcomeConnectButtonCTA>
