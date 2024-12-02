0fb3bec93e385bdfc7e681d774eacba4f2c2bbea
dennyscode <43956540+dennyscode@users.noreply.github.com>
2024-12-02 14:14:33 +0100

    chore: remove new label on multiportfolio and fix z indexes (#1559)

--- src/components/Navbar/WalletButton.tsx
+++ src/components/Navbar/WalletButton.tsx
@@ -21,7 +21,6 @@ import {
 import { useLoyaltyPass } from 'src/hooks/useLoyaltyPass';
 import { JUMPER_WASH_PATH } from '../../const/urls';
 import { XPIcon } from '../illustrations/XPIcon';
-import { PromoLabel } from '../PromoLabel.style';
 import {
   ConnectButton,
   ConnectButtonLabel,
@@ -129,15 +128,7 @@ export const WalletButtons = () => {
           <WalletMenuButton
             id="wallet-digest-button"
             onClick={handleWalletMenuClick}
-            sx={{
-              // delete with PromoLabel
-              overflow: 'visible',
-              position: 'relative',
-            }}
           >
-            {/* when deleting PromoLabel, also clear related sx-styles in parent above */}
-            <PromoLabel component="span" variant="bodyXSmallStrong" />
-
             {isSuccess && activeChain ? (
               <WalletMgmtBadge
                 overlap="circular"
