e816218a275d1ce9696df1720a87030fa58a1b0d
Bojan Kopunovic <bkopunovic93@gmail.com>
2024-12-03 09:51:59 +0100

    fix: add more workers in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 2,
+  retries: process.env.CI ? 3 : 3,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], ['json', { outputFile: 'test-results.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
