5f6be6f0611d96e563c5d2856f06e68ad7b941d3
Bojan Kopunovic <bkopunovic93@gmail.com>
2024-12-04 14:49:14 +0100

    fix: remove --force flag from builiding synpress cache command

--- tests/connectWallet.spec.ts
+++ tests/connectWallet.spec.ts
@@ -7,16 +7,16 @@ import { execSync } from 'child_process';
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
 
 const { expect } = test;
-test.beforeAll(()=>{
+test.beforeAll(() => {
   console.log('Building Synpress cache...');
   try {
-    execSync('yarn build:cache --force tests/wallet-setup/', { stdio: 'inherit' });
+    execSync('yarn build:cache tests/wallet-setup/', { stdio: 'inherit' });
     console.log('Synpress cache build complete.');
   } catch (error) {
     console.error('Failed to build Synpress cache:', error);
     throw error; // Fail the test suite if the cache build fails
   }
-})
+});
 test('should connect wallet to Jumper', async ({
   context,
   page,
@@ -31,7 +31,7 @@ test('should connect wallet to Jumper', async ({
   const connectWalletButton = page.locator('#connect-wallet-button');
   const metaMaskWalletOption = page.locator('xpath=//span[normalize-space(text())="MetaMask"]');
   const availableMissionTitle = page.locator('xpath=//p[normalize-space(text())="Available Missions"]');
-  expect(connectWalletButton).toBeEnabled();
+  await expect(connectWalletButton).toBeEnabled();
   await connectWalletButton.click();
   await metaMaskWalletOption.click();
   await metamask.connectToDapp(['Account 1']);
