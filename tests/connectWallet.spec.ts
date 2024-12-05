48a3a03cc8906c8fbba87fa41c38f7dc4ea897f8
Bojan Kopunovic <bkopunovic93@gmail.com>
2024-12-05 16:11:27 +0100

    fix: modify test to include CI commands

--- tests/connectWallet.spec.ts
+++ tests/connectWallet.spec.ts
@@ -1,13 +1,24 @@
+import { execSync } from 'child_process';
 import { testWithSynpress } from '@synthetixio/synpress-core';
 import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress';
-import { openOrCloseMainMenu , itemInMenu, closeWelcomeScreen } from './testData/commonFunctions';
+import { openOrCloseMainMenu, itemInMenu, closeWelcomeScreen } from './testData/commonFunctions';
 import basicSetup from './wallet-setup/basic.setup';
 
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
-
 const { expect } = test;
 
-test.describe('Connect Metamask with Jumper app and open /profile page',()=>{
+test.beforeAll(async () => {
+  console.log('Starting local environment...');
+  execSync('yarn dev &', { stdio: 'inherit' }); 
+  console.log('Waiting for localhost:3000...');
+  execSync('npx wait-on http://localhost:3000 --timeout 300000 --interval 2000 --log-level error', {
+    stdio: 'inherit',
+  }); 
+  console.log('Building cache...');
+  execSync('yarn build:cache:ci --force tests/wallet-setup/', { stdio: 'inherit' });
+});
+
+test.describe('Connect Metamask with Jumper app and open /profile page', () => {
   test('should connect wallet to Jumper', async ({
     context,
     page,
@@ -27,10 +38,10 @@ test.describe('Connect Metamask with Jumper app and open /profile page',()=>{
     await connectWalletButton.click();
     await metaMaskWalletOption.click();
     await metamask.connectToDapp(['Account 1']);
-    await closeWelcomeScreen(page)
+    await closeWelcomeScreen(page);
     await openOrCloseMainMenu(page);
     await itemInMenu(page, 'Jumper Profile');
     await page.locator('.profile-page').isVisible();
     await expect(availableMissionTitle).toBeVisible();
   });
-})
+});
