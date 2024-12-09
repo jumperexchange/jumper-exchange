df9dc2643302e4ef31c5362efcdcbec9baf9134e
Thomas Cherret <b00705488@essec.edu>
2024-12-09 21:34:58 +0100

    automatic push

--- src/components/ProfilePage/LevelBox/PointsBox.tsx
+++ src/components/ProfilePage/LevelBox/PointsBox.tsx
@@ -9,7 +9,11 @@ interface PointsBoxProps {
 
 export const PointsBox = ({ points }: PointsBoxProps) => {
   const currentDate = new Date();
-  const formattedDate = currentDate.toLocaleDateString('en-US', options);
+  const formattedDate = currentDate.toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
 
   return (
     <Box>
