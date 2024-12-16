11bcbb1c14c7cb332827cde01f28bf69d9c89bc0
Denny San <sander.denny@gmx.de>
2024-12-16 12:22:19 +0100

    refactor: remove unused styled component prop

--- src/app/ui/bridge/BridgePage.style.ts
+++ src/app/ui/bridge/BridgePage.style.ts
@@ -3,9 +3,7 @@
 import type { BoxProps } from '@mui/system';
 import { Box, styled } from '@mui/system';
 
-export const BridgePageContainer = styled(Box, {
-  shouldForwardProp: (prop) => prop !== 'active',
-})<BoxProps>(({ theme }) => ({
+export const BridgePageContainer = styled(Box)<BoxProps>(({ theme }) => ({
   color: theme.palette.text.primary,
   maxWidth: '100%',
   flexGrow: 1,
