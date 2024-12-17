079376d28a0367ae99006acc75832ed9b1f35d00
Denny San <sander.denny@gmx.de>
2024-12-17 14:46:41 +0100

    refactor: remove duplicate

--- src/app/ui/swap/utils.tsx
+++ src/app/ui/swap/utils.tsx
@@ -1,8 +1,8 @@
+import { currencyFormatter } from '@/utils/formatNumbers';
+import { getChainById } from '@/utils/tokenAndChain';
+import type { ExtendedChain, Token } from '@lifi/sdk';
 import { Link as MuiLink } from '@mui/material';
 import Link from 'next/link';
-import type { ExtendedChain, Token } from '@lifi/sdk';
-import { getChainById } from '@/utils/tokenAndChain';
-import { currencyFormatter } from '@/utils/formatNumbers';
 
 export function buildExplorerLink(
   blockExplorerUrls: string[] = [],
@@ -22,29 +22,6 @@ export function buildExplorerLink(
       {address}
     </MuiLink>
   );
-}
-
-export function getChainInfoData(chainInfo: ExtendedChain) {
-  return [
-    { label: 'Native token', value: chainInfo.nativeToken.symbol },
-    { label: 'Chain type', value: chainInfo.chainType },
-    { label: 'Chain id', value: chainInfo.id },
-    {
-      label: 'Block explorer urls',
-      value: chainInfo.metamask?.blockExplorerUrls?.map((blockExplorerUrl) => (
-        <MuiLink
-          color="text.primary"
-          sx={{ marginRight: 2 }}
-          component={Link}
-          target="_blank"
-          href={blockExplorerUrl}
-          key={blockExplorerUrl}
-        >
-          {blockExplorerUrl}
-        </MuiLink>
-      )),
-    },
-  ];
 }
 
 export function getTokenInfoData(chains: ExtendedChain[], tokenInfo: Token) {
