d5baa3bdfe0f282ff1cf1072c528575835888bf1
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2025-01-08 11:58:13 +0000

    Fix lint

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -33,7 +33,12 @@ export const Banner = () => {
     <a
       href="https://wrapped.jumper.exchange"
       target="_blank"
-      style={{ textDecoration: 'none', color: 'inherit', position:'relative', zIndex: 1 }}
+      style={{
+        textDecoration: 'none',
+        color: 'inherit',
+        position: 'relative',
+        zIndex: 1,
+      }}
       rel="noreferrer"
     >
       <BannerContainer onClick={handleClick}>
