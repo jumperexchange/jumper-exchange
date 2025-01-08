6b0048d9756a43bda03e2e5880048f144e36003d
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2025-01-08 11:10:00 +0000

    Update packages + enable wrapped banner on bera

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -33,7 +33,7 @@ export const Banner = () => {
     <a
       href="https://wrapped.jumper.exchange"
       target="_blank"
-      style={{ textDecoration: 'none', color: 'inherit' }}
+      style={{ textDecoration: 'none', color: 'inherit', position:'relative', zIndex: 1 }}
       rel="noreferrer"
     >
       <BannerContainer onClick={handleClick}>
