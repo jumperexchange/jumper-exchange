8ad0820c2411bf3362322421170073c8696aa954
tche <tche@tches-MacBook-Pro.local>
2025-01-13 15:29:55 +0100

    chore: fix padding on styled

--- src/components/ZapWidget/Deposit/DepositCard.style.ts
+++ src/components/ZapWidget/Deposit/DepositCard.style.ts
@@ -3,8 +3,10 @@ import { alpha, Box, styled } from '@mui/material';
 export const ColoredStatBox = styled(Box)(({ theme }) => ({
   backgroundColor: alpha(theme.palette.primary.main, 0.08),
   alignItems: 'flex-start',
-  borderRadius: 1,
-  paddingX: 2,
-  paddingY: 1,
+  borderRadius: '16px',
+  paddingRight: '16px',
+  paddingLeft: '16px',
+  paddingTop: '8px',
+  paddingBottom: '8px',
   flex: 1,
 }));
