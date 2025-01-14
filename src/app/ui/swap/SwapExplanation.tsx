a8c284db127308d980e741050a5695f0c4c2c5e9
tche <tche@tches-MacBook-Pro.local>
2025-01-14 21:58:29 +0100

    chore: add faq questions

--- src/app/ui/swap/SwapExplanation.tsx
+++ src/app/ui/swap/SwapExplanation.tsx
@@ -5,8 +5,22 @@ import { DynamicPagesContainer } from 'src/components/DynamicPagesContainer';
 
 const SwapExplanationSection = () => {
   const faqData = [
-    { Question: 'Some Question here 1', Answer: 'Some Answer here 1' },
-    { Question: 'Some Question here 2', Answer: 'Some Answer here 2' },
+    {
+      Question: 'What are cryptocurrencies swaps?',
+      Answer: `a swap is the exchange of one cryptocurrency for another, typically through a decentralized exchange (DEX) or automated market maker (AMM). Swaps bypass intermediaries, allowing users to trade tokens directly from their wallets. They're fast, cost-efficient, and key to DeFi.`,
+    },
+    {
+      Question: 'What is a swap aggregator?',
+      Answer: `A swap aggregator in crypto connects to multiple decentralized exchanges (DEXs) to find the best rates for token swaps. It compares prices, fees, liquidity, and slippage across platforms in real-time, often splitting trades for optimal results. This ensures users get the most value by saving time, reducing costs, and maximizing tokens received in a single transaction.`,
+    },
+    {
+      Question: 'Why use an aggregrator to do swaps in crypto?',
+      Answer: `Using a swap aggregator in crypto ensures you get the best price by comparing rates across multiple swap providers (DEXs). Aggregators find the most efficient route, minimizing slippage and fees. They may split your trade across platforms for better outcomes. This saves time, reduces costs, and enhances liquidity access, offering more transparency and value than using a single provider.`,
+    },
+    {
+      Question: 'Is it safe to swap cryptocurrencies?',
+      Answer: `Swapping crypto can be safe if done on reputable platforms with audited smart contracts. Decentralized exchanges (DEXs) reduce custodial risks since users retain control of their assets. However, risks like hacks, smart contract bugs, or phishing scams exist. To stay safe, research the platform, check fees, and ensure you’re using official sites or wallets. Caution and due diligence are key.`,
+    },
   ];
   return (
     <DynamicPagesContainer
