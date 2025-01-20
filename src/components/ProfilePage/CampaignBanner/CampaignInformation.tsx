784238f442f603139317aa7278675e475928020c
tche <tche@tches-MacBook-Pro.local>
2025-01-20 18:07:44 +0100

    chore: review comments

--- src/components/ProfilePage/CampaignBanner/CampaignInformation.tsx
+++ src/components/ProfilePage/CampaignBanner/CampaignInformation.tsx
@@ -1,18 +1,19 @@
-import { useTranslation } from 'react-i18next';
-import { useOngoingQuests } from 'src/hooks/useOngoingQuests';
-import { Button } from 'src/components/Button';
+import { BannerButton, Button } from 'src/components/Button';
 import { Box, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
 import {
-  CampaignBox,
   CampaignInfoVerticalBox,
   CampaignTagBox,
 } from './CampaignBanner.style';
-import Image from 'next/image';
 import { getContrastAlphaColor } from 'src/utils/colors';
-import { openInNewTab } from 'src/utils/openInNewTab';
 import { useRouter } from 'next/navigation';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { JUMPER_CAMPAIGN_PATH } from 'src/const/urls';
+import {
+  TrackingAction,
+  TrackingCategory,
+  TrackingEventParameter,
+} from 'src/const/trackingKeys';
+import { useUserTracking } from 'src/hooks/userTracking';
 
 export const CampaignInformation = ({
   tag,
@@ -21,10 +22,9 @@ export const CampaignInformation = ({
   tag: string;
   description: string;
 }) => {
-  const { t } = useTranslation();
+  const { trackEvent } = useUserTracking();
   const theme = useTheme();
   const router = useRouter();
-  const { quests, isLoading: isQuestsLoading, url } = useOngoingQuests();
   const isMobile = useMediaQuery((theme: Theme) =>
     theme.breakpoints.down('md'),
   );
@@ -67,37 +67,17 @@ export const CampaignInformation = ({
         </Typography>
       </Box>
 
-      <Button
-        variant="secondary"
+      <BannerButton
         onClick={() => {
-          // trackEvent({
-          //   category: TrackingCategory.Pageload,
-          //   action: TrackingAction.PageLoad,
-          //   label: 'error-discord-jumper',
-          //   data: {
-          //     [TrackingEventParameter.PageloadSource]:
-          //       TrackingCategory.ErrorPage,
-          //     [TrackingEventParameter.PageloadDestination]:
-          //       'discord-jumper',
-          //     [TrackingEventParameter.PageloadURL]: DISCORD_URL,
-          //     [TrackingEventParameter.PageloadExternal]: true,
-          //   },
-          // });
+          trackEvent({
+            category: TrackingCategory.CampaignBanner,
+            action: TrackingAction.ClickCampaignBanner,
+            label: 'click-campaign-banner',
+            data: {
+              [TrackingEventParameter.ActiveCampaignBanner]: 'lisk',
+            },
+          });
           router.push(`${JUMPER_CAMPAIGN_PATH}/lisk`);
-        }}
-        styles={{
-          marginTop: isMobile ? '16px' : undefined,
-          gap: '8px',
-          borderRadius: '24px',
-          '> button:hover': {
-            backgroundColor: getContrastAlphaColor(theme, '4%'),
-          },
-          '> button:hover svg': {
-            fill:
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[700]
-                : theme.palette.grey[300],
-          },
         }}
         fullWidth={true}
       >
@@ -105,7 +85,7 @@ export const CampaignInformation = ({
           Explore
         </Typography>
         <ArrowForwardIcon />
-      </Button>
+      </BannerButton>
     </CampaignInfoVerticalBox>
   );
 };
