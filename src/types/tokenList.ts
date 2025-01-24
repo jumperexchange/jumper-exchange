0586124d1bfffbe7bcd4342461e43162eb8c0df0
Sarah Barker <hello@nursesarahbarker.com>
2025-01-24 08:56:32 +0000

    Update tokenList.ts

--- src/types/tokenList.ts
+++ src/types/tokenList.ts
@@ -1,5 +1,7 @@
+
 export interface StrapiTokenInfo {
-  address: string;
-  chainId: number;
-  logoURI?: string;
-}
+    address: '0x8B8CAAABE9f8a4431A559eAa3690db69fE1fE1eb',
+    chainId: 8453,
+    logoURI: 'https://bloxology.site/wp-content/uploads/2025/01/Magenta.png',
+  }
+
