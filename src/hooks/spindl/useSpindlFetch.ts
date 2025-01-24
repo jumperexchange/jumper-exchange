b3f44f4bf4ec1164e20bb454b102a26ddf055187
Denny San <sander.denny@gmx.de>
2025-01-24 16:30:48 +0100

    chore: return only one spindl cards

--- src/hooks/spindl/useSpindlFetch.ts
+++ src/hooks/spindl/useSpindlFetch.ts
@@ -19,16 +19,17 @@ export const useSpindlFetch = () => {
       address,
     });
     // console.log('DATA', data);
-    if (data) {
-      const featureCardsSpindle: FeatureCardData[] = (data as SpindlItem[]).map(
-        (item, index) => {
-          return {
-            id: `spindle-${index}`,
+    if (data && data.length) {
+      const firstItem = (data as SpindlItem[])[0]; // Get the first item from the array
+      if (firstItem) {
+        const featureCardsSpindle: FeatureCardData[] = [
+          {
+            id: `spindle-0`,
             attributes: {
-              Title: item.title, // used to set the title of the feature card
-              Subtitle: item.description, // used to set the subtitle of the feature card
-              CTACall: item.ctas[0].title, // used to set the CTA label of the feature card
-              URL: item.ctas[0].title,
+              Title: firstItem.title, // used to set the title of the feature card
+              Subtitle: firstItem.description, // used to set the subtitle of the feature card
+              CTACall: firstItem.ctas[0]?.title || '', // used to set the CTA label of the feature card
+              URL: firstItem.ctas[0]?.href || '', // used to set the URL of the feature card
               // TitleColor?: string, // @spindl: you can make use of this to set the color of the title
               // CTAColor?: string, // @spindl: you can make use this to set the color of the CTA
               DisplayConditions: { mode: 'light', showOnce: true },
@@ -36,7 +37,7 @@ export const useSpindlFetch = () => {
               updatedAt: Date.now().toString(),
               PersonalizedFeatureCard: true,
               publishedAt: Date.now().toString(),
-              uid: item.id,
+              uid: firstItem.id,
               // @spindl: @spindl: do we support two versions of one card (light and dark mode) or only show either of them per card?
               BackgroundImageLight: {
                 // @spindl: this would be the place for a light image bg
@@ -46,7 +47,7 @@ export const useSpindlFetch = () => {
                     alternativeText: 'Spindl ad img', // @spindl: can we supply an alternative text or use advertiser.name?
                     width: 384, // @spindl: bg img must have this width
                     height: 160, // @spindl: bg img must have this height
-                    url: item.imageUrl, // !!!! @spindl: problem: Images have texts baked in! We need to remove this and apply texts via Title, Subtitle and CTACall
+                    url: firstItem.imageUrl, // !!!! @spindl: problem: Images have texts baked in! We need to remove this and apply texts via Title, Subtitle and CTACall
                   },
                 },
               },
@@ -55,22 +56,22 @@ export const useSpindlFetch = () => {
                 data: {
                   id: 1,
                   attributes: {
-                    alternativeText: 'Spindl ad img', // @spindl: can we supply an alternative text or use advertiser.name?
+                    alternativeText: `${firstItem.advertiser.name} image`, // @spindl: can we supply an alternative text or use advertiser.name?
                     width: 384, // @spindl: bg img must have this width
                     height: 160, // @spindl: bg img must have this height
-                    url: item.imageUrl, // !!!! @spindl: problem: Images have texts baked in! We need to remove this and apply texts via Title, Subtitle and CTACall
+                    url: firstItem.imageUrl, // !!!! @spindl: problem: Images have texts baked in! We need to remove this and apply texts via Title, Subtitle and CTACall
                   },
                 },
               },
               spindleData: {
-                impression_id: item.impressionId,
-                ad_creative_id: item.advertiserId,
+                impression_id: firstItem.impressionId,
+                ad_creative_id: firstItem.advertiserId,
               },
             },
-          };
-        },
-      );
-      setSpindl(featureCardsSpindle);
+          },
+        ];
+        setSpindl(featureCardsSpindle);
+      }
     }
   };
 
