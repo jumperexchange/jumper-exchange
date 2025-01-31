99e97faea3447c14f57d39318e17d1d70ebc84c0
tche <tche@tches-MacBook-Pro.local>
2025-01-31 12:31:30 +0100

    chore: remove comment

--- src/config/widgetConfig.ts
+++ src/config/widgetConfig.ts
@@ -7,7 +7,6 @@ export const getDefaultWidgetTheme = (
   config: {
     appearance: theme.palette.mode,
     theme: {
-      // @ts-expect-error
       typography: {
         fontFamily: theme.typography.fontFamily,
       },
