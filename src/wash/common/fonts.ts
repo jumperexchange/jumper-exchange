498c81a380279dfd4056c73601c16bf8f4b82ba1
dennyscode <43956540+dennyscode@users.noreply.github.com>
2025-02-03 11:49:46 +0100

    refactor: remove washtrade

--- src/wash/common/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Titan_One } from 'next/font/google';
-
-export const titanOne = Titan_One({
-  weight: ['400'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--titan-one-font',
-});
