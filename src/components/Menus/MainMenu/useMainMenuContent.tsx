3fdd4fc296b4d0045547dcf97b9cbe50e1495c19
tche <tche@tches-MacBook-Pro.local>
2025-02-03 11:53:16 +0100

    chore: close boyco

--- src/components/Menus/MainMenu/useMainMenuContent.tsx
+++ src/components/Menus/MainMenu/useMainMenuContent.tsx
@@ -8,7 +8,6 @@ import {
 } from '@/const/trackingKeys';
 import {
   DISCORD_URL,
-  JUMPER_BOYCO_PATH,
   JUMPER_LEARN_PATH,
   JUMPER_LOYALTY_PATH,
   JUMPER_SCAN_PATH,
@@ -29,7 +28,6 @@ import { useTheme } from '@mui/material/styles';
 import { usePathname, useRouter } from 'next/navigation';
 import { useTranslation } from 'react-i18next';
 import { useThemeSwitchTabs } from './useThemeSwitchTabs';
-import { BoycoIcon } from '@/components/illustrations/BoycoIcon';
 
 export const useMainMenuContent = () => {
   const { t, i18n } = useTranslation();
@@ -142,22 +140,6 @@ export const useMainMenuContent = () => {
           label: `open_submenu_${MenuKeysEnum.Devs.toLowerCase()}`,
           data: { [TrackingEventParameter.Menu]: MenuKeysEnum.Devs },
         });
-      },
-    },
-    {
-      label: 'Jump into Boyco',
-      prefixIcon: <BoycoIcon />,
-      showMoreIcon: false,
-      link: { url: JUMPER_BOYCO_PATH },
-      onClick: () => {
-        trackEvent({
-          category: TrackingCategory.Menu,
-          label: 'click-jumper-pass-berachain',
-          action: TrackingAction.ClickJumperCampaignLink,
-          data: { [TrackingEventParameter.Menu]: 'berachain' },
-        });
-        closeAllMenus();
-        router.push(JUMPER_BOYCO_PATH);
       },
     },
     {
