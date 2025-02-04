26db56b61c7a063d81d1e188cfac7156d73ce887
Denny San <sander.denny@gmx.de>
2025-02-04 16:22:00 +0100

    refactor: defensive approach

--- src/hooks/spindl/fetchSpindl.ts
+++ src/hooks/spindl/fetchSpindl.ts
@@ -13,29 +13,29 @@ export const fetchSpindl = async ({
 }: Omit<SpindlFetchParams, 'enabled'>): Promise<SpindlFetchResult> => {
   const { getUrl } = getSpindlConfig();
   const url = getUrl({ address, country, chainId, tokenAddress });
-  if (url && process.env.NEXT_PUBLIC_SPINDL_API_KEY) {
-    const response = await fetch(url.href, {
-      method: 'GET',
-      headers: {
-        'X-API-ACCESS-KEY': process.env.NEXT_PUBLIC_SPINDL_API_KEY,
-      },
-    });
 
-    if (!response.ok) {
-      throw new Error(
-        `HTTP error while fetching Spindl cards! status: ${response.status}`,
-      );
-    }
-
-    // Type the response JSON as SpindlData
-    const jsonResponse: SpindlData = await response.json();
-
-    return {
-      data: jsonResponse?.items, // Return only items if needed
-    };
-  } else {
+  if (!url || !process.env.NEXT_PUBLIC_SPINDL_API_KEY) {
     return {
       data: undefined,
     };
   }
+  const response = await fetch(url.href, {
+    method: 'GET',
+    headers: {
+      'X-API-ACCESS-KEY': process.env.NEXT_PUBLIC_SPINDL_API_KEY,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      `HTTP error while fetching Spindl cards! status: ${response.status}`,
+    );
+  }
+
+  // Type the response JSON as SpindlData
+  const jsonResponse: SpindlData = await response.json();
+
+  return {
+    data: jsonResponse?.items, // Return only items if needed
+  };
 };
