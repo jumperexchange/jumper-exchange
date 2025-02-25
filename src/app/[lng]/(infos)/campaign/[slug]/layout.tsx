f26b299e0f92b93d32335171a35c4651c4a75115
tche <tche@tches-MacBook-Pro.local>
2025-02-25 17:20:26 +0100

    chore: add berachain campaign v2

---
+++ src/app/[lng]/(infos)/campaign/[slug]/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import type { PropsWithChildren } from 'react';
+import { Layout } from 'src/Layout';
+
+// export const metadata: Metadata = {
+//   other: {
+//     'partner-theme': 'default',
+//   },
+// };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  return {
+    title: `Jumper Campaign - ${params.slug}`, // Example: Dynamic title
+    other: {
+      'partner-theme': params.slug,
+    },
+  };
+}
+
+export default async function CampaignLayout({ children }: PropsWithChildren) {
+  return <Layout>{children}</Layout>;
+}
