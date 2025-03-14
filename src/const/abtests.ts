29a7692cc050ba275a900aa34365e089f5c36ff8
Denny San <sander.denny@gmx.de>
2025-03-14 14:24:58 +0100

    chore: remove ab-test

--- src/const/abtests.ts
+++ src/const/abtests.ts
@@ -1,3 +1,3 @@
 export const AbTestCases = {
-  TEST_WIDGET_SUBVARIANTS: 'test_widget_subvariants',
+  // TEST_WIDGET_SUBVARIANTS: 'test_widget_subvariants', // Example: Value must match the name in posthog
 };
