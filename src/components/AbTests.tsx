8cfe48b6cc9e17578966e1357571c051634cbce8
Denny San <sander.denny@gmx.de>
2025-03-18 17:22:49 +0000

    chore: remove abtest for split subvariant

--- src/components/AbTests.tsx
+++ src/components/AbTests.tsx
@@ -1,14 +1,15 @@
 'use client';
-import { useAccount } from '@lifi/wallet-management';
-import { AbTestCases } from 'src/const/abtests';
-import { useABTest } from 'src/hooks/useABTest';
+// import { useAccount } from '@lifi/wallet-management';
+// import { AbTestCases } from 'src/const/abtests';
+// import { useABTest } from 'src/hooks/useABTest';
 
 export const AbTests = () => {
-  const { account } = useAccount();
+  // const { account } = useAccount();
 
-  useABTest({
-    feature: AbTestCases.TEST_WIDGET_SUBVARIANTS,
-    user: account?.address || '',
-  });
+  // Example: Use useABTest-hook to integrate an a/b test
+  // useABTest({
+  //   feature: AbTestCases.TEST_WIDGET_SUBVARIANTS,
+  //   user: account?.address || '',
+  // });
   return null;
 };
