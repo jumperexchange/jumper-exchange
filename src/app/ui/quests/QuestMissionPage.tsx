f01dc0fe6be698d9707d7b073535cbbdae0db584
Denny San <sander.denny@gmx.de>
2025-05-07 02:13:14 +0200

    fix: enable redirect to campaign page

--- src/app/ui/quests/QuestMissionPage.tsx
+++ src/app/ui/quests/QuestMissionPage.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { QuestsMissionPage } from 'src/components/QuestPage/QuestsMissionPage';
-import { JUMPER_LOYALTY_PATH } from 'src/const/urls';
+import { JUMPER_CAMPAIGN_PATH, JUMPER_LOYALTY_PATH } from 'src/const/urls';
 
 const QuestPage = ({ quest, url }: any) => {
+  const path = quest.campaign?.Slug
+    ? `${JUMPER_CAMPAIGN_PATH}${quest.campaign.Slug}`
+    : JUMPER_LOYALTY_PATH;
+
   return (
-    <QuestsMissionPage quest={quest} baseUrl={url} path={JUMPER_LOYALTY_PATH} />
+    <QuestsMissionPage
+      quest={quest}
+      baseUrl={url}
+      path={path}
+      activeCampaign={quest.campaign?.Title}
+    />
   );
 };
 
