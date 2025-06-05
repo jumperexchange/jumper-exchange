d9a787e3fd714e13b1b7bbe21e0c1e228c1cffcd
dennyscode <43956540+dennyscode@users.noreply.github.com>
2025-06-05 14:01:12 +0200

    refactor: cleanup unused packages and components (#1933)

--- src/components/Zap/BackButton/BackButton.style.ts
+++ src/components/Zap/BackButton/BackButton.style.ts
@@ -1,6 +1,5 @@
-import { type Breakpoint, Typography, styled } from '@mui/material';
+import { styled } from '@mui/material';
 import { ButtonPrimary } from 'src/components/Button';
-import { urbanist } from 'src/fonts/fonts';
 
 export const BackButtonStyles = styled(ButtonPrimary)(({ theme }) => ({
   display: 'flex',
@@ -8,16 +7,4 @@ export const BackButtonStyles = styled(ButtonPrimary)(({ theme }) => ({
   justifyContent: 'space-between',
   alignContent: 'center',
   alignItems: 'center',
-}));
-
-export const ButtonTypography = styled(Typography)(({ theme }) => ({
-  marginLeft: '4px',
-  fontSize: '14px',
-  typography: urbanist.style.fontFamily,
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  maxWidth: 232,
-  [theme.breakpoints.up('sm' as Breakpoint)]: {
-    maxWidth: 168,
-  },
 }));
