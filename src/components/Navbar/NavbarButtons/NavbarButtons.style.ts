95886dfd83f0e463abbc5d34f93042c0848c9767
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-06-09 14:46:01 +0200

    refactor: toggle menu styling
    refactor: toggle menu styling

--- src/components/Navbar/NavbarButtons/NavbarButtons.style.ts
+++ src/components/Navbar/NavbarButtons/NavbarButtons.style.ts
@@ -3,6 +3,7 @@
 import { ButtonSecondary } from '@/components/Button/Button.style';
 import type { ButtonProps } from '@mui/material';
 import { alpha, styled } from '@mui/material/styles';
+import MoreHoriz from '@mui/icons-material/MoreHoriz';
 
 export const NavbarButtonsContainer = styled('div')({
   display: 'flex',
@@ -12,26 +13,45 @@ export const NavbarButtonsContainer = styled('div')({
 });
 
 export const MenuToggle = styled(ButtonSecondary)<ButtonProps>(({ theme }) => {
-  return ({
-  justifyContent: 'center',
-  backgroundColor: 'transparent',
-  color:
-    (theme.vars || theme).palette.accent1Alt.main,
-  width: 48,
-  borderRadius: '50%',
-  marginLeft: theme.spacing(1.5),
-  minWidth: 'unset',
-  height: 48,
-  ':hover:before': {
-    backgroundColor: alpha(theme.palette.white.main, 0.12),
-    ...theme.applyStyles("light", {
-      backgroundColor: (theme.vars || theme).palette.alphaDark100.main
-    })
-  },
-  ':hover': {
+  return {
+    justifyContent: 'center',
     backgroundColor: 'transparent',
-  },
-  ...theme.applyStyles("light", {
-    color: (theme.vars || theme).palette.primary.main
-  })
-})});
+    color: (theme.vars || theme).palette.accent1Alt.main,
+    width: 48,
+    borderRadius: '50%',
+    marginLeft: theme.spacing(1.5),
+    minWidth: 'unset',
+    height: 48,
+    ':hover:before': {
+      backgroundColor: alpha(theme.palette.white.main, 0.12),
+      ...theme.applyStyles('light', {
+        backgroundColor: (theme.vars || theme).palette.alphaDark100.main,
+      }),
+    },
+    '&[aria-expanded="true"]': {
+      backgroundColor: (theme.vars || theme).palette.alphaLight600.main,
+    },
+    ':hover': {
+      backgroundColor: (theme.vars || theme).palette.alphaLight600.main,
+    },
+    ...theme.applyStyles('light', {
+      color: (theme.vars || theme).palette.primary.main,
+      '&[aria-expanded="true"]': {
+        backgroundColor: (theme.vars || theme).palette.white.main,
+        boxShadow: '0px 2px 8px 0px rgba(0, 0, 0, 0.04)',
+      },
+      ':hover': {
+        backgroundColor: (theme.vars || theme).palette.white.main,
+        boxShadow: '0px 2px 8px 0px rgba(0, 0, 0, 0.04)',
+      },
+    }),
+  };
+});
+
+export const MenuIcon = styled(MoreHoriz)(({ theme }) => ({
+  fontSize: '24px',
+  color: theme.palette.white.main,
+  ...theme.applyStyles('light', {
+    color: theme.palette.black.main,
+  }),
+}));
