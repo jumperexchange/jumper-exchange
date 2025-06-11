23ddd7cf6363204305878882aa9c1fb4cfc17ec1
Denny San <sander.denny@gmx.de>
2025-06-11 12:46:52 +0200

    refactor: simplify for more readability

--- src/utils/merkl/fetchTaskOpportunities.ts
+++ src/utils/merkl/fetchTaskOpportunities.ts
@@ -6,14 +6,19 @@ export const fetchTaskOpportunities = async (tasks: TaskVerification[]) => {
   return await Promise.all(
     (tasks || []).map(async (task) => {
       if (!task.CampaignId) return task;
-      const maxApy = await getMerklOpportunities({
+
+      const opportunities = await getMerklOpportunities({
         campaignId: task.CampaignId,
-      }).then((tasksItems) => {
-        const maxApy = tasksItems.map((taskItem) => {
-          return calculateMaxApy([taskItem]);
-        });
-        return maxApy[0];
       });
+
+      const apyValues = opportunities.map((opportunity) =>
+        calculateMaxApy([opportunity]),
+      );
+
+      // Though if here we only need the 1st opportunity APY we could compute only that
+      // Otherwise we might consider using Math.max(...apyValues)
+      const maxApy = apyValues.length > 0 ? apyValues[0] : 0;
+
       return { ...task, maxApy };
     }),
   );
