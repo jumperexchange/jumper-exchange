c41e8d180cc799bb82808626879d86223d6754eb
Denny San <sander.denny@gmx.de>
2025-06-23 16:03:47 +0200

    refactor: cleanup maxApy

--- src/utils/merkl/fetchTaskOpportunities.ts
+++ src/utils/merkl/fetchTaskOpportunities.ts
@@ -1,8 +1,13 @@
 import { getMerklOpportunities } from 'src/app/lib/getMerklOpportunities';
-import { TaskVerification } from 'src/types/loyaltyPass';
+import {
+  TaskVerification,
+  TaskVerificationWithApy,
+} from 'src/types/loyaltyPass';
 import { calculateMaxApy } from './merklHelper';
 
-export const fetchTaskOpportunities = async (tasks: TaskVerification[]) => {
+export const fetchTaskOpportunities = async (
+  tasks: TaskVerification[],
+): Promise<TaskVerificationWithApy[]> => {
   return await Promise.all(
     (tasks || []).map(async (task) => {
       if (!task.CampaignId) return task;
@@ -11,13 +16,10 @@ export const fetchTaskOpportunities = async (tasks: TaskVerification[]) => {
         campaignId: task.CampaignId,
       });
 
-      const apyValues = opportunities.map((opportunity) =>
-        calculateMaxApy([opportunity]),
-      );
+      if (!opportunities.length) return task;
 
-      // Though if here we only need the 1st opportunity APY we could compute only that
-      // Otherwise we might consider using Math.max(...apyValues)
-      const maxApy = apyValues.length > 0 ? apyValues[0] : 0;
+      // Calculate APY only if we have opportunities
+      const maxApy = calculateMaxApy(opportunities);
 
       return { ...task, maxApy };
     }),
