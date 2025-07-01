288838ecc26a913f16e09a83066ed83f01d86f63
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-07-01 17:48:13 +0200

    chore: add page metadata

--- src/app/[lng]/missions/page.tsx
+++ src/app/[lng]/missions/page.tsx
@@ -1,9 +1,19 @@
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { Suspense } from 'react';
 import { getFeatureFlag } from 'src/app/lib/getFeatureFlag';
 import { MissionsPage } from 'src/app/ui/missions/MissionsPage';
 import { MissionsPageSkeleton } from 'src/app/ui/missions/MissionsPageSkeleton';
 import { GlobalFeatureFlags } from 'src/const/abtests';
+import { getSiteUrl, AppPaths } from 'src/const/urls';
+
+export const metadata: Metadata = {
+  title: 'Jumper Missions',
+  description: 'Jumper Missions is the reward area of Jumper Exchange.',
+  alternates: {
+    canonical: `${getSiteUrl()}${AppPaths.Missions}`,
+  },
+};
 
 export default async function Page() {
   const isPageEnabled = await getFeatureFlag(
