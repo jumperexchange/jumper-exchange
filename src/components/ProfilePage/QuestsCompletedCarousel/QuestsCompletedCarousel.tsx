159d853830eb35f3c4bcc479e911bc07c8f991b1
Denny San <sander.denny@gmx.de>
2025-07-04 11:09:55 +0200

    refactor: useMemo

--- src/components/ProfilePage/QuestsCompletedCarousel/QuestsCompletedCarousel.tsx
+++ src/components/ProfilePage/QuestsCompletedCarousel/QuestsCompletedCarousel.tsx
@@ -5,6 +5,7 @@ import { useAccount } from '@lifi/wallet-management';
 import { useTranslation } from 'react-i18next';
 // import { CarouselContainer } from 'src/components/Blog/BlogCarousel/CarouselContainer';
 import { Box } from '@mui/material';
+import { useMemo } from 'react';
 import { Carousel } from 'src/components/Carousel/Carousel';
 import { CarouselNavigation } from 'src/components/Carousel/Navigation';
 import { capitalizeString } from 'src/utils/capitalizeString';
@@ -25,10 +26,6 @@ export const QuestsCompletedCarousel = ({
   const { account } = useAccount();
   const { t } = useTranslation();
 
-  const showVoidCardsAsFewPdas =
-    (!loading && pdas && pdas?.length < 6 && account?.address) ||
-    !account?.address;
-
   const today = new Date();
   // to do: activate when PDA were not done on time
   // const lastMonth = new Date(
@@ -38,39 +35,47 @@ export const QuestsCompletedCarousel = ({
   // );
   const updateDay = new Date(today.getFullYear(), today.getMonth(), 2);
 
-  const carouselContent = [
-    // Render quests
-    ...(!loading && pdas
-      ? pdas
-          .filter((pda: PDA) => pda?.reward) // Filter out PDAs without rewards
-          .map((pda: PDA, index: number) => {
-            const data = {
-              id: pda?.id,
-              completed: true,
-              active: false,
-              title: capitalizeString(pda?.reward?.name),
-              image: pda?.reward?.image,
-              points: pda?.points,
-            };
-            return <QuestCard key={`completed-mission-${index}`} data={data} />;
-          })
-      : []),
-    // Render void cards
-    ...(showVoidCardsAsFewPdas
-      ? Array.from({ length: 10 }, () => 42).map((_, idx) => (
-          <VoidQuestCard
-            key={'void-' + idx}
-            // connected={!!account?.address && account?.chainType === 'EVM'}
-          />
-        ))
-      : []),
-    // Render loading skeletons
-    ...(loading
-      ? Array.from({ length: 8 }, () => 42).map((_, idx) => (
-          <QuestCard key={'skeleton-' + idx} data={{ title: 'test' + idx }} />
-        ))
-      : []),
-  ];
+  const carouselContent = useMemo(() => {
+    const showVoidCardsAsFewPdas =
+      (!loading && pdas && pdas?.length < 6 && account?.address) ||
+      !account?.address;
+
+    return [
+      // Render quests
+      ...(!loading && pdas
+        ? pdas
+            .filter((pda: PDA) => pda?.reward) // Filter out PDAs without rewards
+            .map((pda: PDA, index: number) => {
+              const data = {
+                id: pda?.id,
+                completed: true,
+                active: false,
+                title: capitalizeString(pda?.reward?.name),
+                image: pda?.reward?.image,
+                points: pda?.points,
+              };
+              return (
+                <QuestCard key={`completed-mission-${index}`} data={data} />
+              );
+            })
+        : []),
+      // Render void cards
+      ...(showVoidCardsAsFewPdas
+        ? Array.from({ length: 10 }, () => 42).map((_, idx) => (
+            <VoidQuestCard
+              key={'void-' + idx}
+              // connected={!!account?.address && account?.chainType === 'EVM'}
+            />
+          ))
+        : []),
+      // Render loading skeletons
+      ...(loading
+        ? Array.from({ length: 8 }, () => 42).map((_, idx) => (
+            <QuestCard key={'skeleton-' + idx} data={{ title: 'test' + idx }} />
+          ))
+        : []),
+    ];
+  }, [pdas, loading]);
 
   const headerInfo = (
     <Box
