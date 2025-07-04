dcbdfa459b570090b67fe2334e5c3800d269f6ae
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-07-04 12:59:11 +0200

    feat: add widget variants for missions and zaps

---
+++ src/stores/mission/MissionStore.tsx
@@ -0,0 +1,90 @@
+'use client';
+import {
+  TaskWidgetInformationChainData,
+  TaskWidgetInformationWalletData,
+  TaskWidgetInformationTokenData,
+  TaskType,
+} from 'src/types/loyaltyPass';
+import { createWithEqualityFn } from 'zustand/traditional';
+
+interface MissionState {
+  currentActiveTaskId?: string;
+  currentActiveTaskType?: TaskType;
+
+  destinationChain?: TaskWidgetInformationChainData;
+  destinationToken?: TaskWidgetInformationTokenData;
+
+  sourceChain?: TaskWidgetInformationChainData;
+  sourceToken?: TaskWidgetInformationTokenData;
+
+  fromAmount?: string;
+
+  toAddress?: TaskWidgetInformationWalletData;
+
+  missionChainIds?: number[];
+  missionType?: string;
+
+  setCurrentTaskWidgetFormParams: ({
+    destinationChain,
+    destinationToken,
+    sourceChain,
+    sourceToken,
+    fromAmount,
+    toAddress,
+  }: {
+    destinationChain?: TaskWidgetInformationChainData;
+    destinationToken?: TaskWidgetInformationTokenData;
+    sourceChain?: TaskWidgetInformationChainData;
+    sourceToken?: TaskWidgetInformationTokenData;
+    fromAmount?: string;
+    toAddress?: TaskWidgetInformationWalletData;
+  }) => void;
+
+  setCurrentActiveTask: (taskId: string, taskType: TaskType) => void;
+
+  setMissionDefaults: (chainIds?: number[], missionType?: string) => void;
+
+  isMissionCompleted: boolean;
+  setIsMissionCompleted: (isCompleted: boolean) => void;
+}
+
+export const useMissionStore = createWithEqualityFn<MissionState>(
+  (set) => ({
+    currentActiveTaskId: undefined,
+    currentActiveTaskType: undefined,
+
+    destinationChain: undefined,
+    destinationToken: undefined,
+
+    sourceChain: undefined,
+    sourceToken: undefined,
+
+    fromAmount: undefined,
+
+    toAddress: undefined,
+
+    missionChainIds: [],
+    missionType: undefined,
+
+    setCurrentActiveTask: (currentActiveTaskId, currentActiveTaskType) =>
+      set({
+        currentActiveTaskId,
+        currentActiveTaskType,
+      }),
+
+    setCurrentTaskWidgetFormParams: (params) =>
+      set({
+        ...params,
+      }),
+
+    setMissionDefaults: (missionChainIds, missionType) =>
+      set({
+        missionChainIds,
+        missionType,
+      }),
+
+    isMissionCompleted: false,
+    setIsMissionCompleted: (isMissionCompleted) => set({ isMissionCompleted }),
+  }),
+  Object.is,
+);
