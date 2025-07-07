e693238ddcee88b7028ab94ed2e4d87061e07bbb
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-07-07 19:18:25 +0200

    feat: enable widget

--- src/app/ui/mission/MissionWidget.tsx
+++ src/app/ui/mission/MissionWidget.tsx
@@ -5,10 +5,10 @@ import { useMissionStore } from 'src/stores/mission/MissionStore';
 import { CustomInformation } from 'src/types/loyaltyPass';
 import { TaskType } from 'src/types/strapi';
 import { FC } from 'react';
-// import { WidgetSkeleton } from 'src/components/Widgets/variants/base/WidgetSkeleton';
+import { WidgetSkeleton } from 'src/components/Widgets/variants/base/WidgetSkeleton';
 import Box from '@mui/material/Box';
-// import { MissionBaseWidget } from 'src/components/Widgets/variants/mission/MissionBaseWidget';
-// import { MissionZapDepositWidget } from 'src/components/Widgets/variants/mission/MissionZapDepositWidget';
+import { MissionBaseWidget } from 'src/components/Widgets/variants/mission/MissionBaseWidget';
+import { MissionZapDepositWidget } from 'src/components/Widgets/variants/mission/MissionZapDepositWidget';
 // import { MissionZapWithdrawWidget } from 'src/components/Widgets/variants/mission/MissionZapWithdrawWidget';
 
 export interface MissionWidgetProps {
@@ -21,22 +21,19 @@ export const MissionWidget: FC<MissionWidgetProps> = ({
   // @Note create factory for displaying the mission completed screen instead of the widget
   const { currentActiveTaskType } = useMissionStore();
 
-  return null;
+  if (currentActiveTaskType === TaskType.Zap && !customInformation) {
+    return <WidgetSkeleton />;
+  }
 
-  // @TODO enable once widget variant is in
-  // if (currentActiveTaskType === TaskType.Zap && !customInformation) {
-  //   return <WidgetSkeleton />;
-  // }
-
-  // return (
-  //   <Box sx={{ width: '100%' }}>
-  //     <ClientOnly>
-  //       {currentActiveTaskType === TaskType.Zap ? (
-  //         <MissionZapDepositWidget customInformation={customInformation} />
-  //       ) : (
-  //         <MissionBaseWidget />
-  //       )}
-  //     </ClientOnly>
-  //   </Box>
-  // );
+  return (
+    <Box sx={{ width: '100%' }}>
+      <ClientOnly>
+        {currentActiveTaskType === TaskType.Zap ? (
+          <MissionZapDepositWidget customInformation={customInformation} />
+        ) : (
+          <MissionBaseWidget />
+        )}
+      </ClientOnly>
+    </Box>
+  );
 };
