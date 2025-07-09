035ba2ecec264d3f241903f77c26800b9435cb33
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-07-09 19:10:59 +0200

    chore: remove deposit card from zap widget

--- src/components/Widgets/variants/base/ZapWidget/ZapDepositWidget.tsx
+++ src/components/Widgets/variants/base/ZapWidget/ZapDepositWidget.tsx
@@ -3,8 +3,6 @@
 import { FC, useEffect, useMemo } from 'react';
 import { LiFiWidget, Route, useWidgetEvents, WidgetEvent } from '@lifi/widget';
 import { useInitializeZapConfig } from './useInitializeZapConfig';
-import { formatUnits } from 'viem/utils';
-import { DepositCard } from 'src/components/ZapWidget/Deposit/DepositCard';
 import { WidgetSkeleton } from '../WidgetSkeleton';
 import { useLiFiWidgetConfig } from '../../widgetConfig/hooks';
 import { WidgetProps } from '../Widget.types';
@@ -20,18 +18,12 @@ export const ZapDepositWidget: FC<ZapDepositWidgetProps> = ({
     return customInformation?.projectData;
   }, [customInformation?.projectData]);
 
-  const claimingIds = useMemo(() => {
-    return customInformation?.claimingIds;
-  }, [customInformation?.claimingIds]);
-
   const {
     isInitialized,
     providers,
     toAddress,
     zapData,
     isZapDataSuccess,
-    depositTokenData,
-    depositTokenDecimals,
     refetchDepositToken,
     setCurrentRoute,
   } = useInitializeZapConfig(projectData);
@@ -97,53 +89,9 @@ export const ZapDepositWidget: FC<ZapDepositWidgetProps> = ({
     };
   }, [widgetEvents, refetchDepositToken, setCurrentRoute]);
 
-  const token = useMemo(
-    () =>
-      isZapDataSuccess && zapData
-        ? {
-            chainId: zapData.market?.depositToken.chainId,
-            address: zapData.market?.depositToken.address as `0x${string}`,
-            symbol: zapData.market?.depositToken.symbol,
-            name: zapData.market?.depositToken.name,
-            decimals: zapData.market?.depositToken.decimals,
-            priceUSD: '0',
-            coinKey:
-              zapData.market?.depositToken.symbol ||
-              zapData.market?.depositToken.name ||
-              '',
-            logoURI: zapData.market?.depositToken.logoURI,
-            amount: BigInt(0),
-          }
-        : null,
-    [isZapDataSuccess, zapData],
-  );
-
-  const lpTokenDecimals = Number(depositTokenDecimals ?? 18);
-
-  const analytics = useMemo(
-    () => ({
-      ...(zapData?.analytics || {}), // Provide default empty object
-      position: depositTokenData
-        ? formatUnits(depositTokenData as bigint, lpTokenDecimals)
-        : 0,
-    }),
-    [zapData, lpTokenDecimals],
-  );
-
-  return token && isZapDataSuccess && isInitialized ? (
+  return isInitialized && isZapDataSuccess ? (
     <LiFiWidget
-      contractComponent={
-        <DepositCard
-          poolName={poolName}
-          underlyingToken={zapData?.market?.depositToken}
-          token={token}
-          chainId={zapData?.market?.depositToken.chainId}
-          contractTool={zapData?.meta}
-          analytics={analytics}
-          contractCalls={[]}
-          claimingIds={claimingIds}
-        />
-      }
+      key={poolName}
       config={widgetConfig}
       integrator={widgetConfig.integrator}
     />
