21756e4e22a1305c3f97fa367afae3fadc5936a4
Laurent Senta <laurent@singulargarden.com>
2025-07-22 09:38:26 +0200

    fix: remove dead code in env-config

--- src/app/api/env-config.js/route.tsx
+++ src/app/api/env-config.js/route.tsx
@@ -6,9 +6,5 @@ import { getEnvVars } from 'src/config/env-config';
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
-  const keys = (Object.keys(process.env) ?? []).filter((key) =>
-    key.startsWith('NEXT_PUBLIC_'),
-  );
-
   return new NextResponse(`window._env_ = ` + JSON.stringify(getEnvVars()));
 }
