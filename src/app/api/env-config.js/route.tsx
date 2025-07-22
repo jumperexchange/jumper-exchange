89398244a16c8e64a43d0938701fc98cf48db17a
Laurent Senta <laurent@singulargarden.com>
2025-07-22 09:46:12 +0200

    feat: split public / private env vars in config

--- src/app/api/env-config.js/route.tsx
+++ src/app/api/env-config.js/route.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 
 import { NextResponse } from 'next/server';
-import { getEnvVars } from 'src/config/env-config';
+import { getPublicEnvVars } from 'src/config/env-config';
 
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
-  return new NextResponse(`window._env_ = ` + JSON.stringify(getEnvVars()));
+  return new NextResponse(
+    `window._env_ = ` + JSON.stringify(getPublicEnvVars()),
+  );
 }
