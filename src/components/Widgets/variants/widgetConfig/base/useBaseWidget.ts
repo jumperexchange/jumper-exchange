8073e23282aef98795bb5f9a577c7ff342c67805
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-07-24 09:23:00 +0200

    fix: missions/zap widget box shadow

--- src/components/Widgets/variants/widgetConfig/base/useBaseWidget.ts
+++ src/components/Widgets/variants/widgetConfig/base/useBaseWidget.ts
@@ -40,6 +40,7 @@ export const useBaseWidget = () => {
           maxHeight: '100%',
           maxWidth: 'unset',
           borderRadius: 24,
+          boxShadow: '0px 4px 24px 0px rgba(0, 0, 0, 0.08)', // @TODO Figma: elevation 4
         },
         header: {
           // @Note this needs a workaround to be able to show title on multiple lines
