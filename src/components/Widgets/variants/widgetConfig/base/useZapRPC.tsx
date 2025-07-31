3b17811df09aa13c4592ad9d4446a6c2004ed1ef
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-07-31 08:35:26 +0200

    chore: remove gaszip from allowed bridges

--- src/components/Widgets/variants/widgetConfig/base/useZapRPC.tsx
+++ src/components/Widgets/variants/widgetConfig/base/useZapRPC.tsx
@@ -45,7 +45,7 @@ export const useZapRPC = (
       },
       explorerUrls,
       bridges: {
-        allow: ['across', 'relay', 'gasZipBridge'],
+        allow: ['across', 'relay'],
       },
       sdkConfig: {
         apiUrl: envConfig.NEXT_PUBLIC_LIFI_API_URL,
