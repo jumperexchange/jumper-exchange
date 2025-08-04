79b70fe52ee195caaa7cdc6112a5a4ad23183674
Bojan Kopunovic <bkopunovic93@gmail.com>
2025-08-04 17:26:29 +0200

    Upgraded synpress to 4.x version

--- tests/wallet-setup/basic.setup.ts
+++ tests/wallet-setup/basic.setup.ts
@@ -1,11 +1,17 @@
-import { MetaMask, defineWalletSetup } from '@synthetixio/synpress';
+import type { BrowserContext, Page } from '@playwright/test'
+import { defineWalletSetup } from '@synthetixio/synpress'
+import { MetaMask } from '@synthetixio/synpress/playwright'
 
 const SEED_PHRASE =
-  'test test test test test test test test test test test junk';
-const PASSWORD = 'SynpressIsAwesomeNow!!!';
+  'test test test test test test test test test test test junk'
+const PASSWORD = 'SynpressIsAwesomeNow!!!'
 
-export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
-  const metamask = new MetaMask(context, walletPage, PASSWORD);
+// @ts-ignore - Playwright version compatibility issue
+export default defineWalletSetup(
+  PASSWORD,
+  async (context: BrowserContext, walletPage: Page) => {
+    const metamask = new MetaMask(context, walletPage, PASSWORD)
 
-  await metamask.importWallet(SEED_PHRASE);
-});
+    await metamask.importWallet(SEED_PHRASE)
+  }
+);
