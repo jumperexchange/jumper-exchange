d54eff67f5b37ba3920cd9b692c8f6dc73470c6c
vinzenzLIFI <vinzenz@li.finance>
2025-08-05 18:44:46 +0200

    test(e2e): fix report array

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [['list']['blob'], qaseReporter]
+    ? [['list'], ['blob'], qaseReporter]
     : [['list'], ['html'], qaseReporter],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
