62815eb86b859f0fc537dd5847c6e5ef50b3210a
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-08-06 17:34:43 +0200

    feat: add widget container for padding (#2119)

--- src/app/[lng]/(main)/page.tsx
+++ src/app/[lng]/(main)/page.tsx
@@ -1,15 +1,13 @@
 import { Widgets } from '@/components/Widgets';
 import { Widget } from '@/components/Widgets/Widget';
+import { MainWidgetContainer } from 'src/components/Containers/MainWidgetContainer';
 
 export default async function Page() {
   const variant = 'default'; // exchange
   return (
-    <>
-      <Widget
-        activeTheme={variant}
-        starterVariant={variant}
-      />
+    <MainWidgetContainer>
+      <Widget activeTheme={variant} starterVariant={variant} />
       <Widgets widgetVariant={variant} />
-    </>
+    </MainWidgetContainer>
   );
 }
