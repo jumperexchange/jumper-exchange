f48d81807c4a0abb21527134a460cf82684a6675
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-08-06 17:34:43 +0200

    feat: add widget container for padding (#2119)

--- src/app/[lng]/(main)/gas/page.tsx
+++ src/app/[lng]/(main)/gas/page.tsx
@@ -1,13 +1,14 @@
 import { Widgets } from '@/components/Widgets';
 import { Widget } from '@/components/Widgets/Widget';
+import { MainWidgetContainer } from 'src/components/Containers/MainWidgetContainer';
 
 const Page = async () => {
   const variant = 'refuel';
   return (
-    <>
+    <MainWidgetContainer>
       <Widget starterVariant={variant} />
       <Widgets widgetVariant={variant} />
-    </>
+    </MainWidgetContainer>
   );
 };
 
---
+++ src/components/Containers/MainWidgetContainer.tsx
@@ -0,0 +1,18 @@
+import { FC, PropsWithChildren } from 'react';
+import Box from '@mui/material/Box';
+
+interface MainWidgetContainerProps extends PropsWithChildren {}
+
+export const MainWidgetContainer: FC<MainWidgetContainerProps> = ({
+  children,
+}) => {
+  return (
+    <Box
+      sx={{
+        paddingX: { xs: 2, sm: 0 },
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
