38af87139209d4cba872dd05d72744e6e56d3ce7
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-08-07 19:07:33 +0200

    fix: re-enable main link for scan and learn pages

--- src/components/Navbar/ServerNavbar.tsx
+++ src/components/Navbar/ServerNavbar.tsx
@@ -6,24 +6,20 @@ import { NavbarContainer } from './Navbar.style';
 import { Layout } from './layout/Layout';
 import { Link } from '../Link';
 
-import { checkIsLearnPage, checkIsScanPage } from './utils';
+import { checkIsLearnPage } from './utils';
 import { AppPaths } from 'src/const/urls';
 
 export const ServerNavbar = async () => {
   const cookieStore = await cookies();
   const pathname = cookieStore.get('pathname')?.value || '';
   const isLearnPage = checkIsLearnPage(pathname);
-  const isScanPage = checkIsScanPage(pathname);
 
   return (
     <NavbarContainer enableColorOnDark>
       <Link component={RouterLink} href={AppPaths.Main}>
         <Logo variant="default" />
       </Link>
-      <Layout
-        hideConnectButton={isLearnPage}
-        isNotMainApp={isScanPage || isLearnPage}
-      />
+      <Layout hideConnectButton={isLearnPage} />
     </NavbarContainer>
   );
 };
--- src/components/Navbar/layout/DesktopLayout.tsx
+++ src/components/Navbar/layout/DesktopLayout.tsx
@@ -7,30 +7,26 @@ import { LabelButton } from '../components/Buttons/LabelButton';
 import { MainMenuToggle } from '../components/Buttons/MainMenuToggle';
 import { LayoutVariantProps } from './Layout.types';
 
-export const DesktopLayout: FC<LayoutVariantProps> = ({
-  secondaryButtons,
-  hideMainLinks,
-}) => {
+export const DesktopLayout: FC<LayoutVariantProps> = ({ secondaryButtons }) => {
   const { links, activeLink } = useMainLinks();
 
   return (
     <>
-      {!hideMainLinks && (
-        <MainLinksContainer sx={{ width: '100%', mr: 1 }}>
-          {links.map((link) => (
-            <LabelButton
-              isActive={activeLink?.value === link.value}
-              key={link.value}
-              href={link.value}
-              label={
-                <Box component="span" sx={{ paddingX: 1.5 }}>
-                  {link.label}
-                </Box>
-              }
-            />
-          ))}
-        </MainLinksContainer>
-      )}
+      <MainLinksContainer sx={{ width: '100%', mr: 1 }}>
+        {links.map((link) => (
+          <LabelButton
+            isActive={activeLink?.value === link.value}
+            key={link.value}
+            href={link.value}
+            label={
+              <Box component="span" sx={{ paddingX: 1.5 }}>
+                {link.label}
+              </Box>
+            }
+          />
+        ))}
+      </MainLinksContainer>
+
       <SecondaryLinksContainer>
         {secondaryButtons}
         <MainMenuToggle />
