f96d3ef2b3aafb5b028d38b22090fee14dcfe858
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-08-12 19:01:38 +0200

    feat: add wallet ecosystem order to the WalletManagementProvider config

---
+++ src/providers/WalletProvider/constants.ts
@@ -0,0 +1,7 @@
+import { ChainType } from '@lifi/sdk';
+
+export const walletEcosystemsOrder = {
+  MetaMask: [ChainType.EVM, ChainType.UTXO, ChainType.SVM, ChainType.MVM],
+  'Rabby Wallet': [ChainType.EVM, ChainType.UTXO, ChainType.SVM, ChainType.MVM],
+  Phantom: [ChainType.SVM, ChainType.EVM, ChainType.UTXO, ChainType.MVM],
+};
