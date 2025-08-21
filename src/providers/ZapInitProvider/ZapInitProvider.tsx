72ab823645de938dc62a69e2f277b442dfff247a
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-08-21 15:26:47 +0200

    chore: add log for the toAddress

--- src/providers/ZapInitProvider/ZapInitProvider.tsx
+++ src/providers/ZapInitProvider/ZapInitProvider.tsx
@@ -209,12 +209,15 @@ export const ZapInitProvider: FC<ZapInitProviderProps> = ({
   });
 
   const toAddress = useBiconomyClientsStore((state) => {
-    console.warn('🔍 toAddress selector running');
-    return state.getToAddress(
+    const valueFromStore = state.getToAddress(
       projectData.address as EVMAddress | undefined,
       projectData.chainId,
       address as EVMAddress | undefined,
     );
+
+    console.warn('🔍 toAddress selector running', valueFromStore);
+
+    return valueFromStore;
   });
 
   // RPC operation queueing
