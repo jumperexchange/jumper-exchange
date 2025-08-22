12b5520f5b224c6d1a6dfae01c2d4e660c3afd6b
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-08-22 16:32:12 +0200

    chore: UX updates

--- src/components/Widgets/variants/base/ZapWidget/ZapMultisigPlaceholderWidget.tsx
+++ src/components/Widgets/variants/base/ZapWidget/ZapMultisigPlaceholderWidget.tsx
@@ -1,17 +1,24 @@
+import { MouseEvent } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useWalletMenu } from '@lifi/wallet-management';
+
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import { useTranslation } from 'react-i18next';
-import { Button } from 'src/components/Button/Button';
 
-import { AppPaths } from 'src/const/urls';
-import { Link } from 'src/components/Link';
 import { Badge } from 'src/components/Badge/Badge';
+import { Button } from 'src/components/Button/Button';
 import { SectionCard } from 'src/components/Cards/SectionCard/SectionCard';
 import { BadgeVariant } from 'src/components/Badge/Badge.styles';
 
 export const ZapMultisigPlaceholderWidget = () => {
   const { t } = useTranslation();
+  const { openWalletMenu } = useWalletMenu();
+
+  const handleOpenWalletMenu = (event?: MouseEvent<HTMLButtonElement>) => {
+    event?.stopPropagation();
+    openWalletMenu();
+  };
   return (
     <SectionCard>
       <Stack sx={{ gap: 3 }}>
@@ -22,21 +29,17 @@ export const ZapMultisigPlaceholderWidget = () => {
         />
         <Stack sx={{ gap: 0.5 }}>
           <Typography variant="bodyLargeStrong">
-            Not yet supported in your environment
+            Your wallet is currently not supported
           </Typography>
           <Typography>
-            We're not supporting zap missions yet for embedded and smart
-            contract wallets
+            We are working on adding support for embedded and smart contract
+            wallets. In the mean time please use a different wallet to complete
+            this mission.
           </Typography>
         </Stack>
-        <Link
-          href={AppPaths.Missions}
-          sx={{ width: '100%', textDecoration: 'none' }}
-        >
-          <Button fullWidth size="medium">
-            {t('button.goBack')}
-          </Button>
-        </Link>
+        <Button fullWidth size="medium" onClick={handleOpenWalletMenu}>
+          {t('button.connectAnotherWallet')}
+        </Button>
       </Stack>
     </SectionCard>
   );
