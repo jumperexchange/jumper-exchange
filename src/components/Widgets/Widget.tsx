986bba2373cc95cd9eeb91bc35fe4557d9ff8615
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-08-26 18:48:53 +0200

    chore: remove this

--- src/components/Widgets/Widget.tsx
+++ src/components/Widgets/Widget.tsx
@@ -303,6 +303,14 @@ export function Widget({
       autoHeight={autoHeight}
       contributionDisplayed={contributionDisplayed}
     >
+      <button
+        type="button"
+        onClick={() => {
+          throw new Error('Sentry Test Error');
+        }}
+      >
+        Break the world
+      </button>
       <ClientOnly fallback={<LifiWidgetSkeleton config={config} />}>
         <LiFiWidget
           integrator={config.integrator}
