321a6a395c45b32a4d5c810d688f8e266db51d54
Lorsmirq0cB <lorsmirqbenton@gmail.com>
2025-09-02 15:02:47 +0800

    Corrected misspellings issues

--- src/components/Widgets/Widget.tsx
+++ src/components/Widgets/Widget.tsx
@@ -93,11 +93,11 @@ export function Widget({
     if (widgetIntegrator) {
       return widgetIntegrator;
     }
-    // all the trafic from mobile (including "/gas")
+    // all the traffic from mobile (including "/gas")
     // if (!isDesktop) {
     //   return envConfig.NEXT_PUBLIC_INTEGRATOR_MOBILE;
     // }
-    // all the trafic from web on "/gas"
+    // all the traffic from web on "/gas"
     if (isGasVariant) {
       return envConfig.NEXT_PUBLIC_WIDGET_INTEGRATOR_REFUEL;
     }
