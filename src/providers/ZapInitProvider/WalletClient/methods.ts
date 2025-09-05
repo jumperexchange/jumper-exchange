f73d04541ea1021803f9e3370ff379facceac0bc
Adam <info@admsk2.pl>
2025-09-05 13:41:35 +0200

    fix: Gas refund in zaps;

--- src/providers/ZapInitProvider/WalletClient/methods.ts
+++ src/providers/ZapInitProvider/WalletClient/methods.ts
@@ -285,6 +285,7 @@ export const sendCalls = async (
     userBalance,
     requestedAmount,
     isEmbeddedWallet: sendCallsExtraParams.isEmbeddedWallet,
+    eoaWallet: currentAddress as EVMAddress,
   });
 
   return { id: getFormattedTransactionHash(hash) };
