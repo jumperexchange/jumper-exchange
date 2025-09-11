a3c0f6b26c79874958ef0b3e2edb13e1461cc8ad
Bojan Kopunovic <bkopunovic93@gmail.com>
2025-09-11 15:36:21 +0200

    feat: adding checks for number of exchanges

--- tests/zapPage.spec.ts
+++ tests/zapPage.spec.ts
@@ -1,29 +1,34 @@
 import { expect, test, type Page } from '@playwright/test';
 import { qase } from 'playwright-qase-reporter';
-import {
-    clickItemInSettingsMenu,
-  } from './testData/settingsFunctions'
-import { SETTINGS_MENU } from './testData/testConstants';
-import values from './testData/values.json' assert { type: 'json' };
-import { checkTabsInHeader } from './testData/menuFunctions';
 
- async function verifyBridgesCount(page: Page, numberOfBridges: number): Promise<void> {
+import { checkTabsInHeader } from './testData/menuFunctions';
+import { clickItemInSettingsMenu } from './testData/settingsFunctions';
+import { SETTINGS_MENU } from './testData/testConstants';
+import values from './testData/values.json' with { type: 'json' };
+
+ async function verifyItemsCount(page: Page, locator: string, expectedCount: number): Promise<void> {
     const widgetContainer = page.getByTestId('zap-widget-container');
-    const listOfBridges = widgetContainer.locator('ul.MuiList-root.MuiList-padding div[role="button"]');
-    await expect(listOfBridges).toHaveCount(numberOfBridges);
+    const listOfItems = widgetContainer.locator(locator);
+    await expect(listOfItems).toHaveCount(expectedCount);
   }
 
-test.describe.skip('Zap Morpho Katana Page', () => {
+test.describe('Zap Morpho Katana Page', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('/missions/morpho-katana-gauntlet-usdc');
         await page.waitForLoadState('networkidle');
     });
 
-    test(qase(33,'Should verify bridge selection list has exactly 2 bridges'), async ({ page }) => {
-        await page.getByRole('button', { name: SETTINGS_MENU.TITLE }).click();
-        await clickItemInSettingsMenu(page, SETTINGS_MENU.BRIDGES.LABEL);
-        await verifyBridgesCount(page, 2);
-    });
+    test(qase(33,'Verify bridge selection list has exactly 23 bridges'), async ({ page }) => {
+            await page.getByRole('button', { name: SETTINGS_MENU.TITLE }).click();
+            await clickItemInSettingsMenu(page, SETTINGS_MENU.BRIDGES.LABEL);
+            await verifyItemsCount(page, '[data-testid="bridges-list"] > div', 23);
+        });
+
+    test(qase(34,'Should verify the number of Exchanges is exactly 24'), async ({ page }) => {
+            await page.getByRole('button', { name: SETTINGS_MENU.TITLE }).click();
+            await clickItemInSettingsMenu(page, SETTINGS_MENU.EXCHANGES.LABEL);
+            await verifyItemsCount(page, '[data-testid="exchanges-list"] > div', 24);
+        });
 
     test(qase(32,'Verify the url of Discover Morpho link'), async ({ page }) => {
       await test.step('Verify the url of Discover Morpho link', async () => {
@@ -31,9 +36,8 @@ test.describe.skip('Zap Morpho Katana Page', () => {
         await expect(discoverMorphoLink).toHaveAttribute('href', values.morphoURL);
       });
       
-      await test.step(('Verify that Missions and Exchange tabs are visible in the header of the page'), async () => {
+      await test.step('Verify that Missions and Exchange tabs are visible in the header of the page', async () => {
         await checkTabsInHeader(page);
       });
     });
-    
-});
+  })
