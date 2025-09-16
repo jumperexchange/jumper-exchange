71ccdc3ab7d8e99fbf20177528511290f23adf45
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-09-16 17:21:02 +0800

    fix: add allowed chains list

--- src/components/Widgets/variants/base/ZapWidget/ZapDepositBackendWidget.tsx
+++ src/components/Widgets/variants/base/ZapWidget/ZapDepositBackendWidget.tsx
@@ -10,6 +10,8 @@ import { useLiFiWidgetConfig } from '../../widgetConfig/hooks';
 import { ConfigContext } from '../../widgetConfig/types';
 import { WidgetProps } from '../Widget.types';
 import { ZapDepositSettings } from './ZapDepositSettings';
+import { useZapSupportedChains } from 'src/hooks/zaps/useZapSupportedChains';
+import { ChainId } from '@lifi/sdk';
 
 interface ZapDepositBackendWidgetProps extends WidgetProps {}
 
@@ -32,11 +34,48 @@ export const ZapDepositBackendWidget: FC<ZapDepositBackendWidgetProps> = ({
     refetchDepositToken,
   } = useEnhancedZapData(projectData);
 
+  const { data: zapSupportedChains } = useZapSupportedChains();
+
   const { setDestinationChainTokenForTracking } = useWidgetTrackingContext();
 
   const [setSupportModalState] = useMenuStore((state) => [
     state.setSupportModalState,
   ]);
+
+  const allowedChains = useMemo(() => {
+    // @Note: This is a fallback for when the zap supported chains are not loaded yet
+    if (!zapSupportedChains) {
+      return [
+        ChainId.ETH,
+        ChainId.BSC,
+        ChainId.ARB,
+        ChainId.BAS,
+        ChainId.AVA,
+        ChainId.POL,
+        ChainId.SCL,
+        ChainId.OPT,
+        ChainId.DAI,
+        ChainId.UNI,
+        ChainId.SEI,
+        ChainId.SON,
+        ChainId.APE,
+        ChainId.WCC,
+        ChainId.HYP,
+        // @Note: Even though docs say they are supported, they are not retrieved from the API
+        // https://docs.biconomy.io/supportedNetworks#-supported-chains
+        // ChainId.KAT,
+        // ChainId.LSK,
+      ];
+    }
+
+    const zapSupportedChainsIds = zapSupportedChains.map(
+      (chain) => chain.chainId,
+    );
+
+    return Object.values(ChainId).filter((chainId): chainId is ChainId =>
+      zapSupportedChainsIds?.includes(chainId.toString()),
+    );
+  }, [zapSupportedChains]);
 
   const poolName = useMemo(() => {
     return `${zapData?.meta.name} ${zapData?.market?.depositToken?.symbol.toUpperCase()} Pool`;
@@ -75,6 +114,13 @@ export const ZapDepositBackendWidget: FC<ZapDepositBackendWidgetProps> = ({
   }, [JSON.stringify(ctx), projectData.integrator, poolName]);
 
   const widgetConfig = useLiFiWidgetConfig(enhancedCtx);
+
+  // @Note: we want to ensure that the chains are set in the widget config without any delay
+  if (allowedChains) {
+    widgetConfig.chains = {
+      allow: allowedChains,
+    };
+  }
 
   return (
     <LiFiWidget
