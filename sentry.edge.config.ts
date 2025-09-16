f36681a86856356e900c3ec5e19ebd0a45c1546f
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-09-16 22:44:01 +0800

    fix: sentry env and logs (#2272)

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,7 +3,7 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { isProduction } from '@/utils/isProduction';
+import { isProduction } from './src/utils/isProduction';
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
