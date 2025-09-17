91656923a8010b47a1e320109b04dd2727519fc5
Adam <info@admsk2.pl>
2025-09-17 14:10:15 +0200

    feat: Hyperflow boost;

--- src/components/Widgets/Widget.tsx
+++ src/components/Widgets/Widget.tsx
@@ -252,7 +252,50 @@ export function Widget({
             },
           },
           exchanges: {
-            allow: ['hyperbloom'], // Replace by hyperbloom when available
+            allow: ['hyperbloom'],
+          },
+        },
+        {
+          label: {
+            text: '1.5x points',
+            sx: {
+              order: 1,
+              display: 'flex',
+              alignItems: 'center',
+              position: 'relative',
+              overflow: 'hidden',
+              marginLeft: 'auto',
+              gap: theme.spacing(0.5),
+              paddingLeft: theme.spacing(0.5),
+              paddingRight: theme.spacing(0.5),
+              background: `linear-gradient(90deg, ${(theme.vars || theme).palette.orchid[600]} 0%, ${(theme.vars || theme).palette.lavenderDark[300]} 100%)`,
+              color: (theme.vars || theme).palette.white.main,
+              ...theme.typography.bodyXSmallStrong,
+              ...theme.applyStyles('light', {
+                // @Note we might adjust to use the theme config
+                background: 'linear-gradient(90deg, #9B006F 0%, #37006B 100%)',
+              }),
+              '&::before': {
+                content: '""',
+                width: '16px',
+                height: '16px',
+                borderRadius: '50%', // Makes the icon circular
+                backgroundImage:
+                  'url(https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/exchanges/hyperflow.svg)',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                flexShrink: 0,
+              },
+              '&>p': {
+                alignContent: 'flex-end',
+                paddingLeft: theme.spacing(0.5),
+                paddingRight: theme.spacing(0.5),
+              },
+            },
+          },
+          exchanges: {
+            allow: ['hyperflow'],
           },
         },
       ],
