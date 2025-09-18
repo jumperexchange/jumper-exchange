754a93adbb718c076c90a21708a4061d709ae254
Bojan <97167801+callMeTheQA@users.noreply.github.com>
2025-09-18 15:13:41 +0200

    feat: add QR code visibility test for wallet connect option on landin… (#2290)

--- tests/landingPage.spec.ts
+++ tests/landingPage.spec.ts
@@ -4,7 +4,9 @@ import {
   navigateToTab,
   clickOnJumperLogo,
 } from './testData/landingPageFunctions';
+import { connectButton } from './testData/connectWalletFunctions';
 import { qase } from 'playwright-qase-reporter';
+import values from './testData/values.json' assert { type: 'json' };
 
 test.describe('Landing page and navigation', () => {
   test.beforeEach(async ({ page }) => {
@@ -24,5 +26,16 @@ test.describe('Landing page and navigation', () => {
     await clickOnJumperLogo(page);
     await closeWelcomeScreen(page);
     await expect(headerText).toBe('Find the best route');
+  });
+
+  test(qase(35,'QR code should be visible when select wallet connect option'), async ({ page }) => {
+    const walletConnectOption = page.locator('xpath=//span[normalize-space(text())="WalletConnect"]');
+    await connectButton(page).click();
+    await walletConnectOption.click();
+    const walletConnectModal = page.locator('[data-testid="w3m-modal-card"]');
+    const scanQRCodeTitle = walletConnectModal.getByText(values.scanQRCodeTitle);
+    const qrCode = walletConnectModal.locator('[data-testid="wui-qr-code"]');
+    await expect(qrCode).toBeVisible();
+    await expect(scanQRCodeTitle).toBeVisible();
   });
 });
