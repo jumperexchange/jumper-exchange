58fedc806459f9b2d44db1f69170c21bad973b20
Laurent Senta <laurent@singulargarden.com>
2025-09-22 10:49:05 +0200

    feat: introduce copy variations

---
+++ src/components/Cards/EarnCard/variants/TopHiglight.tsx
@@ -0,0 +1,22 @@
+import { styled } from '@mui/material/styles';
+import { FC, PropsWithChildren } from 'react';
+
+interface Props {
+  type: 'asset' | 'protocol' | 'apy' | 'token' | 'tag' | 'chain';
+}
+
+const HighlightedSpan = styled('span')(({ theme }) => ({
+  color: (theme.vars || theme).palette.accent1Alt.main,
+  fontWeight: theme.typography.fontWeightMedium,
+  ...theme.applyStyles('light', {
+    color: (theme.vars || theme).palette.accent1.main,
+  }),
+}));
+
+export const TopHighlight: FC<PropsWithChildren<Props>> = ({
+  type,
+  children,
+}) => {
+  // TODO: LF-14990: Generate clickable links / filters events if possible.
+  return <HighlightedSpan>{children}</HighlightedSpan>;
+};
