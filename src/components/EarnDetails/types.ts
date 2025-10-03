8b8ab74bdf6a634d12560cc68b5bc3243091de98
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-10-03 19:05:23 +0700

    feat: add earn analytics chart section to earn details page

---
+++ src/components/EarnDetails/types.ts
@@ -0,0 +1,11 @@
+export enum AnalyticsValueFieldEnum {
+  APY = 'apy',
+  TVL = 'tvl',
+}
+
+export enum AnalyticsRangeFieldEnum {
+  DAY = 'day',
+  WEEK = 'week',
+  MONTH = 'month',
+  YEAR = 'year',
+}
