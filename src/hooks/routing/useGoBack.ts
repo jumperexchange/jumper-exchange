b89ab482c5d929f92c340fe7d4a048c3ceb5bd27
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-10-15 15:27:06 +0700

    fix: go back navigation hook

--- src/hooks/routing/useGoBack.ts
+++ src/hooks/routing/useGoBack.ts
@@ -5,7 +5,11 @@ import { AppPaths } from 'src/const/urls';
 export const useGoBack = (fallbackPath: AppPaths) => {
   const router = useRouter();
   const handleGoBack = useCallback(() => {
-    if (window.history.length > 1) {
+    const referrer = document.referrer;
+    const isInternalNavigation =
+      referrer && new URL(referrer).origin === window.location.origin;
+
+    if (isInternalNavigation) {
       router.back();
     } else {
       router.push(fallbackPath);
