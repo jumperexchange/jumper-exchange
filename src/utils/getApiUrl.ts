f8eeaf69bebb801592cf924985ec5af012ea0b61
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2025-11-04 11:43:39 +0000

    feat: Support pipeline flow

--- src/utils/getApiUrl.ts
+++ src/utils/getApiUrl.ts
@@ -1,4 +1,8 @@
 import config from '@/config/env-config';
+import {
+  ZAP_EARN_OPPORTUNITY_SLUG_SESSION_STORAGE_KEY,
+  ZAP_QUEST_ID_SESSION_STORAGE_KEY,
+} from 'src/const/quests';
 const getApiUrl = (): string => {
   let apiUrl = config.NEXT_PUBLIC_LIFI_API_URL;
   if (typeof window === 'undefined') {
@@ -9,6 +13,20 @@ const getApiUrl = (): string => {
 
   if (isBetaEnabled) {
     apiUrl = `${apiUrl}/beta`;
+  }
+
+  if (typeof window !== 'undefined') {
+    const hasZapQuestId = sessionStorage.getItem(
+      ZAP_QUEST_ID_SESSION_STORAGE_KEY,
+    );
+    // zaps flow
+    const hasEarnOpportunitySlug = sessionStorage.getItem(
+      ZAP_EARN_OPPORTUNITY_SLUG_SESSION_STORAGE_KEY,
+    );
+
+    return hasZapQuestId || hasEarnOpportunitySlug
+      ? apiUrl.replace('p/lifi', 'pipeline')
+      : apiUrl;
   }
 
   return apiUrl;
