a68b48334af01a4563408890b4b08c3db1027e92
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-11-05 20:47:50 +0700

    fix: limit chains and comment out previous allowed chains

---
+++ src/components/Widgets/variants/base/ZapWidget/constants.ts
@@ -0,0 +1,12 @@
+import { ChainId } from '@lifi/sdk';
+
+export const ZAP_ALLOWED_SOURCE_CHAINS = [
+  ChainId.ETH,
+  ChainId.BAS,
+  ChainId.ARB,
+  ChainId.PLA,
+  ChainId.KAT,
+  ChainId.HYP,
+  ChainId.LNA,
+  ChainId.OPT,
+];
