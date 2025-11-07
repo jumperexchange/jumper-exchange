47bd9c1a7cb39b6528b488b2930372a596a311ba
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-11-07 16:26:06 +0700

    revert: add intercom support (#2383) (#2399)

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,6 +2,7 @@ import Background from '@/components/Background';
 import type { PropsWithChildren } from 'react';
 import { PartnerThemeFooterImage } from './components/PartnerThemeFooterImage';
 import { Snackbar } from './components/Snackbar/Snackbar';
+import { SupportModal } from './components/SupportModal/SupportModal';
 
 interface LayoutProps {
   fixedPoweredBy?: boolean | undefined;
@@ -16,6 +17,7 @@ export const Layout: React.FC<PropsWithChildren<LayoutProps>> = ({
       <Background />
       {/* <AbTests /> */}
       {children}
+      <SupportModal />
       <Snackbar />
       <PartnerThemeFooterImage />
     </>
