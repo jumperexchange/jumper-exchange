6226fa7e511fd7955070144b834b321dded313cc
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2025-11-14 16:34:05 +0000

    chore: Add pipeline test

--- src/utils/getApiUrl.ts
+++ src/utils/getApiUrl.ts
@@ -12,21 +12,7 @@ const getApiUrl = (): string => {
   const isBetaEnabled = window?.localStorage.getItem('use-beta');
 
   if (isBetaEnabled) {
-    apiUrl = `${apiUrl}/beta`;
-  }
-
-  if (typeof window !== 'undefined') {
-    const hasZapQuestId = sessionStorage.getItem(
-      ZAP_QUEST_ID_SESSION_STORAGE_KEY,
-    );
-    // zaps flow
-    const hasEarnOpportunitySlug = sessionStorage.getItem(
-      ZAP_EARN_OPPORTUNITY_SLUG_SESSION_STORAGE_KEY,
-    );
-
-    return hasZapQuestId || hasEarnOpportunitySlug
-      ? apiUrl.replace('p/lifi', 'pipeline')
-      : apiUrl;
+    apiUrl = `${apiUrl.replace('p/lifi', 'pipeline')}/beta`;
   }
 
   return apiUrl;
