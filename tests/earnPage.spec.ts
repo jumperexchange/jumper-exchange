66e1eb54d4aca1243cc993fb354efe2298e27739
Bojan Kopunovic <bkopunovic93@gmail.com>
2025-11-20 13:41:29 +0100

    refactor:  modify earnPage.spec.ts test

--- tests/earnPage.spec.ts
+++ tests/earnPage.spec.ts
@@ -1,158 +1,220 @@
-import { expect, test } from "@playwright/test";
+import { expect, test } from '@playwright/test';
 import {
-	verifyNoSelectedProtocolsAreVisible,
-	selectAllMarketsTab,
-	selectOptionFromDropDown,
-	verifyOnlySelectedAssetIsVisible,
-	verifyAllCardsShowChain,
-	verifyOnlySelectedTagIsVisible,
-	verifyAnalyticsButtonsAreVisible,
-} from "./testData/earnPageFunctions";
+  verifyNoSelectedProtocolsAreVisible,
+  selectAllMarketsTab,
+  selectOptionFromDropDown,
+  verifyOnlySelectedAssetIsVisible,
+  verifyAllCardsShowChain,
+  verifyOnlySelectedTagIsVisible,
+  verifyAnalyticsButtonsAreVisible,
+} from './testData/earnPageFunctions';
 import { qase } from 'playwright-qase-reporter';
 
-test.describe("Chains filters on Earn page", () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto("/earn");
-		await selectAllMarketsTab(page);
-	});
+test.describe('Chains filters on Earn page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/earn');
+    await selectAllMarketsTab(page);
+  });
 
-	test(qase(40, 'Should be able to navigate to the earn page'), async ({ page }) => {
-		await test.step("Navigate to earn page and verify URL", async () => {
-			await expect(page.url()).toContain("/earn");
-		});
+  test(
+    qase(40, 'Should be able to navigate to the earn page'),
+    async ({ page }) => {
+      await test.step('Navigate to earn page and verify URL', async () => {
+        await expect(page.url()).toContain('/earn');
+      });
 
-		await test.step("Verify Earn tabs are visible", async () => {
-			const allMarketsTab = page.getByTestId('earn-filter-tab-all');
-			const forYouTab = page.getByTestId('earn-filter-tab-foryou');
-			const tabs = [allMarketsTab, forYouTab];
-			for (const tab of tabs) {
-				await expect(tab).toBeVisible();
-			}
-		});
+      await test.step('Verify Earn tabs are visible', async () => {
+        const allMarketsTab = page.getByTestId('earn-filter-tab-all');
+        const forYouTab = page.getByTestId('earn-filter-tab-foryou');
+        const tabs = [allMarketsTab, forYouTab];
+        for (const tab of tabs) {
+          await expect(tab).toBeVisible();
+        }
+      });
 
-		await test.step("Validate if filters are visible on All Markets tab", async () => {
-			const filterIds = [
-				"earn-filter-chain-select",
-				"earn-filter-protocol-select",
-				"earn-filter-tag-select",
-				"earn-filter-asset-select",
-				"earn-filter-apy-select",
-			];
-			for (const filterId of filterIds) {
-				await expect(page.getByTestId(filterId)).toBeVisible();
-			}
-		});
-	});
-	test(qase(41, 'Should be able to filter by base chain'), async ({ page }) => {
-		await test.step("Select base chain", async () => {
-			await selectOptionFromDropDown(page, "earn-filter-chain-select", "base");
-		});
+      await test.step('Validate if filters are visible on All Markets tab', async () => {
+        const filterIds = [
+          'earn-filter-chain-select',
+          'earn-filter-protocol-select',
+          'earn-filter-tag-select',
+          'earn-filter-asset-select',
+          'earn-filter-apy-select',
+        ];
+        for (const filterId of filterIds) {
+          await expect(page.getByTestId(filterId)).toBeVisible();
+        }
+      });
+    },
+  );
+  test(qase(41, 'Should be able to filter by base chain'), async ({ page }) => {
+    await test.step('Select base chain', async () => {
+      await selectOptionFromDropDown(page, 'earn-filter-chain-select', 'base');
+    });
 
-		await test.step("Verify all cards show Base chain name", async () => {
-			await verifyAllCardsShowChain(page, "Base");
-		});
-	});
+    await test.step('Verify all cards show Base chain name', async () => {
+      await verifyAllCardsShowChain(page, 'Base');
+    });
+  });
 
-	test(qase(42,'Should be able to filter by arbitrum chain'), async ({ page }) => {
-		await test.step("Select arbitrum chain", async () => {
-			await selectOptionFromDropDown(page, "earn-filter-chain-select", "arbitrum");
-		});
+  test(
+    qase(42, 'Should be able to filter by arbitrum chain'),
+    async ({ page }) => {
+      await test.step('Select arbitrum chain', async () => {
+        await selectOptionFromDropDown(
+          page,
+          'earn-filter-chain-select',
+          'arbitrum',
+        );
+      });
 
-		await test.step("Verify all cards show Arbitrum chain name", async () => {
-			await verifyAllCardsShowChain(page, "Arbitrum");
-		});
-	});
+      await test.step('Verify all cards show Arbitrum chain name', async () => {
+        await verifyAllCardsShowChain(page, 'Arbitrum');
+      });
+    },
+  );
 
-	test(qase(43,'Should be able to filter by mainnet chain'), async ({ page }) => {
-		await test.step("Select mainnet chain", async () => {
-			await selectOptionFromDropDown(page, "earn-filter-chain-select", "mainnet");
-		});
+  test(
+    qase(43, 'Should be able to filter by mainnet chain'),
+    async ({ page }) => {
+      await test.step('Select mainnet chain', async () => {
+        await selectOptionFromDropDown(
+          page,
+          'earn-filter-chain-select',
+          'mainnet',
+        );
+      });
 
-		await test.step("Verify all cards show Mainnet chain name", async () => {
-			await verifyAllCardsShowChain(page, "Mainnet");
-		});
-	});
+      await test.step('Verify all cards show Mainnet chain name', async () => {
+        await verifyAllCardsShowChain(page, 'Mainnet');
+      });
+    },
+  );
 });
 
-test.describe("Protocols filters on Earn page", () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto("/earn");
-		await selectAllMarketsTab(page);
-	});
+test.describe('Protocols filters on Earn page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/earn');
+    await selectAllMarketsTab(page);
+  });
 
-	test(qase(39,'Should be able to filter by Aave protocol'), async ({ page }) => {
-		await test.step("Select Aave protocol", async () => {
-			await selectOptionFromDropDown(page, "earn-filter-protocol-select", "aave");
-		});
+  test(
+    qase(39, 'Should be able to filter by Aave protocol'),
+    async ({ page }) => {
+      await test.step('Select Aave protocol', async () => {
+        await selectOptionFromDropDown(
+          page,
+          'earn-filter-protocol-select',
+          'aave',
+        );
+      });
 
-		await test.step("Verify no morpho protocol is visible after selecting aave protocol", async () => {
-			await verifyNoSelectedProtocolsAreVisible(page, "morpho");
-		});
-	});
+      await test.step('Verify no morpho protocol is visible after selecting aave protocol', async () => {
+        await verifyNoSelectedProtocolsAreVisible(page, 'morpho');
+      });
+    },
+  );
 
-	test(qase(38, 'Should be able to filter by morpho protocol'), async ({ page }) => {
-		await test.step("Select morpho protocol", async () => {
-			await selectOptionFromDropDown(page, "earn-filter-protocol-select", "morpho");
-		});
+  test(
+    qase(38, 'Should be able to filter by morpho protocol'),
+    async ({ page }) => {
+      await test.step('Select morpho protocol', async () => {
+        await selectOptionFromDropDown(
+          page,
+          'earn-filter-protocol-select',
+          'morpho',
+        );
+      });
 
-		await test.step("Verify no aave protocol is visible after selecting morpho protocol", async () => {
-			await verifyNoSelectedProtocolsAreVisible(page, "aave");
-		});
-	});
+      await test.step('Verify no aave protocol is visible after selecting morpho protocol', async () => {
+        await verifyNoSelectedProtocolsAreVisible(page, 'aave');
+      });
+    },
+  );
+});
 
-	});
+test.describe('Assets filters on Earn page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/earn');
+    await selectAllMarketsTab(page);
+  });
 
-	test.describe("Assets filters on Earn page", () => {
-		test.beforeEach(async ({ page }) => {
-			await page.goto("/earn");
-			await selectAllMarketsTab(page);
-		});
+  test(qase(44, 'Should be able to filter by EURC asset'), async ({ page }) => {
+    await test.step('Select EURC asset', async () => {
+      await selectOptionFromDropDown(page, 'earn-filter-asset-select', 'EURC');
+    });
 
-		test(qase(44,'Should be able to filter by Maker asset'), async ({ page }) => {
-			await test.step("Select MKR asset", async () => {
-				await selectOptionFromDropDown(page, "earn-filter-asset-select", "Maker");
-			});
+    await test.step('Verify only EURC asset is visible', async () => {
+      await verifyOnlySelectedAssetIsVisible(page, 'EURC');
+    });
+  });
+});
+test.describe('Tags filters on Earn page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/earn');
+    await selectAllMarketsTab(page);
+  });
 
-			await test.step("Verify only MKR asset is visible", async () => {
-				await verifyOnlySelectedAssetIsVisible(page, "MKR");
-			});
-		});
+  test(
+    qase(45, 'Should be able to filter by Synthetic tag'),
+    async ({ page }) => {
+      await test.step('Select Lending tag', async () => {
+        await selectOptionFromDropDown(
+          page,
+          'earn-filter-tag-select',
+          'Synthetic',
+        );
+      });
 
-	});
-	test.describe("Tags filters on Earn page", () => {
-		test.beforeEach(async ({ page }) => {
-			await page.goto("/earn");
-			await selectAllMarketsTab(page);
-		});
+      await test.step('Verify only Synthetic tag is visible', async () => {
+        await verifyOnlySelectedTagIsVisible(page, 'Synthetic');
+      });
+    },
+  );
 
-	test(qase(45,'Should be able to filter by Lending tag'), async ({ page }) => {
-		await test.step("Select Lending tag", async () => {
-			await selectOptionFromDropDown(page, "earn-filter-tag-select", "Lending");
-		});
+  test(
+    qase(46, 'Should be able to filter by Yield Aggregator tag'),
+    async ({ page }) => {
+      await test.step('Select Yield Aggregator tag', async () => {
+        await selectOptionFromDropDown(
+          page,
+          'earn-filter-tag-select',
+          'Yield Aggregator',
+        );
+      });
 
-		await test.step("Verify only Lending tag is visible", async () => {
-			await verifyOnlySelectedTagIsVisible(page, "Lending");
-		});
-	});
+      await test.step('Verify only Yield Aggregator tag is visible', async () => {
+        await verifyOnlySelectedTagIsVisible(page, 'Liquid Staking');
+      });
+    },
+  );
+  test(
+    qase(46, 'Should be able to filter by Liquid Staking tag'),
+    async ({ page }) => {
+      await test.step('Select Yield Aggregator tag', async () => {
+        await selectOptionFromDropDown(
+          page,
+          'earn-filter-tag-select',
+          'Liquid Staking',
+        );
+      });
 
-	test(qase(46,'Should be able to filter by Yield Aggregator tag'), async ({ page }) => {
-		await test.step("Select Yield Aggregator tag", async () => {
-			await selectOptionFromDropDown(page, "earn-filter-tag-select", "Yield Aggregator");
-		});
+      await test.step('Verify only Liquid Staking tag is visible', async () => {
+        await verifyOnlySelectedTagIsVisible(page, 'Liquid Staking');
+      });
+    },
+  );
+  test.describe('Analytics filters on Earn page', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/earn/hyperbeat-ultra-hype-on-hyperliquid');
+    });
 
-		await test.step("Verify only Yield Aggregator tag is visible", async () => {
-			await verifyOnlySelectedTagIsVisible(page, "Yield Aggregator");
-		});
-	});
-	});
-	test.describe("Analytics filters on Earn page", () => {
-		test.beforeEach(async ({ page }) => {
-			await page.goto("/earn/aave-v3-usdc-on-mainnet");
-		});
-
-		test(qase(47,'Should be able to verify analytics buttons are visible'), async ({ page }) => {
-			await test.step("Verify analytics range filters are visible", async () => {
-				await verifyAnalyticsButtonsAreVisible(page);
-			});
-		});
-	});
+    test(
+      qase(47, 'Should be able to verify analytics buttons are visible'),
+      async ({ page }) => {
+        await test.step('Verify analytics range filters are visible', async () => {
+          await verifyAnalyticsButtonsAreVisible(page);
+        });
+      },
+    );
+  });
+});
