b7886f11d1764ea887b0cbbb2a5ee74930bfacd6
Bojan Kopunovic <bkopunovic93@gmail.com>
2025-11-20 10:12:56 +0100

    fix: skip wallet connection tests in connectWallet.spec.ts

--- tests/connectWallet.spec.ts
+++ tests/connectWallet.spec.ts
@@ -18,55 +18,58 @@ import { qase } from 'playwright-qase-reporter';
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
 const { expect } = test;
 
-test.describe('Connect/disconnect Metamask with Jumper app and open /profile page', () => {
-  test(
-    qase(3, 'Complete wallet connection and disconnection flow'),
-    async ({ context, page, extensionId }) => {
-      await test.step('Connect Metamask wallet to Jumper', async () => {
-        await connectMetaMaskWallet(context, page, extensionId);
-      });
-
-      await test.step('Close welcome screen and navigate to profile', async () => {
-        await closeWelcomeScreen(page);
-        await triggerButtonClick(page, 'Pass');
-        await page.locator('.profile-page').isVisible();
-      });
-
-      await test.step('Check Perks and Achievements tabs', async () => {
-        const perksTab = await page.locator('#profile-tabs-perks');
-        const achievementsTab = await page.locator(
-          '#profile-tabs-achievements',
-        );
-        const startSwappingButton = await page.getByRole('link', {
-          name: 'Start swapping',
+test.describe.skip(
+  'Connect/disconnect Metamask with Jumper app and open /profile page',
+  () => {
+    test(
+      qase(3, 'Complete wallet connection and disconnection flow'),
+      async ({ context, page, extensionId }) => {
+        await test.step('Connect Metamask wallet to Jumper', async () => {
+          await connectMetaMaskWallet(context, page, extensionId);
         });
-        await expect(achievementsTab).toBeVisible();
-        await achievementsTab.click();
-        await expect(startSwappingButton).not.toBeVisible();
-        await perksTab.click();
-        const perkCards = page.locator('[data-testid="perks-card"]');
-        const cardCount = await perkCards.count();
-        expect(cardCount).toBeGreaterThan(1);
-      });
 
-      await test.step('Check transaction history', async () => {
-        const noRecentTransactions = page.locator(
-          'xpath=//p[normalize-space(text())="No recent transactions"]',
-        );
-        const transactionHistoryButton = page.locator(
-          '//button[@aria-label="Transaction history"]',
-        );
-        await clickOnJumperLogo(page);
-        await transactionHistoryButton.click();
-        await expect(noRecentTransactions).toBeVisible();
-      });
+        await test.step('Close welcome screen and navigate to profile', async () => {
+          await closeWelcomeScreen(page);
+          await triggerButtonClick(page, 'Pass');
+          await page.locator('.profile-page').isVisible();
+        });
 
-      await test.step('Disconnect wallet from the Jumper app ', async () => {
-        await connectedWalletButton(page); // click on the connected wallet icon
-        await expect(connectAnotherWalletButton(page)).toBeVisible();
-        await disconnectWalletButton(page).click();
-        await expect(connectButton(page)).toHaveText('Connect');
-      });
-    },
-  );
-});
+        await test.step('Check Perks and Achievements tabs', async () => {
+          const perksTab = await page.locator('#profile-tabs-perks');
+          const achievementsTab = await page.locator(
+            '#profile-tabs-achievements',
+          );
+          const startSwappingButton = await page.getByRole('link', {
+            name: 'Start swapping',
+          });
+          await expect(achievementsTab).toBeVisible();
+          await achievementsTab.click();
+          await expect(startSwappingButton).not.toBeVisible();
+          await perksTab.click();
+          const perkCards = page.locator('[data-testid="perks-card"]');
+          const cardCount = await perkCards.count();
+          expect(cardCount).toBeGreaterThan(1);
+        });
+
+        await test.step('Check transaction history', async () => {
+          const noRecentTransactions = page.locator(
+            'xpath=//p[normalize-space(text())="No recent transactions"]',
+          );
+          const transactionHistoryButton = page.locator(
+            '//button[@aria-label="Transaction history"]',
+          );
+          await clickOnJumperLogo(page);
+          await transactionHistoryButton.click();
+          await expect(noRecentTransactions).toBeVisible();
+        });
+
+        await test.step('Disconnect wallet from the Jumper app ', async () => {
+          await connectedWalletButton(page); // click on the connected wallet icon
+          await expect(connectAnotherWalletButton(page)).toBeVisible();
+          await disconnectWalletButton(page).click();
+          await expect(connectButton(page)).toHaveText('Connect');
+        });
+      },
+    );
+  },
+);
