6e174f677ec7a1fd9cc9c428847c860424045611
BRAMILLE Sébastien <2752200+oktapodia@users.noreply.github.com>
2025-11-21 09:40:38 +0000

    fix: explorer: remove custom apiUrl (#2439)

--- src/app/ui/scan/ScanPage.tsx
+++ src/app/ui/scan/ScanPage.tsx
@@ -26,7 +26,6 @@ export default function ScanPage({ lng }: { lng: string }) {
 
   const explorerConfig = useMemo(
     () => ({
-      apiUrl: config.NEXT_PUBLIC_LIFI_API_URL,
       // appearance: 'light' as PaletteMode, // This controls light and dark mode
       integrator: config.NEXT_PUBLIC_WIDGET_INTEGRATOR, // TODO: change as needed
       base: `${lng !== fallbackLng ? `${lng}` : ''}${JUMPER_SCAN_PATH}`, // Important for the routing and having everything served under /scan. Do not remove!
