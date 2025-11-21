8d499d7d6c693e92ed69837ed0c53ae71e21cc9f
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-11-21 11:29:05 +0100

    chore: refactor

--- src/app/ui/portfolio/PortfolioTokensList.tsx
+++ src/app/ui/portfolio/PortfolioTokensList.tsx
@@ -1,41 +1,16 @@
 import { TokenListCard } from 'src/components/composite/TokenListCard/TokenListCard';
 import { usePortfolioTokensFiltering } from './PortfolioTokensFilteringContext';
-import { useMemo } from 'react';
 import { TokenListCardTokenSize } from 'src/components/composite/TokenListCard/TokenListCard.types';
 import {
   PortfolioAssetContainer,
   PortfolioAssetsListContainer,
 } from './PortfolioPage.styles';
+import { useFormatDisplayWalletTokens } from '@/hooks/portfolio/useFormatDisplayWalletTokens';
 
 export const PortfolioTokensList = () => {
   const { data, filter } = usePortfolioTokensFiltering();
 
-  const tokens = useMemo(() => {
-    if (!data || data.length === 0) {
-      return [];
-    }
-
-    return data.map((token) => ({
-      address: token.address,
-      symbol: token.symbol,
-      chain: {
-        chainId: token.chainId,
-        chainKey: 'chainName' in token ? (token.chainName ?? '') : '',
-      },
-      balance: token.cumulatedBalance ?? 0,
-      totalPriceUSD: token.cumulatedTotalUSD ?? 0,
-      relatedTokens: token.chains.map((chain) => ({
-        address: chain.address,
-        symbol: chain.symbol,
-        chain: {
-          chainId: chain.chainId,
-          chainKey: chain.chainName ?? '',
-        },
-        balance: chain.cumulatedBalance ?? 0,
-        totalPriceUSD: chain.totalPriceUSD ?? 0,
-      })),
-    }));
-  }, [data]);
+  const tokens = useFormatDisplayWalletTokens(data);
 
   return (
     <PortfolioAssetsListContainer useFlexGap direction="column">
