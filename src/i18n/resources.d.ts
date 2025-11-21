cf8c9c46187805815a0747144ce6d1dba5ea8709
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-11-21 09:23:39 +0100

    feat: add wallets generic button for multiple connected accounts

--- src/i18n/resources.d.ts
+++ src/i18n/resources.d.ts
@@ -294,6 +294,7 @@ interface Resources {
         support: 'Support';
         theme: 'Theme';
       };
+      pass: 'Pass';
       seeAllWallets: 'See all wallets';
       statsCards: {
         bridges: 'Bridges';
@@ -336,6 +337,7 @@ interface Resources {
         };
         wallets: 'Wallets';
       };
+      wallets: 'Wallets';
       welcome: {
         cta: 'Get started';
         subtitle: '<0>4x audited</0> multi-chain liquidity aggregator';
