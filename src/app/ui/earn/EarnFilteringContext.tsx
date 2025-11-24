7b62ee7268b2e58cf53288bcd776f4cc444f80d0
Laurent Senta <laurent@singulargarden.com>
2025-11-24 09:41:00 +0100

    fix: no updated time on all markets

--- src/app/ui/earn/EarnFilteringContext.tsx
+++ src/app/ui/earn/EarnFilteringContext.tsx
@@ -119,7 +119,6 @@ export const EarnFilteringProvider = ({
   const forYouUpdatedAt = forYou.data?.meta?.updatedAt ?? undefined;
 
   const allData = useMemo(() => all.data?.data ?? [], [all.data]);
-  const allUpdatedAt = all.data?.meta?.updatedAt ?? undefined;
 
   const allNoFilterData = useMemo(
     () => allNoFilter.data?.data ?? [],
@@ -178,7 +177,7 @@ export const EarnFilteringProvider = ({
     toggleForYou,
     totalMarkets,
     data: showForYou ? forYouData : allData,
-    updatedAt: showForYou ? forYouUpdatedAt : allUpdatedAt,
+    updatedAt: showForYou ? forYouUpdatedAt : undefined,
     isLoading: showForYou ? forYou.isLoading || !address : all.isLoading,
     error: (showForYou ? forYou.error : all.error) ?? null,
     isAllDataLoading: allNoFilter.isLoading,
