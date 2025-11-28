b2f7f3a26daf12e0dc287de7843cde5fd59bc402
BRAMILLE Sébastien <2752200+oktapodia@users.noreply.github.com>
2025-11-28 09:11:50 +0000

    feat: earn: Remove Day from chart analytics (#2458)

--- src/components/EarnDetails/hooks.ts
+++ src/components/EarnDetails/hooks.ts
@@ -1,14 +1,14 @@
 import { useColorScheme, useTheme } from '@mui/material/styles';
 import { useState, useCallback, useMemo } from 'react';
-import { EarnOpportunityAnalyticsQuery } from 'src/app/lib/getOpportunityAnalytics';
+import type { EarnOpportunityAnalyticsQuery } from 'src/app/lib/getOpportunityAnalytics';
 import { useEarnAnalytics } from 'src/hooks/earn/useEarnAnalytics';
-import { EarnOpportunityHistory } from 'src/types/jumper-backend';
+import type { EarnOpportunityHistory } from 'src/types/jumper-backend';
 import { AnalyticsRangeFieldEnum, AnalyticsValueFieldEnum } from './types';
 
 export const useAnalyticsQuery = (slug: string) => {
   const [query, setQuery] = useState<EarnOpportunityAnalyticsQuery>({
     value: AnalyticsValueFieldEnum.APY,
-    range: AnalyticsRangeFieldEnum.DAY,
+    range: AnalyticsRangeFieldEnum.WEEK,
   });
 
   const result = useEarnAnalytics({ slug, query });
@@ -59,13 +59,11 @@ export const useAnalyticsChartData = (
     return {
       data,
       dateFormat:
-        range === AnalyticsRangeFieldEnum.DAY
-          ? 'PP @ HH:mm'
-          : range === AnalyticsRangeFieldEnum.WEEK
+        range === AnalyticsRangeFieldEnum.WEEK
+          ? 'PP'
+          : range === AnalyticsRangeFieldEnum.MONTH
             ? 'PP'
-            : range === AnalyticsRangeFieldEnum.MONTH
-              ? 'PP'
-              : 'MMM yyyy',
+            : 'MMM yyyy',
       theme: {
         areaTopColor: isLightTheme
           ? `#F2D9F6`
