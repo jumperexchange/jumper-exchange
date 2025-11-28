71095edf967fe8784e2c3650a26b5eb67a4efae5
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-11-28 11:08:32 +0100

    chore: update after cr

--- src/hooks/portfolio/useFormatDisplayDeFiPositions.ts
+++ src/hooks/portfolio/useFormatDisplayDeFiPositions.ts
@@ -2,10 +2,12 @@ import type { DefiPosition } from '@/types/jumper-backend';
 import { groupBy } from 'lodash';
 import { useMemo } from 'react';
 
+const defaultGroupByProtocolName = (position: DefiPosition) =>
+  position.protocol.name;
+
 export const useFormatDisplayDeFiPositions = (
   positions?: DefiPosition[],
-  groupByFn: (position: DefiPosition) => string = (position) =>
-    position.protocol.name,
+  groupByFn: (position: DefiPosition) => string = defaultGroupByProtocolName,
 ) => {
   return useMemo(() => {
     const groupedData = groupBy(positions ?? [], groupByFn);
