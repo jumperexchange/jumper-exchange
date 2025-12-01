d06ddfa247b750e572cf4f80770a0d4b14d86d56
Bojan Kopunovic <bkopunovic93@gmail.com>
2025-12-01 13:16:24 +0100

    fix: update visibility checks in connectWallet and mainMenu tests

--- tests/mainMenu.spec.ts
+++ tests/mainMenu.spec.ts
@@ -30,8 +30,8 @@ test.describe('Main Menu flows', () => {
   test(
     qase(12, 'Should be able to open menu and close it'),
     async ({ page }) => {
-      openOrCloseMainMenu(page);
-      await checkTheNumberOfMenuItems(page, 5);
+      await openOrCloseMainMenu(page);
+      await checkTheNumberOfMenuItems(page, 6);
       await page.locator('body').click();
       await expect(page.getByRole('menu')).not.toBeVisible();
     },
