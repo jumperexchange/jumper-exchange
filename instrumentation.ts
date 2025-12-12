f8e3e75b3cca446ac592dcaa4d8a93f7e80d1d83
Mihaela-Ioana Mot <mmioana94@gmail.com>
2025-12-12 15:27:32 +0100

    chore: remove import from edge runtime

--- instrumentation.ts
+++ instrumentation.ts
@@ -7,7 +7,6 @@ export async function register() {
 
   if (process.env.NEXT_RUNTIME === 'edge') {
     await import('./sentry.edge.config');
-    await import('./src/utils/api/global-interceptor');
   }
 }
 
