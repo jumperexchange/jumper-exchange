581691958f7577eb7bd1463ae94371ae87671cd8
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2025-12-29 18:43:18 +0700

    fix: sdk integrator based on pathname (#2549)

---
+++ src/utils/widgets/getPathBasedIntegrator.ts
@@ -0,0 +1,12 @@
+import config from '@/config/env-config';
+import { AppPaths } from '@/const/urls';
+
+export const getPathBasedIntegrator = (pathname?: string | null): string => {
+  const earnRelatedPaths = [AppPaths.Earn, AppPaths.Portfolio];
+  const isEarnRelatedPath = earnRelatedPaths.some(
+    (path) => pathname === path || pathname?.startsWith(path + '/'),
+  );
+  return isEarnRelatedPath
+    ? config.NEXT_PUBLIC_WIDGET_INTEGRATOR_EARN
+    : config.NEXT_PUBLIC_WIDGET_INTEGRATOR;
+};
