e55de2897cf0e84cee4e92303a7f55ea55997073
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-01-08 09:26:25 +0100

    fix: clear all button on mobile

--- src/components/composite/MultiLayerDrawer/MultiLayerDrawer.styles.tsx
+++ src/components/composite/MultiLayerDrawer/MultiLayerDrawer.styles.tsx
@@ -36,8 +36,11 @@ export const MultiLayerDrawerPrimaryButton = styled(ButtonPrimary)(
 
 export const MultiLayerDrawerAlphaButton = styled(ButtonTransparent)(
   ({ theme }) => ({
-    backgroundColor: (theme.vars || theme).palette.buttonAlphaLightBg,
-    color: (theme.vars || theme).palette.buttonAlphaLightAction,
+    backgroundColor: (theme.vars || theme).palette.buttonAlphaDarkBg,
+    color: (theme.vars || theme).palette.buttonAlphaDarkAction,
+    ...theme.applyStyles('light', {
+      color: (theme.vars || theme).palette.buttonAlphaLightAction,
+    }),
     '&:hover': {
       backgroundColor: (theme.vars || theme).palette.buttonActiveBg,
       color: (theme.vars || theme).palette.buttonActiveAction,
