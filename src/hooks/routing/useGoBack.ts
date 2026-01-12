2f01305ec01eded718724290395ca31b2af0665e
Nicolas KOWALSKI <kowalski.kowalskin@gmail.com>
2026-01-12 12:07:17 +0100

    fix: referrer might be empty on same site

--- src/hooks/routing/useGoBack.ts
+++ src/hooks/routing/useGoBack.ts
@@ -1,14 +1,14 @@
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
-import { AppPaths } from 'src/const/urls';
+import { type AppPaths } from 'src/const/urls';
 
 export const useGoBack = (fallbackPath: AppPaths) => {
   const router = useRouter();
   const handleGoBack = useCallback(() => {
     const referrer = document.referrer;
     const isInternalNavigation =
-      referrer && new URL(referrer).origin === window.location.origin;
-
+      referrer === '' ||
+      (referrer && new URL(referrer).origin === window.location.origin);
     if (isInternalNavigation) {
       router.back();
     } else {
