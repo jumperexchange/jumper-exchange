181c315d85bf54ba57c8e55bd1788f6fb88b5eed
Laurent Senta <laurent@singulargarden.com>
2026-01-16 10:00:01 +0100

    feat: bounce gas widget

--- src/app/[lng]/(main)/gas/page.tsx
+++ src/app/[lng]/(main)/gas/page.tsx
@@ -1,12 +1,12 @@
 import { Widgets } from '@/components/Widgets';
-import { Widget } from '@/components/Widgets/Widget';
+import { BouncedWidget } from '@/jumperFlags/bouncer/BouncedWidget';
 import { MainWidgetContainer } from 'src/components/Containers/MainWidgetContainer';
 
 const Page = async () => {
   const variant = 'refuel';
   return (
     <MainWidgetContainer>
-      <Widget starterVariant={variant} />
+      <BouncedWidget starterVariant={variant} />
       <Widgets widgetVariant={variant} />
     </MainWidgetContainer>
   );
