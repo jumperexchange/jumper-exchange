4b0597607b06bfacb32bb284f76daf4e0b021876
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-01-21 16:22:27 +0100

    chore: override theme components for wallet mgmt

--- src/providers/ThemeProvider/WalletManagementThemeProvider.tsx
+++ src/providers/ThemeProvider/WalletManagementThemeProvider.tsx
@@ -3,16 +3,21 @@ import { ThemeProvider } from '@mui/material';
 import { useMemo } from 'react';
 import { createTheme } from '@lifi/widget';
 import { useGetPartnerWidgetTheme } from '@/hooks/theme/useGetPartnerWidgetTheme';
+import { deepmerge } from '@mui/utils';
 
 export const WalletManagementThemeProvider: React.FC<
   React.PropsWithChildren
 > = ({ children }) => {
   const widgetTheme = useGetPartnerWidgetTheme();
 
-  const theme = useMemo(
-    () => createTheme(widgetTheme.config.theme),
-    [widgetTheme.config.theme],
-  );
+  const theme = useMemo(() => {
+    const _theme = createTheme(widgetTheme.config.theme);
+    _theme.components = deepmerge(
+      _theme.components,
+      widgetTheme.config.theme?.components ?? {},
+    );
+    return _theme;
+  }, [widgetTheme.config.theme]);
 
   return (
     <ThemeProvider
