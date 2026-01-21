c88714c26570e0aa1c658000d28407545372feff
Laurent Senta <laurent@singulargarden.com>
2026-01-21 18:04:21 +0100

    feat: stacked APY rewards -- LF-17256 (#2620)

---
+++ src/components/EarnDetails/EarnDetailsApyChart.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEarnApyAnalytics } from 'src/hooks/earn/useEarnApyAnalytics';
+import { StackedAreaChart } from '../core/charts/StackedAreaChart/StackedAreaChart';
+import { useApyAnalyticsChartConfig } from './hooks';
+import type { AnalyticsRangeFieldEnum } from './types';
+
+interface EarnDetailsApyChartProps {
+  slug: string;
+  range: AnalyticsRangeFieldEnum;
+}
+
+export const EarnDetailsApyChart: React.FC<EarnDetailsApyChartProps> = ({
+  slug,
+  range,
+}) => {
+  const { isLoading, data: rawData } = useEarnApyAnalytics({ slug, range });
+  const config = useApyAnalyticsChartConfig(rawData, range);
+
+  return (
+    <StackedAreaChart
+      isLoading={isLoading}
+      data-testid="analytics-chart"
+      {...config}
+    />
+  );
+};
---
+++ src/hooks/earn/useEarnApyAnalytics.ts
@@ -0,0 +1,34 @@
+import type { ApyAnalyticsHistory } from '@/types/jumper-backend';
+import type { UseQueryResult } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
+import type { ApyAnalyticsRangeField } from 'src/app/lib/getOpportunityApyAnalytics';
+import { getOpportunityApyAnalytics } from 'src/app/lib/getOpportunityApyAnalytics';
+import { FIVE_MINUTES_MS } from 'src/const/time';
+
+export interface UseEarnApyAnalyticsProps {
+  slug: string;
+  range: ApyAnalyticsRangeField;
+}
+
+export type UseEarnApyAnalyticsResult = UseQueryResult<
+  ApyAnalyticsHistory,
+  unknown
+>;
+
+export const useEarnApyAnalytics = ({
+  slug,
+  range,
+}: UseEarnApyAnalyticsProps): UseEarnApyAnalyticsResult => {
+  return useQuery({
+    queryKey: ['earn-apy-analytics', slug, range],
+    queryFn: async () => {
+      const result = await getOpportunityApyAnalytics(slug, { range });
+      if (!result.ok) {
+        throw result.error;
+      }
+      // @ts-expect-error: see LF-15589 - we are transforming data in the backend
+      return result.data.data;
+    },
+    refetchInterval: FIVE_MINUTES_MS,
+  });
+};
