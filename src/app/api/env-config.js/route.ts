957e8451bbaec4f4fb1af38d28ca28b74e99e650
BRAMILLE Sébastien <2752200+oktapodia@users.noreply.github.com>
2026-01-29 14:46:35 +0000

    fix: env-config.js content-type (#2651)

--- src/app/api/env-config.js/route.ts
+++ src/app/api/env-config.js/route.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-
 import { NextResponse } from 'next/server';
 import { getPublicEnvVars } from 'src/config/env-config';
 
@@ -8,5 +6,11 @@ export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   return new NextResponse(
     `window._env_ = ` + JSON.stringify(getPublicEnvVars()),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/javascript; charset=utf-8',
+      },
+    },
   );
 }
