3d228cf23d0cd35dc2abbb6843bf818a77a6ddfc
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2026-01-29 13:47:08 +0000

    fix: env-config.js content-type

--- src/app/api/env-config.js/route.ts
+++ src/app/api/env-config.js/route.ts
@@ -8,5 +8,11 @@ export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   return new NextResponse(
     `window._env_ = ` + JSON.stringify(getPublicEnvVars()),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/javascript; charset=utf-8',
+      },
+    },
   );
 }
