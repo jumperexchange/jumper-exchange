6cddf236045e6aa45280fedbf2bd8a0c20a814bc
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-02-16 12:11:25 +0100

    chore: fix after cr

--- src/app/[lng]/error-preview/[statusCode]/page.tsx
+++ src/app/[lng]/error-preview/[statusCode]/page.tsx
@@ -2,7 +2,7 @@ import { HttpError } from '@/types/http-error';
 import { notFound, redirect } from 'next/navigation';
 import { isProduction } from '@/utils/isProduction';
 
-type Params = Promise<{ statusCode: number }>;
+type Params = Promise<{ statusCode: string }>;
 
 interface ErrorPreviewPageProps {
   params: Params;
@@ -15,13 +15,14 @@ export default async function ErrorPreviewPage({
     return redirect('/');
   }
   const { statusCode } = await params;
+  const statusCodeNum = Number(statusCode);
 
-  if (Number.isNaN(statusCode)) {
+  if (Number.isNaN(statusCodeNum)) {
     return notFound();
   }
 
   throw new HttpError(
     'Error preview: intentional throw to test error page UI.',
-    statusCode,
+    statusCodeNum,
   );
 }
