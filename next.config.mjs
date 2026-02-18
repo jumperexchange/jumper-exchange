2be1e0fa03078f37d53fcef8acff5156fa12898e
operagxsasha <operagxsasha@gmail.com>
2026-02-18 14:24:48 +0200

    chore: remove duplicate image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -119,36 +119,6 @@ const nextConfig = {
         port: '',
         pathname: '/lifinance/types/main/src/assets/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'assets.coingecko.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 's2.coinmarketcap.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.debank.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.sei.io',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.etherscan.io',
-        port: '',
-        pathname: '/token/images/**',
-      },
     ],
   },
   async redirects() {
