f0c2fc3474ecce0d9af307dac8193ea89c081726
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-02-26 09:48:26 +0100

    chore: use batch fetcher for tokens

--- src/hooks/useTokens.ts
+++ src/hooks/useTokens.ts
@@ -6,25 +6,49 @@ import type { Address } from 'viem';
 
 import { ExtendedToken } from '../utils/Token';
 import { getQueryKey } from '@/utils/queries/getQueryKey';
+import { createBatchFetcher } from '@/utils/batches/fetcher';
 
 // NOTE: We are using the StaticToken type here as USD prices from the /tokens
 // endpoint tend to be incorrect. Use useToken() instead
 // TODO: This needs to be StaticToken
 export type AllTokens = { tokens: { [chainId: number]: Token[] } };
 
-export const getTokensQuery = async (): Promise<AllTokens> => {
-  const data = await getTokens({
-    chainTypes: [ChainType.EVM, ChainType.SVM, ChainType.UTXO, ChainType.MVM],
-  });
+const TOKEN_CHAIN_TYPES: ChainType[] = [
+  ChainType.EVM,
+  ChainType.SVM,
+  ChainType.UTXO,
+  ChainType.MVM,
+];
 
-  return data as { tokens: { [chainId: number]: Token[] } };
+const tokensBatchesByChainType: Record<string, ChainType[]> =
+  Object.fromEntries(
+    TOKEN_CHAIN_TYPES.map((chainType) => [chainType, [chainType]]),
+  );
+
+export const getTokensQuery = async (
+  signal?: AbortSignal,
+): Promise<AllTokens> => {
+  const { results } = createBatchFetcher<ChainType, AllTokens>(
+    tokensBatchesByChainType,
+    async (_batchKey, chainTypes) => {
+      const data = await getTokens({ chainTypes: [...chainTypes] });
+      return [data];
+    },
+    {},
+    { concurrency: 4 },
+    signal,
+  );
+
+  const resultsList = await results;
+
+  return Object.assign({}, ...resultsList.map((r) => r.tokens));
 };
 
 export const useTokens = () => {
   const { data, isLoading, isSuccess, isError, error, dataUpdatedAt } =
     useQuery({
       queryKey: [getQueryKey('tokens', 'jumper-default')],
-      queryFn: getTokensQuery,
+      queryFn: ({ signal }) => getTokensQuery(signal),
       refetchInterval: 1000 * 60 * 60,
       refetchOnMount: false,
       refetchOnWindowFocus: false,
