1747f613a7330ab5818a31888cc8b3d9d3b6a270
Nicolas KOWALSKI <kowalski.kowalskin@gmail.com>
2026-02-26 12:47:44 +0100

    feat: Added frontend action for widget

--- src/components/Widgets/variants/widgetConfig/useMainWidgetConfig.ts
+++ src/components/Widgets/variants/widgetConfig/useMainWidgetConfig.ts
@@ -1,12 +1,18 @@
-import { useMemo } from 'react';
+import type { FrontendAction } from '@lifi/sdk';
 import type { WidgetConfig } from '@lifi/widget';
-import { HiddenUI, RequiredUI, ChainId } from '@lifi/widget';
-import { ThemesMap } from 'src/const/themesMap';
-import type { MainWidgetContext, HookDependencies } from './types';
-import { useMemelist } from 'src/hooks/useMemelist';
+import { ChainId, HiddenUI, RequiredUI } from '@lifi/widget';
+import { AttachmentSharp } from '@mui/icons-material';
+import { OfframpClient, peerExtensionSdk } from '@zkp2p/sdk';
+import { useMemo } from 'react';
 import { tokens } from 'src/config/tokens';
-import { generateRouteLabel } from './utils';
+import { ThemesMap } from 'src/const/themesMap';
+import { useMemelist } from 'src/hooks/useMemelist';
+import { createWalletClient, http, type WalletClient } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { base } from 'viem/chains';
 import { themeAllowChains } from '../../Widget.types';
+import type { HookDependencies, MainWidgetContext } from './types';
+import { generateRouteLabel } from './utils';
 
 /**
  * Configuration hook for the main widget variant
@@ -104,6 +110,108 @@ export function useMainWidgetConfig(
           'https://raw.githubusercontent.com/lifinance/types/main/src/assets/icons/exchanges/hyperflow.svg',
         ),
       ],
+      sdkConfig: {
+        executionOptions: {
+          executeFrontendActionHook: ({
+            toolDetails,
+            data,
+          }: FrontendAction) => {
+            if (toolDetails.key === 'zkp2p') {
+              if (!data?.intentHash) {
+                throw new Error(
+                  'Missing intent hash for zkP2P frontend action',
+                );
+              }
+              peerExtensionSdk.onramp({
+                intentHash: data.intentHash,
+              });
+
+              return new Promise((resolve, reject) => {
+                const unsubscribe = peerExtensionSdk.onProofComplete(
+                  (result) => {
+                    switch (result.status) {
+                      case 'success': {
+                        if (!result.proof) {
+                          reject(
+                            new Error(
+                              'Proof result is missing from Peer transcript',
+                            ),
+                          );
+                          break;
+                        }
+                        console.log('Proof completed!', result.proof);
+                        const READONLY_PRIVATE_KEY =
+                          '0x0000000000000000000000000000000000000000000000000000000000000001';
+                        const account =
+                          privateKeyToAccount(READONLY_PRIVATE_KEY);
+                        const rpcUrl = base.rpcUrls.default.http[0];
+
+                        const walletClient: WalletClient = createWalletClient({
+                          account,
+                          chain: base,
+                          transport: http(rpcUrl),
+                        });
+                        const client = new OfframpClient({
+                          walletClient,
+                          chainId: data.chainId,
+                          rpcUrl,
+                          baseApiUrl: 'https://api.zkp2p.xyz',
+                          runtimeEnv: 'production',
+                          apiKey: process.env.NEXT_PUBLIC_ZKP2P_API_KEY,
+                        });
+                        client.fulfillIntent
+                          .prepare({
+                            intentHash: data.intentHash,
+                            proof: result.proof,
+                          })
+                          .then((intentTransaction) => {
+                            resolve({
+                              transactionRequest: {
+                                to: intentTransaction.to,
+                                data: intentTransaction.data,
+                                value: intentTransaction.value,
+                              },
+                            });
+                          });
+                        break;
+                      }
+                      case 'failure':
+                        reject(
+                          new Error(
+                            `Peer Proof failed ${result.error?.message}`,
+                          ),
+                        );
+                        break;
+
+                      case 'cancelled':
+                        // reject(new Error(`User cancelled`));
+                        console.log('cancelled');
+                        resolve({
+                          transactionRequest: {
+                            from: '0x5BdEDCC02d09033C56a9d4bbAba6d23cc2ABEdDf',
+                            to: '0x5BdEDCC02d09033C56a9d4bbAba6d23cc2ABEdDf',
+                            data: '0x1',
+                            value: BigInt('0'),
+                            chainId: base.id,
+                          },
+                        });
+                        break;
+                      case 'timeout':
+                        reject(new Error('Proof timed out'));
+                        break;
+                    }
+
+                    unsubscribe();
+                  },
+                );
+              });
+            }
+            throw new Error(
+              `No frontend action implemented for tool ${toolDetails.key}`,
+            );
+          },
+        },
+      },
     };
 
     if (context.bridgeConditions?.isAGWToNonABSChain) {
