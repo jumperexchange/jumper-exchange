5c163cb4e42e7953b706f141625038d986cf0565
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-03-02 09:16:50 +0100

    chore: set preload to false for unused fonts

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -12,6 +12,7 @@ export const manrope = Manrope({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-manrope',
+  preload: false,
 });
 
 export const urbanist = Urbanist({
@@ -25,16 +26,19 @@ export const sora = Sora({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-sora',
+  preload: false,
 });
 
 export const sequel65 = localFont({
   src: './customFonts/Sequel100Wide-65.woff2',
   variable: '--font-sequel-65',
+  preload: false,
 });
 
 export const sequel85 = localFont({
   src: './customFonts/Sequel100Wide-85.woff2',
   variable: '--font-sequel-85',
+  preload: false,
 });
 
 export const fonts = [inter, urbanist, sora, sequel65, sequel85, manrope];
