ebb003d022b34e1752f5d090ad259f6ee9a94858
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-03-09 13:13:18 +0100

    chore: add storage keys in the extend theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -20,6 +20,10 @@ import { createPaletteLight } from './paletteLight';
 import { defaultShape } from './shape';
 import type { ThemeFonts } from './typography';
 import { createTypography, defaultFonts } from './typography';
+import {
+  THEME_MODE_STORAGE_KEY,
+  THEME_COLOR_SCHEME_STORAGE_KEY,
+} from '@/providers/ThemeProvider/constants';
 
 type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
@@ -99,6 +103,8 @@ export const createJumperTheme = (
     typography,
     palette,
     colorSchemes,
+    modeStorageKey: THEME_MODE_STORAGE_KEY,
+    colorSchemeStorageKey: THEME_COLOR_SCHEME_STORAGE_KEY,
   } as Parameters<typeof extendTheme>[0]);
 };
 
