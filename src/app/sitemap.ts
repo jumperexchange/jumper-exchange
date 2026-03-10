251536ad6532da60fd362f58a9b77f6bd879aaab
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-03-10 11:44:58 +0100

    chore: add missing app paths to app sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,9 +10,13 @@ const strapiUrl = getStrapiBaseUrl();
 export const pages: SitemapPage[] = [
   { path: AppPaths.Main, priority: 1.0 },
   { path: AppPaths.Learn, priority: 0.9 },
+  { path: AppPaths.Earn, priority: 0.8 },
+  { path: AppPaths.Portfolio, priority: 0.8 },
   { path: AppPaths.Profile, priority: 0.8 },
   { path: AppPaths.Gas, priority: 0.7 },
   { path: AppPaths.PrivacyPolicy, priority: 0.6 },
+  { path: AppPaths.TermsOfBusiness, priority: 0.6 },
+  { path: AppPaths.Newsletter, priority: 0.5 },
 ];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
