c3b7c1da9f55e64b6d91360bcb169d1dab01e457
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2026-03-11 11:14:51 +0100

    chore: update sitemap indexing (#2735)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,4 +1,5 @@
-import { generateSitemaps } from '@/app/[lng]/bridge/sitemap';
+import { generateSitemaps as generateBridgeSitemaps } from '@/app/[lng]/bridge/sitemap';
+import { generateSitemaps as generateLearnSitemaps } from '@/app/[lng]/(infos)/learn/sitemap';
 import { getSiteUrl } from '@/const/urls';
 import { isProduction } from '@/utils/isProduction';
 import type { MetadataRoute } from 'next';
@@ -7,8 +8,11 @@ export const dynamic = 'force-dynamic';
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
   // Cannot have a sitemap index yet with app router, so we need to generate the sitemaps here
-  const bridgeSitemaps = (await generateSitemaps()).map(
+  const bridgeSitemaps = (await generateBridgeSitemaps()).map(
     (_, index) => `${getSiteUrl()}/en/bridge/sitemap/${index}.xml`,
+  );
+  const learnSitemaps = (await generateLearnSitemaps()).map(
+    (_, index) => `${getSiteUrl()}/en/learn/sitemap/${index}.xml`,
   );
   return {
     rules: {
@@ -18,6 +22,7 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
       ...(!isProduction && { disallow: '/' }),
     },
     sitemap: [`${getSiteUrl()}/sitemap.xml`].concat(
+      learnSitemaps,
       bridgeSitemaps,
       `${getSiteUrl()}/en/swap/sitemap.xml`,
     ),
--- src/const/urls.ts
+++ src/const/urls.ts
@@ -1,5 +1,4 @@
 import config from '@/config/env-config';
-import type { SitemapPage } from '@/types/sitemap';
 
 export const JUMPER_URL = 'https://jumper.xyz';
 export const JUMPER_STRAPI_URL = 'https://strapi.jumper.xyz';
@@ -35,6 +34,10 @@ export const JUMPER_TERMS_OF_BUSINESS_PATH = '/terms-of-business';
 export const JUMPER_PORTFOLIO_PATH = '/portfolio';
 export const JUMPER_NEWSLETTER_PATH = '/newsletter';
 
+export const JUMPER_BRIDGE_PATH_SOURCE_DESTINATION_DELIMITER = 'to';
+export const JUMPER_BRIDGE_PATH_DELIMITER = '-';
+export const JUMPER_BRIDGE_PATH_SOURCE_DESTINATION_FULL_DELIMITER = `${JUMPER_BRIDGE_PATH_DELIMITER}${JUMPER_BRIDGE_PATH_SOURCE_DESTINATION_DELIMITER}${JUMPER_BRIDGE_PATH_DELIMITER}`;
+
 export const DEFAULT_WALLET_ADDRESS =
   '0x0000000000000000000000000000000000000000';
 
@@ -43,15 +46,6 @@ export function getSiteUrl() {
     ? `https://${config.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
     : config.NEXT_PUBLIC_SITE_URL;
 }
-
-// prepare sitemap
-export const pages: SitemapPage[] = [
-  { path: JUMPER_MAIN_PATH, priority: 1.0 },
-  { path: JUMPER_LEARN_PATH, priority: 0.9 },
-  { path: JUMPER_PROFILE_PATH, priority: 0.8 },
-  { path: '/gas', priority: 0.7 },
-  { path: JUMPER_PRIVACY_POLICY_PATH, priority: 0.6 },
-];
 
 export enum AppPaths {
   Main = JUMPER_MAIN_PATH,
