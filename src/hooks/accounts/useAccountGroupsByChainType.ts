d22df7904f18b1e589ff7ea20ea2b371da0abe55
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2026-03-27 14:53:05 +0100

    feat: query positions by same chain type wallets (#2774)

---
+++ src/hooks/accounts/useAccountGroupsByChainType.ts
@@ -0,0 +1,25 @@
+import { useMemo } from 'react';
+import groupBy from 'lodash/groupBy';
+import type { Account } from '@lifi/wallet-management';
+import type { AddressQueryParams } from '@/app/lib/getPositionsForAddress';
+import { CHAIN_TYPE_TO_QUERY_PARAM } from '@/app/lib/getPositionsForAddress';
+
+export const useAccountGroupsByChainType = (accounts: Account[]) =>
+  useMemo(() => {
+    const connected = accounts.filter(
+      (acc) =>
+        acc.isConnected &&
+        acc.address &&
+        CHAIN_TYPE_TO_QUERY_PARAM[acc.chainType],
+    );
+
+    const grouped = groupBy(
+      connected,
+      (acc) => CHAIN_TYPE_TO_QUERY_PARAM[acc.chainType],
+    );
+
+    return Object.entries(grouped).map(([addressParam, accs]) => ({
+      addressParam: addressParam as keyof AddressQueryParams,
+      addresses: [...new Set(accs.map((acc) => acc.address!))].sort(),
+    }));
+  }, [accounts]);
