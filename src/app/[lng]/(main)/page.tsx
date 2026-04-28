e86fc716c2bc4785b9f3eaba3cba859ed94d52a3
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-04-28 10:22:13 +0300

    fix: revalidate main page and change isAnonymousSwapEnabled implementation

--- src/app/[lng]/(main)/page.tsx
+++ src/app/[lng]/(main)/page.tsx
@@ -2,6 +2,8 @@ import { Widgets } from '@/components/Widgets/Widgets';
 import { Widget } from '@/components/Widgets/Widget';
 import { MainWidgetContainer } from 'src/components/Containers/MainWidgetContainer';
 
+export const revalidate = 5 * 60; // Revalidate every 5 minutes
+
 export default async function Page() {
   const variant = 'default'; // exchange
   return (
