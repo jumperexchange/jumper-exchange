d66ea2ecda53a814b2c975bcde6bce5a27945559
Mihaela-Ioana Mot <32430018+mmioana@users.noreply.github.com>
2026-04-28 11:47:35 +0300

    fix: revalidate main page and change isAnonymousSwapEnabled implementation (#2832)

--- src/app/lib/getFeatureFlag.ts
+++ src/app/lib/getFeatureFlag.ts
@@ -1,5 +1,4 @@
 import config from '@/config/env-config';
-import { isProduction } from '@/utils/isProduction';
 
 const apiBaseUrl = config.NEXT_PUBLIC_BACKEND_URL;
 
@@ -35,8 +34,5 @@ export const isNewsletterFeatureEnabled = () => {
 };
 
 export const isAnonymousSwapEnabled = () => {
-  if (isProduction) {
-    return false;
-  }
-  return true;
+  return config.NEXT_PUBLIC_ENVIRONMENT !== 'production';
 };
