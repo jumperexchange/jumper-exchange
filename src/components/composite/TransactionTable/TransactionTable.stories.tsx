c9b05af6bac440ab79362337a6b46e6ef2bdf059
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-06-15 11:47:32 +0200

    chore: rename

--- src/components/composite/TransactionTable/TransactionTable.stories.tsx
+++ src/components/composite/TransactionTable/TransactionTable.stories.tsx
@@ -11,7 +11,7 @@ import {
 } from './fixtures';
 
 const meta: Meta<typeof TransactionTable> = {
-  title: 'components/Composite/TransactionCard/TransactionTable',
+  title: 'components/Composite/TransactionTable',
   component: TransactionTable,
   tags: ['autodocs'],
   parameters: {
