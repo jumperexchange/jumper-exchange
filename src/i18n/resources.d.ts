f6d96efb9cf5cf69836b8f24a9bf71928fdb89f5
Emil Yordanov <74621065+yordanove@users.noreply.github.com>
2026-06-17 16:37:08 +0300

    fix(i18n): rename private swap widget title to "Private Swap" (JUM-1145)

--- src/i18n/resources.d.ts
+++ src/i18n/resources.d.ts
@@ -1088,7 +1088,7 @@ export default interface Resources {
         title: 'Exchange';
       };
       private: {
-        title: 'Anonymous Swap';
+        title: 'Private Swap';
       };
       swapBridge: {
         title: 'Swap & Bridge';
