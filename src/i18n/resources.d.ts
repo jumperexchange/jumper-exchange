341eac8fb6ada5d898bbbfd4d5479f010bed2cc6
Kayanski <44806566+Kayanski@users.noreply.github.com>
2026-06-22 12:43:06 +0200

    Feature/jum 1147 privatehoudini surface minimum send amount when no private (#2966)

--- src/i18n/resources.d.ts
+++ src/i18n/resources.d.ts
@@ -600,6 +600,8 @@ export default interface Resources {
         confirm: 'Confirm';
         disclaimer1: "The address is correct and not an exchange wallet. Tokens sent to the wrong address can't be retrieved.";
         disclaimer2: "This transaction is fulfilled by a centralized provider who might ask for KYC if it's flagged.";
+        noRouteMinAmountSubtitle: 'Private swap routes typically appear for $40+ transactions.';
+        noRouteMinAmountTitle: 'No private route available';
         paste: 'Paste';
         subtitle: 'Set recipient address to keep it private.';
         title: "You're going Incognito";
