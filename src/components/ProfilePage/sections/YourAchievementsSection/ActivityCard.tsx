5a44cb8a81853210ac94e07cbac5a5456908ad72
Christian Maniewski <github@chmanie.com>
2026-06-23 14:26:44 +0200

    feat: [Jumper Pass] add running-month activity progress to Earn XP (#2947)

--- src/components/ProfilePage/sections/YourAchievementsSection/ActivityCard.tsx
+++ src/components/ProfilePage/sections/YourAchievementsSection/ActivityCard.tsx
@@ -1,8 +1,6 @@
-import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import type { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Tooltip } from '@/components/core/Tooltip/Tooltip';
 import { Badge } from 'src/components/Badge/Badge';
 import { BadgeSize, BadgeVariant } from 'src/components/Badge/Badge.styles';
 import type { PDA } from 'src/types/loyaltyPass';
@@ -10,13 +8,13 @@ import { formatDateLocalized } from 'src/utils/formatDateLocalized';
 import { getTextEllipsisStyles } from 'src/utils/styles/getTextEllipsisStyles';
 import {
   AchievementTile,
-  ActivityFooterRow,
-  ActivityHeaderGroup,
-  ActivityHeaderRow,
   TileContent,
+  TileFooterRow,
+  TileHeaderGroup,
+  TileHeaderRow,
   TileImage,
   TileImagePlaceholder,
-} from './YourAchievementsSection.styles';
+} from '../Section.style';
 
 interface ActivityCardProps {
   pda: PDA;
@@ -33,8 +31,8 @@ export const ActivityCard: FC<ActivityCardProps> = ({ pda }) => {
         <TileImagePlaceholder />
       )}
       <TileContent>
-        <ActivityHeaderGroup>
-          <ActivityHeaderRow>
+        <TileHeaderGroup>
+          <TileHeaderRow>
             <Typography variant="bodyMediumStrong" color="textPrimary" noWrap>
               {pda.reward.name}
             </Typography>
@@ -43,7 +41,7 @@ export const ActivityCard: FC<ActivityCardProps> = ({ pda }) => {
               variant={BadgeVariant.Alpha}
               size={BadgeSize.SM}
             />
-          </ActivityHeaderRow>
+          </TileHeaderRow>
           <Typography
             variant="bodyXSmall"
             color="textSecondary"
@@ -51,8 +49,8 @@ export const ActivityCard: FC<ActivityCardProps> = ({ pda }) => {
           >
             {pda.reward.description}
           </Typography>
-        </ActivityHeaderGroup>
-        <ActivityFooterRow>
+        </TileHeaderGroup>
+        <TileFooterRow>
           <Badge
             label={t('profile_page.yourAchievements.xpEarned', {
               xp: pda.points,
@@ -60,16 +58,7 @@ export const ActivityCard: FC<ActivityCardProps> = ({ pda }) => {
             variant={pda.points > 0 ? BadgeVariant.Success : BadgeVariant.Alpha}
             size={BadgeSize.MD}
           />
-          {pda.ongoing && (
-            <Tooltip
-              title={t('profile_page.tooltips.ongoingAchievement')}
-              arrow
-              placement="top"
-            >
-              <CircularProgress size={24} />
-            </Tooltip>
-          )}
-        </ActivityFooterRow>
+        </TileFooterRow>
       </TileContent>
     </AchievementTile>
   );
