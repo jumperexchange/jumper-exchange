5d36b8a0af299bef9716f17f6531a1618bd4049b
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-06-23 16:44:32 +0200

    chore: remove change to jumper-backend

--- src/types/jumper-backend.ts
+++ src/types/jumper-backend.ts
@@ -1684,7 +1684,7 @@ export interface EarnOpportunityWithScore {
   lpToken: Token;
   slug: string;
   featured: boolean;
-  lockupDays?: number;
+  lockupDays?: number | null;
   /** The cap in dollar */
   capInDollar?: string;
   /** @deprecated */
