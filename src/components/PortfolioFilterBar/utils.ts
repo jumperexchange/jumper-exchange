3f25edafc4831d5301e4aec7a1526141c1be39f3
Mihaela-Ioana Mot <mmioana94@gmail.com>
2026-07-01 20:20:43 +0200

    chore: extract dates badge

--- src/components/PortfolioFilterBar/utils.ts
+++ src/components/PortfolioFilterBar/utils.ts
@@ -1,33 +1,10 @@
 import { formatSliderValue } from '@/components/core/form/Select/utils';
 import { toFixedFractionDigits } from '@/utils/formatNumbers';
-import { differenceInDays } from 'date-fns';
+
+export { datesBadge } from '@/utils/filters/datesBadge';
 
 export const countBadge = (count: number): string | undefined =>
   count > 0 ? count.toString() : undefined;
-
-export const datesBadge = (
-  usedMin: Date,
-  usedMax: Date,
-  rangeMin: Date,
-  rangeMax: Date,
-  pendingValue: (Date | null)[],
-  rangeLabel: string,
-): string | undefined => {
-  if (
-    differenceInDays(usedMin, rangeMin) === 0 &&
-    differenceInDays(usedMax, rangeMax) === 0
-  ) {
-    return;
-  }
-
-  const [start, end] = pendingValue;
-
-  if (!start && !end) {
-    return;
-  }
-
-  return rangeLabel;
-};
 
 export const valueBadge = (
   usedMin: number,
