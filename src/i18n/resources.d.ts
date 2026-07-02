23cc61e4c97c1c93150ad04ee5d0b894b344cbc6
Kayanski <kowalski.kowalskin@gmail.com>
2026-07-02 10:15:27 +0200

    chore: only svm/evm supported

--- src/i18n/resources.d.ts
+++ src/i18n/resources.d.ts
@@ -986,7 +986,7 @@ export default interface Resources {
         tokens: 'Tokens';
       };
       overviewCard: {
-        pnlChartDisclaimer: 'Some DeFi and perp positions may not yet appear in the chart. New integrations are continuously being added.';
+        pnlChartDisclaimer: 'Only EVM/SVM addresses are supported. Some DeFi and perp positions may not yet appear in the chart. New integrations are continuously being added.';
         refreshTooltip: 'Click here to restart the indexing of your assets.';
         title: 'Portfolio';
       };
